10c553dbc0a9044c76402bb3b7facc36acc8296c
Yuri <silenok-86@mail.ru>
2018-04-26 13:20:23 +0300

    Init general form

--- TimetableOfClasses/Start.cs
+++ TimetableOfClasses/Start.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace TimetableOfClasses
 {
-    public partial class Form1 : Form
+    public partial class Start : Form
     {
-        public Form1()
+        public Start()
         {
             InitializeComponent();
         }
