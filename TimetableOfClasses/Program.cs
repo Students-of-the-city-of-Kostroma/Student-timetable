0cb56bf4268aacf5674db5db5d43c0a822a24d22
unknown <student@e325-03.kstu.edu.ru>
2018-06-05 09:55:41 +0300

    Demo git

--- TimetableOfClasses/Program.cs
+++ TimetableOfClasses/Program.cs
@@ -9,7 +9,7 @@ namespace TimetableOfClasses
     static class Program
     {
         /// <summary>
-        /// Главная точка входа для приложения.
+        /// Главная точка входа для приложения
         /// </summary>
         [STAThread]
         static void Main()
