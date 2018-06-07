061c5846cc2e8bc6148d76b769208a01ed15864f
TanyaMalis <tanya.malishev@gmail.com>
2018-06-07 10:37:33 +0300

    Инкапсуляция полей

--- TimetableOfClasses/MTeacher.cs
+++ TimetableOfClasses/MTeacher.cs
@@ -15,5 +15,96 @@ namespace TimetableOfClasses
         string academicDegree;
         string academicRank;
         byte sizeOfTeachingExperience;
+
+        public string Id
+        {
+            get
+            {
+                return id;
+            }
+
+            set
+            {
+                id = value;
+            }
+        }
+
+        public string Surname
+        {
+            get
+            {
+                return surname;
+            }
+
+            set
+            {
+                surname = value;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                name = value;
+            }
+        }
+
+        public string MiddleName
+        {
+            get
+            {
+                return middleName;
+            }
+
+            set
+            {
+                middleName = value;
+            }
+        }
+
+        public string AcademicDegree
+        {
+            get
+            {
+                return academicDegree;
+            }
+
+            set
+            {
+                academicDegree = value;
+            }
+        }
+
+        public string AcademicRank
+        {
+            get
+            {
+                return academicRank;
+            }
+
+            set
+            {
+                academicRank = value;
+            }
+        }
+
+        public byte SizeOfTeachingExperience
+        {
+            get
+            {
+                return sizeOfTeachingExperience;
+            }
+
+            set
+            {
+                sizeOfTeachingExperience = value;
+            }
+        }
     }
 }
