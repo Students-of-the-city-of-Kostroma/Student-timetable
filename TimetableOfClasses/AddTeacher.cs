cc841e93eed6b5e19c822733f4f3de71b9c1757f
Danil Smirnov <38256956+Whitetech7417@users.noreply.github.com>
2018-06-07 18:49:34 +0300

    Add files via upload

---
+++ TimetableOfClasses/AddTeacher.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TimetableOfClasses
+{
+    public partial class AddTeacher : Form
+    {
+        public AddTeacher()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {    
+            int a;
+            if (int.TryParse(comboBox1.Text, out a)) { this.Close(); }
+            else MessageBox.Show("Введите корректный стаж! (0-60)","Попробуйте снова",MessageBoxButtons.OK);
+           
+        }
+
+        private void AddTeacher_Load(object sender, EventArgs e)
+        {
+            
+            for(int i = 0;i<61;i++) comboBox1.Items.Add(i);
+           
+        }
+    }
+}
--- TimetableOfClasses/Teachers.cs
+++ TimetableOfClasses/Teachers.cs
@@ -16,5 +16,11 @@ namespace TimetableOfClasses
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            AddTeacher t = new AddTeacher();
+            t.ShowDialog();
+        }
     }
 }
