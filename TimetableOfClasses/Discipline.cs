9ff13f3acaa6e4675caf8fdbfa7c847fb3f37639
alvhmnv <38126911+alvhmnv@users.noreply.github.com>
2018-06-09 00:23:17 +0300

    Add files via upload

--- TimetableOfClasses/Discipline.cs
+++ TimetableOfClasses/Discipline.cs
@@ -20,7 +20,7 @@ namespace TimetableOfClasses
 
         private void DG_Disc_SelectionChanged(object sender, EventArgs e)
         {
-            if (DG_Disc.SelectedRows.Count > 0)
+            if ((DG_Disc.SelectedRows.Count > 0)&&(DG_Disc.SelectedCells[0].RowIndex != DG_Disc.Rows.Count - 1))
             {
                 Delete.Enabled = true;
             }
@@ -32,8 +32,7 @@ namespace TimetableOfClasses
 
         private void Delete_Click(object sender, EventArgs e)
         {
-            int delete = DG_Disc.SelectedCells[0].RowIndex;
-            DG_Disc.Rows.RemoveAt(delete);
+           DG_Disc.Rows.RemoveAt(DG_Disc.SelectedCells[0].RowIndex);
         }
     }
 }
