5ef2c45d95761a21c53a90f841ed11d990a886f1
Prizrak86 <silenok-86@mail.ru>
2018-06-11 23:37:07 +0300

    rebase issue-62 to Testing

---
+++ LibOfTimetableOfClasses/MDiscipline.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibOfTimetableOfClasses
+{
+     public class MDiscipline
+    {
+        string id;
+        string name;
+        string code;
+
+        public string Id
+        {
+            get
+            {
+                return id;
+            }
+
+            set
+            {
+                id = value;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                name = value;
+            }
+        }
+
+        public string Code
+        {
+            get
+            {
+                return code;
+            }
+
+            set
+            {
+                code = value;
+            }
+        }
+    }
+}
