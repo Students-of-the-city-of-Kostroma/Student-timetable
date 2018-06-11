5840098423adaa0d782a8b2f058ce27cdb544195
TanyaMalis <tanya.malishev@gmail.com>
2018-06-11 20:56:36 +0300

    Контроллер создан

---
+++ LibOfTimetableOfClasses/CTeacher.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibOfTimetableOfClasses
+{
+    public class CTeacher
+    {
+        List <MTeacher> teacherList = new List <MTeacher>();
+
+        public bool AddTeacher(string surname, string name, string middleName, string academicDegree, string AcademicRank, byte SizeOfTeachingExperience)
+        {
+
+        }
+
+        public bool SaveTeacher(string ID, string surname, string name, string middleName, string academicDegree, string AcademicRank, byte SizeOfTeachingExperience)
+        {
+
+        }
+
+        public bool DeleteTeacher(string ID)
+        {
+
+        }
+
+        public string[,] GetData()
+        {
+
+        }
+
+        public string[,] GetSortedData(string columnName, bool order)
+        {
+
+        }
+    }
+}
