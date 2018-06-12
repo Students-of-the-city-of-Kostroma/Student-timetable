4d1c7aa03cf789bc44ba8af5ac8225663f665a13
Prizrak86 <silenok-86@mail.ru>
2018-06-12 16:23:47 +0300

    merge

--- LibOfTimetableOfClasses/CTeacher.cs
+++ LibOfTimetableOfClasses/CTeacher.cs
@@ -20,9 +20,20 @@ namespace LibOfTimetableOfClasses
 
         }
 
-        public bool DeleteTeacher(string ID)
+        public bool DeleteTeacher(Guid ID)
         {
-
+            if (teacherList.Count > 0)
+            {
+                int i = 0;
+                while (!ID.Equals(teacherList[i].Id) || i < teacherList.Count) i++;
+                if ((i == teacherList.Count - 1) && (!ID.Equals(teacherList[i].Id))) return false;
+                else
+                {
+                    teacherList.RemoveAt(i);
+                    return true;
+                }
+            }
+            else return false;
         }
 
         public string[,] GetData()
--- TimetableOfClasses/AddTeacher.cs
+++ TimetableOfClasses/AddTeacher.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -18,24 +19,63 @@ namespace TimetableOfClasses
         }
 
         private void button1_Click(object sender, EventArgs e)
-        {    
+        {
             int a;
             if (int.TryParse(comboBox1.Text, out a)) { this.Close(); }
-            else MessageBox.Show("Введите корректный стаж! (0-60)","Попробуйте снова",MessageBoxButtons.OK);
-           
+            else MessageBox.Show("Введите корректный стаж! (0-60)", "Попробуйте снова", MessageBoxButtons.OK);
+
         }
 
         private void AddTeacher_Load(object sender, EventArgs e)
         {
             comboBox1.Text = "0";
-            textBox1.Text = "Jonh";
-            textBox2.Text = "Smith";
-            textBox3.Text = "Hoerq";
-            textBox4.Text = "First";
-            textBox5.Text = "Docent";
+            textBox1.Text = "Иван";
+            textBox2.Text = "Иванов";
+            textBox3.Text = "Иванович";
+            textBox4.Text = "Доктор наук";
+            textBox5.Text = "Профессор";
 
-            for(int i = 0;i<61;i++) comboBox1.Items.Add(i);
-           
+            for (int i = 0; i < 61; i++) comboBox1.Items.Add(i);
+
+        }
+
+        private void SelectionOfNumber(object sender, EventArgs e)
+        {
+            ComboBox R = sender as ComboBox;
+            try
+            {
+                R.Text = Regex.Replace(R.Text, "[^0-9,]", "");
+                R.Text = Convert.ToString(Math.Floor(Convert.ToDouble(R.Text)));
+                Convert.ToInt32(R.Text);
+                if ((Convert.ToInt32(R.Text) < 0))
+                    R.Text = Convert.ToString(Convert.ToInt32(R.Text) * -1);
+                if (Convert.ToInt32(R.Text) == 0)
+                    R.Text = "0";
+                if (Convert.ToInt32(R.Text) > 99)
+                    comboBox1.Text = comboBox1.Text.Remove(2);
+                else
+                if (Convert.ToInt32(R.Text) > 60)
+                    comboBox1.Text = comboBox1.Text.Remove(1);
+            }
+            catch
+            {
+                comboBox1.Text = "0";
+            }
+        }
+
+        private void SelectionOfLetters1(object sender, EventArgs e)
+        {
+            TextBox R = sender as TextBox;
+            R.Text = Regex.Replace(R.Text, "[^а-яА-Я- ]", "");
+            R.Text = Regex.Replace(R.Text, "[ ]", "-");
+            R.Text = Regex.Replace(R.Text, "[-]+", "-");
+        }
+
+        private void SelectionOfLetters2(object sender, EventArgs e)
+        {
+            TextBox R = sender as TextBox;
+            R.Text = Regex.Replace(R.Text, "[^а-яА-Я ]", "");
+            R.Text = Regex.Replace(R.Text, "[ ]+", " ");
         }
     }
 }
