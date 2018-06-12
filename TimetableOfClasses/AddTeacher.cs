59ee4a977e2554f03adec38a712b6b5e2d31ab1e
alvhmnv <38126911+alvhmnv@users.noreply.github.com>
2018-06-12 17:33:27 +0300

    Add files via upload

--- TimetableOfClasses/AddTeacher.cs
+++ TimetableOfClasses/AddTeacher.cs
@@ -16,6 +16,7 @@ namespace TimetableOfClasses
         public AddTeacher()
         {
             InitializeComponent();
+            caps.Visible = Console.CapsLock;
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -69,6 +70,13 @@ namespace TimetableOfClasses
             R.Text = Regex.Replace(R.Text, "[^а-яА-Я- ]", "");
             R.Text = Regex.Replace(R.Text, "[ ]", "-");
             R.Text = Regex.Replace(R.Text, "[-]+", "-");
+            if (R.Text.IndexOf("-") == 0)
+                R.Text = R.Text.Substring(1);
+            if (R.Text.LastIndexOf("-") == R.Text.Length-1)
+                R.Text = R.Text.Remove(R.Text.Length-1);
+            R.Text=R.Text.ToLower();
+            R.Text = FirstLetterToUpper(R.Text);
+
         }
 
         private void SelectionOfLetters2(object sender, EventArgs e)
@@ -76,6 +84,33 @@ namespace TimetableOfClasses
             TextBox R = sender as TextBox;
             R.Text = Regex.Replace(R.Text, "[^а-яА-Я ]", "");
             R.Text = Regex.Replace(R.Text, "[ ]+", " ");
+            if (R.Text.IndexOf(" ") == 0)
+                R.Text = R.Text.Substring(1);
+            if (R.Text.LastIndexOf(" ") == R.Text.Length - 1)
+                R.Text = R.Text.Remove(R.Text.Length - 1);
+            R.Text = R.Text.ToLower();
+            R.Text = FirstLetterToUpper(R.Text);
         }
+
+        public static string FirstLetterToUpper(string str)
+        {
+            if (str.Length > 0)
+            {
+                if (str.IndexOf("-") > 0)
+                {
+                    return Char.ToUpper(str[0]) + str.Substring(1, str.IndexOf("-")) +Char.ToUpper(str[str.IndexOf("-") + 1]) + str.Substring(str.IndexOf("-") + 2);
+                }
+                else
+                return Char.ToUpper(str[0]) + str.Substring(1);
+            }         
+            return "";
+        }
+
+        private void AddTeacher_KeyUp(object sender, KeyEventArgs e)
+        {
+            if ((e.KeyCode == Keys.CapsLock))
+                caps.Visible = Console.CapsLock;
+        }
+
     }
 }
