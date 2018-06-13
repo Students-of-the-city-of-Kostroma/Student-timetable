8ec6309128a2ecfc30450bdb5296e5fa9df4b8a9
TanyaMalis <tanya.malishev@gmail.com>
2018-06-13 10:55:16 +0300

    Метод SaveTeacher

--- LibOfTimetableOfClasses/CTeacher.cs
+++ LibOfTimetableOfClasses/CTeacher.cs
@@ -15,9 +15,25 @@ namespace LibOfTimetableOfClasses
 
         }
 
-        public bool SaveTeacher(string ID, string surname, string name, string middleName, string academicDegree, string AcademicRank, byte SizeOfTeachingExperience)
+        public bool SaveTeacher(Guid ID, string surname, string name, string middleName, string academicDegree, string AcademicRank, byte SizeOfTeachingExperience)
         {
-
+            if (teacherList.Count > 0)
+            {
+                int i = 0;
+                while (!ID.Equals(teacherList[i].Id) || i < teacherList.Count) i++;
+                if ((i == teacherList.Count - 1) && (!ID.Equals(teacherList[i].Id))) return false;
+                else
+                {
+                    teacherList[i].Surname = surname;
+                    teacherList[i].Name = name;
+                    teacherList[i].MiddleName = middleName;
+                    teacherList[i].AcademicDegree = academicDegree;
+                    teacherList[i].AcademicRank = AcademicRank;
+                    teacherList[i].SizeOfTeachingExperience = SizeOfTeachingExperience;
+                    return true;
+                }
+            }
+            else return false;
         }
 
         public bool DeleteTeacher(string ID)
