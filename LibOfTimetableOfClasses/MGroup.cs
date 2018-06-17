4c588da6b8cef5bed23eaa2707dee22d6ac27d9e
TanyaMalis <tanya.malishev@gmail.com>
2018-06-17 22:04:09 +0300

    Класс MGroup

---
+++ LibOfTimetableOfClasses/MGroup.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibOfTimetableOfClasses
+{
+    public class MGroup
+    {
+        Guid id;
+        string cipher;
+        byte population;
+        public Guid Id
+        {
+            get
+            {
+                return id;
+            }
+
+            set
+            {
+                id = value;
+            }
+        }
+        public string Сipher
+        {
+            get
+            {
+                return cipher;
+            }
+
+            set
+            {
+                cipher = value;
+            }
+        }
+        public byte Population
+        {
+            get
+            {
+                return population;
+            }
+
+            set
+            {
+                population = value;
+            }
+        }
+    }
+}
