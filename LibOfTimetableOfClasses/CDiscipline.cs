907271f50f3108bdb4ecd61a09f9bfd38660d3a9
TanyaMalis <tanya.malishev@gmail.com>
2018-06-20 22:36:40 +0300

    Добавлен класс CDiscipline и метод AddDiscipline

---
+++ LibOfTimetableOfClasses/CDiscipline.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibOfTimetableOfClasses
+{
+    public class CDiscipline
+    {
+        List<MDiscipline> disciplineList = new List<MDiscipline>();
+
+        public bool AddDiscipline(string name, string code)
+        {
+            try
+            {
+                MDiscipline D = new MDiscipline();
+                D.Id = Guid.NewGuid();
+                D.Name = name;
+                D.Code = code;
+                disciplineList.Add(D);
+                return true;
+            }
+            catch { return false; }
+        }
+    }
+}
