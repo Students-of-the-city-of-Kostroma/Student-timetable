aa93b81f9e350e1e6fe853d12be71e73a5275879
Анастасия Баринова <barinova.99.99@mail.ru>
2018-06-21 19:19:47 +0300

    метод DeleteDiscipline

---
+++ LibOfTimetableOfClasses/CDiscipline.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibOfTimetableOfClasses
+{
+    class CDiscipline
+    {
+        List<MDiscipline> disciplineList = new List<MDiscipline>();
+
+        public bool DeleteDiscipline(Guid ID)
+        {
+            try
+            {
+                int i = disciplineList.FindIndex(x => x.Id.ToString() == ID.ToString());
+                disciplineList.Remove(disciplineList[i]);
+                return true;
+            }
+            catch { return false; }
+        }
+    }
+}
