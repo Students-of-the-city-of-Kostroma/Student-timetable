367d3eb7b871f3070b4a786bf30ffef4ac93b156
Danil Smirnov <38256956+Whitetech7417@users.noreply.github.com>
2018-06-22 14:49:51 +0300

    Add files via upload

--- TimetableOfClasses/Teachers.cs
+++ TimetableOfClasses/Teachers.cs
@@ -30,7 +30,7 @@ namespace TimetableOfClasses
 
         private void DG_SelectionChanged(object sender, EventArgs e)
         {
-            button2.Enabled = ((DG.SelectedRows.Count > 0) && (DG.SelectedCells[0].RowIndex != DG.Rows.Count - 1)) ? true : false;
+            button2.Enabled = ((DG.SelectedRows.Count > 0) && (DG.SelectedCells[0].RowIndex != DG.Rows.Count - 1));
         }
 
         private void DG_SizeChanged(object sender, EventArgs e)
