8371c4a98648986f90ff5b15c35376ec37d6f323
TanyaMalis <tanya.malishev@gmail.com>
2018-06-23 22:20:41 +0300

    Реализовано public object this[Keys key] и добавлены комментарии

--- LibOfTimetableOfClasses/MDiscipline.cs
+++ LibOfTimetableOfClasses/MDiscipline.cs
@@ -8,10 +8,45 @@ namespace LibOfTimetableOfClasses
 {
      public class MDiscipline
     {
+        /// <summary>
+        /// Перечисление полей объекта модели Дисциплина
+        /// </summary>
+        public enum Keys { Id = 0, Name = 1, Code = 2 }
+        /// <summary>
+        /// Идентификатор ID
+        /// </summary>
         Guid id;
+        /// <summary>
+        /// Название дисциплины
+        /// </summary>
         string name;
+        /// <summary>
+        /// Код дисциплины
+        /// </summary>
         string code;
 
+        /// <summary>
+        /// Возвращает значение перечисления
+        /// </summary>
+        /// <param ключ="key"></param>
+        /// <returns></returns>
+        public object this[Keys key]
+        {
+            get
+            {
+                switch (key)
+                {
+                    case Keys.Id: return Id;
+                    case Keys.Name: return Name;
+                    case Keys.Code: return Code;
+                    default: return null;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Возвращает или задает значение ID
+        /// </summary>
         public Guid Id
         {
             get
@@ -25,6 +60,9 @@ namespace LibOfTimetableOfClasses
             }
         }
 
+        /// <summary>
+        /// Возвращает или задает значение Name - название
+        /// </summary>
         public string Name
         {
             get
@@ -38,6 +76,9 @@ namespace LibOfTimetableOfClasses
             }
         }
 
+        /// <summary>
+        /// Возвращает или задает значение Code - код
+        /// </summary>
         public string Code
         {
             get
