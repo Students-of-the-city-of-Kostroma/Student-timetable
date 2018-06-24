52f9cac0712a59bbf8715542b4c3c25d748b3d6e
Анастасия Баринова <barinova.99.99@mail.ru>
2018-06-25 01:24:11 +0300

    метод DeleteDiscipline

--- LibOfTimetableOfClasses/CDiscipline.cs
+++ LibOfTimetableOfClasses/CDiscipline.cs
@@ -6,10 +6,20 @@ using System.Threading.Tasks;
 
 namespace LibOfTimetableOfClasses
 {
+    /// <summary>
+    /// Контроллер для дисциплин
+    /// </summary>
     class CDiscipline
     {
+        /// <summary>
+        /// Список объектов Дисциплина
+        /// </summary>
         protected List<MDiscipline> disciplineList = new List<MDiscipline>();
-
+        /// <summary>
+        /// Метод удаления дисциплин
+        /// </summary>
+        /// <param name="ID"></param>
+        /// <returns></returns>
         public bool DeleteDiscipline(Guid ID)
         {
             try
