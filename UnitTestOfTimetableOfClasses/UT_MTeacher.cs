64edd992b715d88032829eb40cb91954d208db53
TanyaMalis <tanya.malishev@gmail.com>
2018-07-10 12:23:56 +0300

    Тест MTeacher

---
+++ UnitTestOfTimetableOfClasses/UT_MTeacher.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TimetableOfClasses;
+using System.Windows.Forms;
+using LibOfTimetableOfClasses;
+
+namespace UnitTestOfTimetableOfClasses
+{
+    [TestClass]
+    public class UT_MTeacher
+    {
+        private static MTeacher T;
+        [TestInitialize]
+        public void ClassInitialize()
+        {
+            T = new MTeacher();
+            T.Surname = "";
+            T.Name = "";
+            T.MiddleName = "";
+            T.AcademicDegree = "";
+            T.AcademicRank = "";
+            T.SizeOfTeachingExperience = 0;
+        }
+        [TestMethod]
+        public void Issue_30_1()
+        {
+            Assert.AreEqual(typeof(Guid), T.Id.GetType(), "Ожидался тип поля Guid");
+        }
+
+        public void Issue_30_2()
+        {
+            Assert.AreEqual(typeof(String), T.Surname.GetType(), "Ожидался тип поля String");
+        }
+
+        public void Issue_30_3()
+        {
+            Assert.AreEqual(typeof(String), T.Name.GetType(), "Ожидался тип поля String");
+        }
+
+        public void Issue_30_4()
+        {
+            Assert.AreEqual(typeof(String), T.MiddleName.GetType(), "Ожидался тип поля String");
+        }
+
+        public void Issue_30_5()
+        {
+            Assert.AreEqual(typeof(String), T.AcademicDegree.GetType(), "Ожидался тип поля String");
+        }
+
+        public void Issue_30_6()
+        {
+            Assert.AreEqual(typeof(String), T.AcademicRank.GetType(), "Ожидался тип поля String");
+        }
+
+        public void Issue_30_7()
+        {
+            Assert.AreEqual(typeof(Byte), T.SizeOfTeachingExperience.GetType(), "Ожидался тип поля String");
+        }
+    }
+}
