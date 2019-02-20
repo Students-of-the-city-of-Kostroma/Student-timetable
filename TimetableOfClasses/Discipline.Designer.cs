601f30562ab59699242582dd0dabdaac41bc9893
Rogachev_99 <rogachev_99@mail.ru>
2019-02-21 00:58:21 +0300

    Добавил кнопку на Discipline, для открытия окна AddDiscipline

--- TimetableOfClasses/Discipline.Designer.cs
+++ TimetableOfClasses/Discipline.Designer.cs
@@ -28,68 +28,81 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.DG_Disc = new System.Windows.Forms.DataGridView();
-            this.Delete = new System.Windows.Forms.Button();
-            this.Kod = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Naim = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            ((System.ComponentModel.ISupportInitialize)(this.DG_Disc)).BeginInit();
-            this.SuspendLayout();
-            // 
-            // DG_Disc
-            // 
-            this.DG_Disc.AllowUserToAddRows = false;
-            this.DG_Disc.AllowUserToDeleteRows = false;
-            this.DG_Disc.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+			this.DG_Disc = new System.Windows.Forms.DataGridView();
+			this.Kod = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.Naim = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.Delete = new System.Windows.Forms.Button();
+			this.btCreateDiscipline = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.DG_Disc)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// DG_Disc
+			// 
+			this.DG_Disc.AllowUserToAddRows = false;
+			this.DG_Disc.AllowUserToDeleteRows = false;
+			this.DG_Disc.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.DG_Disc.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.DG_Disc.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+			this.DG_Disc.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.DG_Disc.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Kod,
             this.Naim});
-            this.DG_Disc.Location = new System.Drawing.Point(12, 12);
-            this.DG_Disc.Name = "DG_Disc";
-            this.DG_Disc.ReadOnly = true;
-            this.DG_Disc.Size = new System.Drawing.Size(394, 156);
-            this.DG_Disc.TabIndex = 0;
-            this.DG_Disc.SelectionChanged += new System.EventHandler(this.DG_Disc_SelectionChanged);
-            // 
-            // Delete
-            // 
-            this.Delete.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
-            this.Delete.Location = new System.Drawing.Point(167, 175);
-            this.Delete.Name = "Delete";
-            this.Delete.Size = new System.Drawing.Size(75, 23);
-            this.Delete.TabIndex = 1;
-            this.Delete.Text = "Удалить";
-            this.Delete.UseVisualStyleBackColor = true;
-            this.Delete.Click += new System.EventHandler(this.Delete_Click);
-            // 
-            // Kod
-            // 
-            this.Kod.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.DisplayedCells;
-            this.Kod.HeaderText = "Код";
-            this.Kod.Name = "Kod";
-            this.Kod.ReadOnly = true;
-            this.Kod.Width = 51;
-            // 
-            // Naim
-            // 
-            this.Naim.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.Naim.HeaderText = "Наименование";
-            this.Naim.Name = "Naim";
-            this.Naim.ReadOnly = true;
-            // 
-            // Discipline
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(417, 203);
-            this.Controls.Add(this.Delete);
-            this.Controls.Add(this.DG_Disc);
-            this.Name = "Discipline";
-            this.Text = "Дисциплины";
-            ((System.ComponentModel.ISupportInitialize)(this.DG_Disc)).EndInit();
-            this.ResumeLayout(false);
+			this.DG_Disc.Location = new System.Drawing.Point(12, 12);
+			this.DG_Disc.Name = "DG_Disc";
+			this.DG_Disc.ReadOnly = true;
+			this.DG_Disc.Size = new System.Drawing.Size(394, 156);
+			this.DG_Disc.TabIndex = 0;
+			this.DG_Disc.SelectionChanged += new System.EventHandler(this.DG_Disc_SelectionChanged);
+			// 
+			// Kod
+			// 
+			this.Kod.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.DisplayedCells;
+			this.Kod.HeaderText = "Код";
+			this.Kod.Name = "Kod";
+			this.Kod.ReadOnly = true;
+			this.Kod.Width = 51;
+			// 
+			// Naim
+			// 
+			this.Naim.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+			this.Naim.HeaderText = "Наименование";
+			this.Naim.Name = "Naim";
+			this.Naim.ReadOnly = true;
+			// 
+			// Delete
+			// 
+			this.Delete.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+			this.Delete.Location = new System.Drawing.Point(167, 175);
+			this.Delete.Name = "Delete";
+			this.Delete.Size = new System.Drawing.Size(75, 23);
+			this.Delete.TabIndex = 1;
+			this.Delete.Text = "Удалить";
+			this.Delete.UseVisualStyleBackColor = true;
+			this.Delete.Click += new System.EventHandler(this.Delete_Click);
+			// 
+			// btCreateDiscipline
+			// 
+			this.btCreateDiscipline.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.btCreateDiscipline.Location = new System.Drawing.Point(330, 175);
+			this.btCreateDiscipline.Name = "btCreateDiscipline";
+			this.btCreateDiscipline.Size = new System.Drawing.Size(75, 23);
+			this.btCreateDiscipline.TabIndex = 2;
+			this.btCreateDiscipline.Text = "Создать";
+			this.btCreateDiscipline.UseVisualStyleBackColor = true;
+			this.btCreateDiscipline.Click += new System.EventHandler(this.btCreateDiscipline_Click);
+			// 
+			// Discipline
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(417, 203);
+			this.Controls.Add(this.btCreateDiscipline);
+			this.Controls.Add(this.Delete);
+			this.Controls.Add(this.DG_Disc);
+			this.Name = "Discipline";
+			this.Text = "Дисциплины";
+			((System.ComponentModel.ISupportInitialize)(this.DG_Disc)).EndInit();
+			this.ResumeLayout(false);
 
         }
 
@@ -99,5 +112,6 @@
         private System.Windows.Forms.Button Delete;
         private System.Windows.Forms.DataGridViewTextBoxColumn Kod;
         private System.Windows.Forms.DataGridViewTextBoxColumn Naim;
-    }
+		private System.Windows.Forms.Button btCreateDiscipline;
+	}
 }
