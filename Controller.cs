1e958e2224a661788a1e7052d12ee49feafee22f
unknown <valerimash78@gmail.com>
2019-03-06 16:09:48 +0300

    Изменить структуру класса Controller

---
+++ Controller.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibOfTimetableOfClasses
+{
+    /// <summary>
+    /// Абстракция реадизуюящая основной функционал управления данными
+    /// </summary>
+    public class Controller: DataTable
+    {
+        /// <summary>
+        /// Создает таблицу с указаным именем
+        /// </summary>
+        /// <param name="tableName">Имя таблицы</param>
+        public Controller(string tableName="Controller"): base (tableName)
+        {
+            DataTable table = new DataTable(tableName);
+            DataColumn column = new DataColumn();
+            column.DataType = typeof(Guid);
+            column.ColumnName = "ID";
+            column.ReadOnly = true;
+            column.Unique = true;
+            table.Columns.Add(column);
+            DataSet.Tables.Add(table);
+        }
+        /// <summary>
+        /// Удаление экземпляра из таблицы
+        /// </summary>
+        /// <param name="model">Удаляемый экземпляр</param>
+        /// <returns>Вернет истину если удалось изменить экземпляр</returns>
+        public abstract bool Delete(Model model);
+        
+    }
+}
