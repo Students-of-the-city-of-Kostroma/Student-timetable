4f6698ce13f4b56e1da5fb0800b6cf81ded1cbc3
denis <kapu4inko@gmail.com>
2019-03-07 12:42:33 +0300

    Изменение пробелов на табы 2

--- TimetableOfClasses/Start.cs
+++ TimetableOfClasses/Start.cs
@@ -53,18 +53,18 @@ namespace TimetableOfClasses
 			ministry.Show();
 		}
 
-        private void редактироватьЗаписьToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            AddOrEditDirectory addOrEditDirectory = new AddOrEditDirectory();
-            addOrEditDirectory.EditRow(null);
-            addOrEditDirectory.Show();
-        }
+		private void редактироватьЗаписьToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			AddOrEditDirectory addOrEditDirectory = new AddOrEditDirectory();
+			addOrEditDirectory.EditRow(null);
+			addOrEditDirectory.Show();
+		}
 
-        private void добавитьЗаписьToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            AddOrEditDirectory addOrEditDirectory = new AddOrEditDirectory();
-            addOrEditDirectory.NewRow(null);
-            addOrEditDirectory.Show();
-        }
+		private void добавитьЗаписьToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			AddOrEditDirectory addOrEditDirectory = new AddOrEditDirectory();
+			addOrEditDirectory.NewRow(null);
+			addOrEditDirectory.Show();
+		}
     }
 }
