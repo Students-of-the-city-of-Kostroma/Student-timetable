abf043373b96e5ff5ce15b87c1e187a6a47ae0b3
denis <kapu4inko@gmail.com>
2019-03-07 12:33:25 +0300

    Изменение пробелов на табы

--- TimetableOfClasses/AddOrEditDirectory.cs
+++ TimetableOfClasses/AddOrEditDirectory.cs
@@ -10,26 +10,26 @@ using System.Windows.Forms;
 
 namespace TimetableOfClasses
 {
-    public partial class AddOrEditDirectory : Form
-    {
-        public AddOrEditDirectory()
-        {
-            InitializeComponent();
-        }
+	public partial class AddOrEditDirectory : Form
+	{
+		public AddOrEditDirectory()
+		{
+			InitializeComponent();
+		}
 
-        public void NewRow(DataRow dataRow)
-        {
-            this.Text = "Добавить запись";
-            this.btClear.Text = "Создать и очистить";
-            this.btClose.Text = "Создать и закрыть";
+		public void NewRow(DataRow dataRow)
+		{
+			this.Text = "Добавить запись";
+			this.btClear.Text = "Создать и очистить";
+			this.btClose.Text = "Создать и закрыть";
 
-        }
+		}
 
-        public void EditRow(DataRow dataRow)
-        {
-            this.Text = "Редактировать запись";
-            this.btClear.Text = "Изменить и очистить";
-            this.btClose.Text = "Изменить и закрыть";
-        }
-    }
+		public void EditRow(DataRow dataRow)
+		{
+			this.Text = "Редактировать запись";
+			this.btClear.Text = "Изменить и очистить";
+			this.btClose.Text = "Изменить и закрыть";
+		}
+	}
 }
