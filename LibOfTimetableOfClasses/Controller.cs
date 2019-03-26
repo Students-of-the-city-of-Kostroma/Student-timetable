2ea18d15ea04c224a4419491aff1c2a02a5c4bcb
unknown <valerimash78@gmail.com>
2019-03-26 11:45:38 +0300

    коммит на изменение структуры класса Controller

--- LibOfTimetableOfClasses/Controller.cs
+++ LibOfTimetableOfClasses/Controller.cs
@@ -16,24 +16,25 @@ namespace LibOfTimetableOfClasses
         /// Создает таблицу с указаным именем
         /// </summary>
         /// <param name="tableName">Имя таблицы</param>
+        
         public Controller(string tableName = "Controller")
             : base(tableName)
         {
-            DataTable table = new DataTable(tableName);
+            //DataTable table = new DataTable(tableName);
             DataColumn column = new DataColumn();
             column.DataType = typeof(Guid);
             column.ColumnName = "ID";
             column.ReadOnly = true;
             column.Unique = true;
-            table.Columns.Add(column);
-            DataSet.Tables.Add(table);
+            this.Columns.Add(column);
+            DataSet.Tables.Add(this);
         }
         /// <summary>
         /// Удаление экземпляра из таблицы
         /// </summary>
         /// <param name="model">Удаляемый экземпляр</param>
         /// <returns>Вернет истину если удалось изменить экземпляр</returns>
-        public abstract bool Delete(Model model);
+        public bool Delete(Model model);
 
     }
 }
