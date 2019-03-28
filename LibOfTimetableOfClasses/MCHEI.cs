960d6960dfc4390481099567c5139282e5940fbe
Dmitry Ivantsov <dmitry.iwantzov@gmail.com>
2019-03-28 10:33:38 +0300

    commit1

---
+++ LibOfTimetableOfClasses/MCHEI.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibOfTimetableOfClasses
+{
+	class MCHEI:Model
+	{
+		string fullName;
+		string abbreviatedName;
+		string rector;
+		int phone;
+		string email;
+		
+
+		public string FullName
+		{
+			get
+			{
+				return fullName;
+			}
+
+			set
+			{
+				fullName = value;
+			}
+		}
+
+		public string AbbreviatedName
+		{
+			get
+			{
+				return abbreviatedName;
+			}
+
+			set
+			{
+				abbreviatedName = value;
+			}
+		}
+
+		public string Rector
+		{
+			get
+			{
+				return rector;
+			}
+
+			set
+			{
+				rector = value;
+			}
+		}
+
+		public int Phone
+		{
+			get
+			{
+				return phone;
+			}
+
+			set
+			{
+				if(value>=0) phone = value;
+			}
+		}
+
+		public string Email
+		{
+			get
+			{
+				return email;
+			}
+
+			set
+			{
+				email = value;
+			}
+		}
+
+
+
+		public MCHEI(string fullName, string abbreviatedName, string rector, int phone, string email) : base()
+		{
+			FullName = fullName;
+			AbbreviatedName = abbreviatedName;
+			Rector = rector;
+			Phone = phone;
+			Email = email;
+		}
+
+
+	}
+}
+
