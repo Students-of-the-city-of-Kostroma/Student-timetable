828bab9af503cfc31a2f269b80782b6ad4f45376
KharlamovaAlexandra <kharlamovaalexandralla@gmail.com>
2019-04-03 21:34:49 +0300

    Созданы тесты

--- UnitTestOfTimetableOfClasses/UT_CTeacher.cs
+++ UnitTestOfTimetableOfClasses/UT_CTeacher.cs
@@ -141,5 +141,140 @@ namespace UnitTestOfTimetableOfClasses
 			Assert.AreEqual(ex, act);
 			Assert.AreEqual(C1, C2);
 		}
+
+		[TestMethod]
+		public void Task_13_1()
+		{
+			Controllers.CTeacher.Select().Clear();
+			bool expected = true;
+			bool actual;
+			MTeacher M_Teacher = new MTeacher("Киприна", "Людмила", "Юрьевна",
+			"Кандидат наук", "Профессор", 20);
+			int Count1 = Controllers.CTeacher.Select().Rows.Count;
+			actual = Controllers.CTeacher.Insert(M_Teacher);
+			int Count2 = Controllers.CTeacher.Select().Rows.Count;
+			Assert.AreEqual(expected, actual);
+			Assert.AreEqual(Count1 + 1, Count2);
+		}
+
+		[TestMethod]
+		public void Task_13_2()
+		{
+			Task_13_1();
+			bool expected = true;
+			bool actual;
+			MTeacher M_Teacher = new MTeacher("Киприна", "Павел", "Игоревич",
+		    "Доктор наук", "Доцент", 30);
+			int Count1 = Controllers.CTeacher.Select().Rows.Count;
+			actual = Controllers.CTeacher.Insert(M_Teacher);
+			int Count2 = Controllers.CTeacher.Select().Rows.Count;
+			Assert.AreEqual(expected, actual);
+			Assert.AreEqual(Count1 + 1, Count2);
+		}
+
+		[TestMethod]
+		public void Task_13_3()
+		{
+			Task_13_1();
+			bool expected = true;
+			bool actual;
+			MTeacher M_Teacher = new MTeacher("Пузиков", "Людмила", "Игоревич",
+			"Доктор наук", "Доцент", 30);
+			int Count1 = Controllers.CTeacher.Select().Rows.Count;
+			actual = Controllers.CTeacher.Insert(M_Teacher);
+			int Count2 = Controllers.CTeacher.Select().Rows.Count;
+			Assert.AreEqual(expected, actual);
+			Assert.AreEqual(Count1 + 1, Count2);
+		}
+
+		[TestMethod]
+		public void Task_13_4()
+		{
+			Task_13_1();
+			bool expected = true;
+			bool actual;
+			MTeacher M_Teacher = new MTeacher("Пузиков", "Павел", "Юрьевна",
+			"Доктор наук", "Доцент", 30);
+			int Count1 = Controllers.CTeacher.Select().Rows.Count;
+			actual = Controllers.CTeacher.Insert(M_Teacher);
+			int Count2 = Controllers.CTeacher.Select().Rows.Count;
+			Assert.AreEqual(expected, actual);
+			Assert.AreEqual(Count1 + 1, Count2);
+		}
+
+		[TestMethod]
+		public void Task_13_5()
+		{
+			Task_13_1();
+			bool expected = true;
+			bool actual;
+			MTeacher M_Teacher = new MTeacher("Пузиков", "Павел", "Игоревич",
+			"Кандидат наук", "Доцент", 30);
+			int Count1 = Controllers.CTeacher.Select().Rows.Count;
+			actual = Controllers.CTeacher.Insert(M_Teacher);
+			int Count2 = Controllers.CTeacher.Select().Rows.Count;
+			Assert.AreEqual(expected, actual);
+			Assert.AreEqual(Count1 + 1, Count2);
+		}
+
+		[TestMethod]
+		public void Task_13_6()
+		{
+			Task_13_1();
+			bool expected = true;
+			bool actual;
+			MTeacher M_Teacher = new MTeacher("Пузиков", "Павел", "Игоревич",
+			"Доктор наук", "Профессор", 30);
+			int Count1 = Controllers.CTeacher.Select().Rows.Count;
+			actual = Controllers.CTeacher.Insert(M_Teacher);
+			int Count2 = Controllers.CTeacher.Select().Rows.Count;
+			Assert.AreEqual(expected, actual);
+			Assert.AreEqual(Count1 + 1, Count2);
+		}
+
+		[TestMethod]
+		public void Task_13_7()
+		{
+			Task_13_1();
+			bool expected = true;
+			bool actual;
+			MTeacher M_Teacher = new MTeacher("Пузиков", "Павел", "Игоревич",
+			"Доктор наук", "Доцент", 20);
+			int Count1 = Controllers.CTeacher.Select().Rows.Count;
+			actual = Controllers.CTeacher.Insert(M_Teacher);
+			int Count2 = Controllers.CTeacher.Select().Rows.Count;
+			Assert.AreEqual(expected, actual);
+			Assert.AreEqual(Count1 + 1, Count2);
+		}
+
+		[TestMethod]
+		public void Task_13_8()
+		{
+			Task_13_1();
+			bool expected = false;
+			bool actual;
+			MTeacher M_Teacher = new MTeacher("Киприна", "Людмила", "Юрьевна",
+			"Кандидат наук", "Профессор", 20);
+			int Count1 = Controllers.CTeacher.Select().Rows.Count;
+			actual = Controllers.CTeacher.Insert(M_Teacher);
+			int Count2 = Controllers.CTeacher.Select().Rows.Count;
+			Assert.AreEqual(expected, actual);
+			Assert.AreEqual(Count1, Count2);
+		}
+
+		[TestMethod]
+		public void Task_13_9()
+		{
+			Task_13_1();
+			bool expected = true;
+			bool actual;
+			MTeacher M_Teacher = new MTeacher("Пузиков", "Павел", "Игоревич",
+			"Доктор наук", "Доцент", 30);
+			int Count1 = Controllers.CTeacher.Select().Rows.Count;
+			actual = Controllers.CTeacher.Insert(M_Teacher);
+			int Count2 = Controllers.CTeacher.Select().Rows.Count;
+			Assert.AreEqual(expected, actual);
+			Assert.AreEqual(Count1 + 1, Count2);
+		}
 	}
 }
