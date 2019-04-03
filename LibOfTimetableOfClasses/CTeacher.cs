39488a5ed8bd1abc149844951381103218ba4fcd
map82top <36793054+map82top@users.noreply.github.com>
2019-04-03 18:16:49 +0300

    Исправлкение замечаний

--- LibOfTimetableOfClasses/CTeacher.cs
+++ LibOfTimetableOfClasses/CTeacher.cs
@@ -53,42 +53,29 @@ namespace LibOfTimetableOfClasses
             column.ReadOnly = true;
             table.Columns.Add(column);
         }
-        public override bool Insert(Model model)
-        {
-			MTeacher mTeacher = (MTeacher)model;
-			//проверяем на наличие в таблице записи с таким ключом
-			for (int i = 0; i < table.Rows.Count; i++)
-			{
-				if (mTeacher.Surname == (string)table.Rows[i]["Surname"] &&
-					mTeacher.Name == (string)table.Rows[i]["Name"] &&
-					mTeacher.MiddleName == (string)table.Rows[i]["MiddleName"] &&
-					mTeacher.SizeOfTeachingExperience == (byte)table.Rows[i]["SizeOfTeachingExperience"])
-				{
-					//в таблице уже есть запись с таким ключом
-					return false;
-				}
-			}
 
+		public override bool Insert(Model model)
+		{
+			MTeacher mTeacher = (MTeacher)model;
 			try
-            {
-                
-                DataRow newRow = table.NewRow();
-                newRow["ID"] = Guid.NewGuid();
-                newRow["Surname"] = mTeacher.Surname;
-                newRow["Name"] = mTeacher.Name;
-                newRow["MiddleName"] = mTeacher.MiddleName;
-                newRow["AcademicDegree"] = mTeacher.AcademicDegree;
-                newRow["AcademicRank"] = mTeacher.AcademicRank;
-                newRow["SizeOfTeachingExperience"] = mTeacher.SizeOfTeachingExperience;
-                table.Rows.Add(newRow);
-                return true;
-            }
-            catch (Exception ex)
-            {
-                Debug.WriteLine(ex.Source);
-                return false;
-            }
-        }
+			{
+				DataRow newRow = table.NewRow();
+				newRow["ID"] = Guid.NewGuid();
+				newRow["Surname"] = mTeacher.Surname;
+				newRow["Name"] = mTeacher.Name;
+				newRow["MiddleName"] = mTeacher.MiddleName;
+				newRow["AcademicDegree"] = mTeacher.AcademicDegree;
+				newRow["AcademicRank"] = mTeacher.AcademicRank;
+				newRow["SizeOfTeachingExperience"] = mTeacher.SizeOfTeachingExperience;
+				table.Rows.Add(newRow);
+				return true;
+			}
+			catch (Exception ex)
+			{
+				Debug.WriteLine(ex.Source);
+				return false;
+			}
+		}
 
         public override bool Update(Model model)
         {
