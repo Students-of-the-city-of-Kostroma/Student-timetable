2aa8a6361b986c355cb4ff6cd4948012a600b9c4
unknown <you@example.com>
2019-04-07 01:49:24 +0300

    Unit-test

---
+++ UnitTestOfTimetableOfClasses/UT_Insert_CGroup.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LibOfTimetableOfClasses;
+
+
+namespace UnitTestOfTimetableOfClasses
+{
+	[TestClass]
+	public class UT_Insert_CGroup
+	{
+		[TestMethod]
+		public void Task_251_1() //пустая таблица
+		{
+			//arrange
+			CGroup.DataSet.Tables.Clear();
+			bool expected = true;
+			//act
+			MGroup gr = new MGroup("17-ИСбо-2а", 1, "ИСиТ", 1, 1, 0, 0, "Воскресенье");
+			CGroup cg = new CGroup();
+			bool actual = cg.Insert(gr);
+			//assert
+			Assert.AreEqual(expected, actual);
+		}
+
+
+		//[TestMethod]
+		//public void Task_251_2() //дуюлирование атрибута "Группа" остальные атрибуты раздичаются
+		//{
+		//	//arrange
+		//	CGroup.DataSet.Tables.Clear();
+		//	MGroup gr = new MGroup("17-ИСбо-2а", 1, "ИСиТ", 1, 1, 0, 0, "Воскресенье");
+		//	CGroup cg = new CGroup();
+		//	bool g = cg.Insert(gr);
+		//	bool expected = false;
+		//	//act
+		//	MGroup gr1 = new MGroup("17-ИСбо-2а", 2, "ИСиТД", 2, 2, 1, 1, "Суббота");
+		//	bool actual = cg.Insert(gr1);
+		//	//assert
+		//	Assert.AreEqual(expected, actual);
+		//}
+
+		[TestMethod]
+		public void Task_251_10() //добавление записи ктороая не дублирует ни один атрибут уже имеющейся записи 
+		{
+			//arrange
+			CGroup.DataSet.Tables.Clear();
+			MGroup gr = new MGroup("17-ЮФбо-2а", 1, "ИСиТ", 1, 1, 0, 0, "Воскресенье");
+			CGroup cg = new CGroup();
+			bool g = cg.Insert(gr);
+			bool expected = true;
+			//act
+			MGroup gr1 = new MGroup("17-ЮФбо-2б", 2, "ИСиТД", 2, 2, 1, 1, "Суббота");
+			bool actual = cg.Insert(gr1);
+			//assert
+			Assert.AreEqual(expected, actual);
+		}
+
+
+		[TestMethod]
+		public void Task_251_11() //полное дублирование всех атрибутов
+		{
+			//arrange
+			CGroup.DataSet.Tables.Clear();
+			MGroup gr = new MGroup("17-ТМбо-2а", 1, "ИСиТ", 1, 1, 0, 0, "Воскресенье");
+			CGroup cg = new CGroup();
+			bool g = cg.Insert(gr);
+			bool expected = false;
+			//act
+			MGroup gr1 = new MGroup("17-ТМбо-2а", 1, "ИСиТ", 1, 1, 0, 0, "Воскресенье");
+			bool actual = cg.Insert(gr1);
+			//assert
+			Assert.AreEqual(expected, actual);
+		}
+
+
+		[TestMethod]
+		public void Task_251_from_3_or_9() // дублирование всех атрибутов, кроме атрибута "Группа"
+		{
+			//arrange
+			CGroup.DataSet.Tables.Clear();
+			MGroup gr = new MGroup("17-ИДбо-2а", 1, "ИСиТ", 1, 1, 0, 0, "Воскресенье");
+			CGroup cg = new CGroup();
+			bool g = cg.Insert(gr);
+			bool expected = true;
+			//act
+			MGroup gr1 = new MGroup("17-ИДбо-2б", 1, "ИСиТ", 1, 1, 0, 0, "Воскресенье");
+			bool actual = cg.Insert(gr1);
+			//assert
+			Assert.AreEqual(expected, actual);
+		}
+	}
+}
