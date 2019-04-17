6c43e63a41f3b76b3d361d0fa8c0f5d12b8a17a6
Takunava <chezcy237@gmail.com>
2019-04-17 22:49:57 +0300

    commit#2

--- TimetableOfClasses/AddAcademicTitle.cs
+++ TimetableOfClasses/AddAcademicTitle.cs
@@ -14,7 +14,7 @@ namespace TimetableOfClasses
 {
 	public partial class AddAcademicTitle : Form
 	{
-		private MTitle Lehrer;
+		
 
 		public AddAcademicTitle()
 		{
@@ -24,8 +24,9 @@ namespace TimetableOfClasses
 		public AddAcademicTitle(MTitle mTitle)
 		{
 			InitializeComponent();
-			this.Text = "Изменение уч. звания";		
-
+			this.Text = "Изменение уч. звания";
+            this.btCreateAndClean.Visible = false;
+            this.btCreateAndClose.Text = "Изменить";
 			FullName.Text = mTitle.FullName;
 			Reduction.Text = mTitle.Reduction;
 
@@ -34,116 +35,21 @@ namespace TimetableOfClasses
 		private void btCreateAndClose_Click(object sender, EventArgs e)
 		{
 
-			if (Add())
-			{
-				Close();
+            if (isNumberDontContains(Reduction.Text) && (Reduction.Text.Length != 0) && (FullName.Text.Length != 0))
+            {
+                MTitle Title = new MTitle(FullName.Text, Reduction.Text);
+                Controllers.CTitle.Insert(Title);
+                FullName.Text = "";
+                Reduction.Text = "";
+                Close();
 			}
 			else MessageBox.Show("Новозможно добавить это уч. звание", "Попробуйте снова");
-		}
-
-
-		private bool Add()
-		{
-			if (Lehrer == null)
-			{
-				MTitle Title = new MTitle(FullName.Text, Reduction.Text);
-				return Controllers.CTitle.Insert(Title);
-			}
-			else
-			{
-				
-				Lehrer.FullName = FullName.Text;
-				Lehrer.Reduction = Reduction.Text;
-				return Controllers.CTeacher.Update(Lehrer);
-			}
-		}
-
-		/// <summary>
-		/// Здесь должны быть комментарии
-		/// </summary>
-		/// <param name="sender"></param>
-		/// <param name="e"></param>
-		private void AddTeacher_Load(object sender, EventArgs e)
-		{
-
-		}
-
-		private void SelectionOfLetters1(object sender, EventArgs e)
-		{
-			TextBox R = sender as TextBox;
-			R.Text = Regex.Replace(R.Text, "[^а-я]", "");
-			R.Text = Regex.Replace(R.Text, "[, ]+", ", ");
-			if (R.Text.Length > 2)
-			{
-				if (R.Text.IndexOf(", ") == 0)
-					R.Text = R.Text.Substring(1);
-				if (R.Text.LastIndexOf(", ") == R.Text.Length - 1)
-					R.Text = R.Text.Remove(R.Text.Length - 1);
-				R.Text = R.Text.ToLower();
-				R.Text = PeriodLetterToUpper(R.Text);
-			}
-
-		}
-
-		private void SelectionOfLetters2(object sender, EventArgs e)
-		{
-			TextBox R = sender as TextBox;
-			R.Text = Regex.Replace(R.Text, "[^а-яА-Я ]", "");
-			R.Text = Regex.Replace(R.Text, "[ ]+", " ");
-			if (R.Text.Length > 2)
-			{
-				if (R.Text.IndexOf(" ") == 0)
-					R.Text = R.Text.Substring(1);
-				if (R.Text.LastIndexOf(" ") == R.Text.Length - 1)
-					R.Text = R.Text.Remove(R.Text.Length - 1);
-				R.Text = R.Text.ToLower();
-				R.Text = FirstLetterToUpper(R.Text);
-			}
-		}
-
-		private static string FirstLetterToUpper(string str)
-		{
-			if (str.Length > 0)
-			{
-				if (str.IndexOf("-") > 0)
-				{
-					return Char.ToUpper(str[0]) + str.Substring(1, str.IndexOf("-")) + Char.ToUpper(str[str.IndexOf("-") + 1]) + str.Substring(str.IndexOf("-") + 2);
-				}
-				else
-					return Char.ToUpper(str[0]) + str.Substring(1);
-			}
-			return "";
-		}
-
-		private static string PeriodLetterToUpper(string str)
-		{
-			if (str.Length > 0)
-			{
-				if (str.IndexOf(",") > 0)
-				{
-					char p;
-					str = Char.ToUpper(str[0]) + str.Substring(1);
-					for (int i = 0; i < str.Length; i++)
-					{
-						if (str[i] == ',')
-						{
-							p = Char.ToUpper(str[i + 2]);
-							str = str.Remove(i + 2, 1);
-							str = str.Insert(i + 2, "" + p);
-						}
-					}
-					return str;
-				}
-				else
-					return Char.ToUpper(str[0]) + str.Substring(1);
-			}
-			return "";
 		}
 
 		private void btCreateAndClean_Click(object sender, EventArgs e)
 		{
 			
-			if ((Reduction.Text.Length != 0) && (FullName.Text.Length != 0))
+			if (isNumberDontContains(Reduction.Text) && (Reduction.Text.Length != 0) && (FullName.Text.Length != 0))
 			{
 				MTitle Title = new MTitle(FullName.Text, Reduction.Text);
 				Controllers.CTitle.Insert(Title);
@@ -157,5 +63,16 @@ namespace TimetableOfClasses
 		{
 			this.Close();
 		}
-	}
+
+
+        static bool isNumberDontContains(string input)
+        {
+            foreach (char c in input)
+                if (Char.IsNumber(c))
+                    return false;
+            return true;
+        }
+
+
+    }
 }
