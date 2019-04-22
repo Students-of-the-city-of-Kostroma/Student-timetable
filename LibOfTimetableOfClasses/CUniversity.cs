ecb4c896c5db96552ab8cabb5ef5052ab5c1d525
SergeAlekseev <serge.alekseev999@gmail.com>
2019-04-22 20:46:31 +0300

    Added CUniversity and MUniversity

---
+++ LibOfTimetableOfClasses/CUniversity.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibOfTimetableOfClasses
+{
+	public class CUniversity : DataTable, IController
+	{
+		public CUniversity() : base("ВУЗ")
+		{
+			DataColumn[] keys = new DataColumn[1];
+			DataColumn column = new DataColumn();
+			column.DataType = typeof(string);
+			column.ColumnName = "INN";
+			column.ReadOnly = true;
+			this.Columns.Add(column);
+			keys[0] = column;
+
+			column = new DataColumn();
+			column.DataType = typeof(string);
+			column.ColumnName = "ShortName";
+			column.ReadOnly = true;
+			this.Columns.Add(column);
+
+			column = new DataColumn();
+			column.DataType = typeof(string);
+			column.ColumnName = "FullName";
+			column.ReadOnly = false;
+			this.Columns.Add(column);
+
+			column = new DataColumn();
+			column.DataType = typeof(string);
+			column.ColumnName = "ActualAddress";
+			column.ReadOnly = false;
+			this.Columns.Add(column);
+
+			column = new DataColumn();
+			column.DataType = typeof(string);
+			column.ColumnName = "LegalAddress";
+			column.ReadOnly = false;
+			this.Columns.Add(column);
+
+			column = new DataColumn();
+			column.DataType = typeof(string);
+			column.ColumnName = "FullNameRector";
+			column.ReadOnly = false;
+			this.Columns.Add(column);
+
+			column = new DataColumn();
+			column.DataType = typeof(string);
+			column.ColumnName = "Email";
+			column.ReadOnly = false;
+			this.Columns.Add(column);
+
+			column = new DataColumn();
+			column.DataType = typeof(string);
+			column.ColumnName = "Phone";
+			column.ReadOnly = false;
+			this.Columns.Add(column);
+
+			this.PrimaryKey = keys;
+		}
+
+
+		public bool Delete(Model model)
+
+		{
+			MUniversity mUniversity = (MUniversity)model;
+			for (int i = 0; i < this.Rows.Count; i++)
+			{
+				if ((string)this.Rows[i]["INN"] == mUniversity.INN)
+				{
+					this.Rows[i].Delete();
+					return true;
+				}
+			}
+			return false;
+		}
+
+		private bool isValidKey(MUniversity mUniversity)
+		{
+			foreach (DataRow row in this.Rows)
+			{
+				if ((string)row["INN"] == mUniversity.INN)
+					return false;
+			}
+			return true;
+		}
+
+		public bool Insert(Model model)
+		{
+			MUniversity mUniversity = (MUniversity)model;
+
+			if (isValidKey(mUniversity))
+			{
+				try
+				{
+
+					DataRow newRow = this.NewRow();
+					newRow["INN"] = mUniversity.INN;
+					newRow["ShortName"] = mUniversity.ShortName;
+					newRow["FullName"] = mUniversity.FullName;
+					newRow["ActualAddress"] = mUniversity.ActualAddress;
+					newRow["LegalAddress"] = mUniversity.LegalAddress;
+					newRow["FullNameRector"] = mUniversity.FullName + "" + mUniversity.SurnameRector + "" + mUniversity.MiddleNameRector;
+					newRow["Email"] = mUniversity.Email;
+					newRow["Phone"] = mUniversity.Phone;
+					this.Rows.Add(newRow);
+					return true;
+				}
+				catch (Exception ex)
+				{
+					Debug.WriteLine(ex.Source);
+					return false;
+				}
+			}
+
+			return false;
+
+		}
+
+		public bool Update(Model model)
+		{
+			MUniversity mUniversity = (MUniversity)model;
+			for (int i = 0; i < this.Rows.Count; i++)
+			{
+				if ((string)this.Rows[i]["INN"] == mUniversity.INN)
+					try
+					{
+						DataRow newRow = this.Rows[i];
+						newRow["ShortName"] = mUniversity.ShortName;
+						newRow["FullName"] = mUniversity.FullName;
+						newRow["ActualAddress"] = mUniversity.ActualAddress;
+						newRow["LegalAddress"] = mUniversity.LegalAddress;
+						newRow["FullNameRector"] = mUniversity.FullName + "" + mUniversity.SurnameRector + "" + mUniversity.MiddleNameRector;
+						newRow["Email"] = mUniversity.Email;
+						newRow["Phone"] = mUniversity.Phone;
+						return true;
+					}
+					catch (Exception ex)
+					{
+						Debug.WriteLine(ex.Source);
+						return false;
+					}
+			}
+			return false;
+		}
+
+	}
+}
---
+++ LibOfTimetableOfClasses/MUniversity.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibOfTimetableOfClasses
+{
+	
+
+	/// <summary>
+	/// ВУЗ
+	/// </summary>
+	public class MUniversity : Model
+	{
+		string _inn;
+		string _shortName;
+		string _fullName;
+		string _actualAddress;
+		string _legalAddress;
+		string _nameRector;
+		string _surnameRector;
+		string _middleNameRector;
+		string _email;
+		string _phone;
+
+		public string INN
+		{
+			get
+			{
+				return _inn;
+			}
+			set
+			{
+				_inn = value;
+			}
+		}
+
+		public string ShortName
+		{
+			get
+			{
+				return _shortName;
+			}
+			set
+			{
+				_shortName = value;
+			}
+		}
+
+		public string FullName
+		{
+			get
+			{
+				return _fullName;
+			}
+			set
+			{
+				_fullName = value;
+			}
+		}
+		public string ActualAddress
+		{
+			get
+			{
+				return _actualAddress;
+			}
+			set
+			{
+				_actualAddress = value;
+			}
+		}
+		public string LegalAddress
+		{
+			get
+			{
+				return _legalAddress;
+			}
+			set
+			{
+				_legalAddress = value;
+			}
+		}
+
+		public string NameRector
+		{
+			get
+			{
+				return _nameRector;
+			}
+			set
+			{
+				_nameRector = value;
+			}
+		}
+
+		public string SurnameRector
+		{
+			get
+			{
+				return _surnameRector;
+			}
+			set
+			{
+				_surnameRector = value;
+			}
+		}
+
+		public string MiddleNameRector
+		{
+			get
+			{
+				return _middleNameRector;
+			}
+			set
+			{
+				_middleNameRector = value;
+			}
+		}
+		public string Email
+		{
+			get
+			{
+				return _email;
+			}
+			set
+			{
+				_email = value;
+			}
+		}
+		public string Phone
+		{
+			get
+			{
+				return _phone;
+			}
+			set
+			{
+				_phone = value;
+			}
+		}
+
+		/// <summary>
+		/// Создает экземпляр
+		/// </summary>
+		public MUniversity(string inn,string shortName,string fullName,string actualAddress,string legalAddress,string nameRector,string surnameRector,string middleNameRector,string email,string phone) : base()
+		{
+			INN = inn;
+			ShortName = shortName;
+			FullName = fullName;
+			ActualAddress = actualAddress;
+			LegalAddress = legalAddress;
+			NameRector = nameRector;
+			SurnameRector = surnameRector;
+			MiddleNameRector = middleNameRector;
+			Email = email;
+			Phone = phone;
+		}
+
+		public MUniversity(string inn) : base()
+		{
+			INN = inn;
+		}
+	}
+}
