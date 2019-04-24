241d419677e5035a24097a6b5c45eb51c95130c9
SamuraSwordS <misterdzej@gmail.com>
2019-04-24 19:51:49 +0300

    Commit|2|:Functional

--- TimetableOfClasses/University.Designer.cs
+++ TimetableOfClasses/University.Designer.cs
@@ -32,6 +32,7 @@
 			this.delete = new System.Windows.Forms.Button();
 			this.add = new System.Windows.Forms.Button();
 			this.update = new System.Windows.Forms.Button();
+			this.INN = new System.Windows.Forms.DataGridViewTextBoxColumn();
 			this.shortname = new System.Windows.Forms.DataGridViewTextBoxColumn();
 			this.fulltitle = new System.Windows.Forms.DataGridViewTextBoxColumn();
 			this.actualaddress = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -52,6 +53,7 @@
 			this.DG.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
 			this.DG.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
 			this.DG.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.INN,
             this.shortname,
             this.fulltitle,
             this.actualaddress,
@@ -66,6 +68,9 @@
 			this.DG.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
 			this.DG.Size = new System.Drawing.Size(700, 309);
 			this.DG.TabIndex = 0;
+			this.DG.ColumnHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.DG_ColumnHeaderMouseClick);
+			this.DG.DataBindingComplete += new System.Windows.Forms.DataGridViewBindingCompleteEventHandler(this.DG_DataBindingComplete);
+			this.DG.RowPrePaint += new System.Windows.Forms.DataGridViewRowPrePaintEventHandler(this.DG_RowPrePaint);
 			// 
 			// delete
 			// 
@@ -76,6 +81,7 @@
 			this.delete.TabIndex = 1;
 			this.delete.Text = "Удалить";
 			this.delete.UseVisualStyleBackColor = true;
+			this.delete.Click += new System.EventHandler(this.Delete);
 			// 
 			// add
 			// 
@@ -86,6 +92,7 @@
 			this.add.TabIndex = 2;
 			this.add.Text = "Добавить";
 			this.add.UseVisualStyleBackColor = true;
+			this.add.Click += new System.EventHandler(this.Add);
 			// 
 			// update
 			// 
@@ -96,45 +103,60 @@
 			this.update.TabIndex = 3;
 			this.update.Text = "Изменить";
 			this.update.UseVisualStyleBackColor = true;
+			this.update.Click += new System.EventHandler(this.Update);
+			// 
+			// INN
+			// 
+			this.INN.DataPropertyName = "INN";
+			this.INN.HeaderText = "ИНН";
+			this.INN.Name = "INN";
+			this.INN.ReadOnly = true;
 			// 
 			// shortname
 			// 
+			this.shortname.DataPropertyName = "ShortName";
 			this.shortname.HeaderText = "Краткое название";
 			this.shortname.Name = "shortname";
 			this.shortname.ReadOnly = true;
 			// 
 			// fulltitle
 			// 
+			this.fulltitle.DataPropertyName = "FullName";
 			this.fulltitle.HeaderText = "Полное название";
 			this.fulltitle.Name = "fulltitle";
 			this.fulltitle.ReadOnly = true;
 			// 
 			// actualaddress
 			// 
+			this.actualaddress.DataPropertyName = "ActualAddress";
 			this.actualaddress.HeaderText = "Фактический адрес";
 			this.actualaddress.Name = "actualaddress";
 			this.actualaddress.ReadOnly = true;
 			// 
 			// legaladdress
 			// 
+			this.legaladdress.DataPropertyName = "LegalAddress";
 			this.legaladdress.HeaderText = "Юридический адрес";
 			this.legaladdress.Name = "legaladdress";
 			this.legaladdress.ReadOnly = true;
 			// 
 			// namerector
 			// 
+			this.namerector.DataPropertyName = "FullNameRector";
 			this.namerector.HeaderText = "ФИО ректора";
 			this.namerector.Name = "namerector";
 			this.namerector.ReadOnly = true;
 			// 
 			// mail
 			// 
+			this.mail.DataPropertyName = "Email";
 			this.mail.HeaderText = "Почта";
 			this.mail.Name = "mail";
 			this.mail.ReadOnly = true;
 			// 
 			// phone
 			// 
+			this.phone.DataPropertyName = "Phone";
 			this.phone.HeaderText = "Телефон";
 			this.phone.Name = "phone";
 			this.phone.ReadOnly = true;
@@ -161,6 +183,7 @@
 		private System.Windows.Forms.Button delete;
 		private System.Windows.Forms.Button add;
 		private System.Windows.Forms.Button update;
+		private System.Windows.Forms.DataGridViewTextBoxColumn INN;
 		private System.Windows.Forms.DataGridViewTextBoxColumn shortname;
 		private System.Windows.Forms.DataGridViewTextBoxColumn fulltitle;
 		private System.Windows.Forms.DataGridViewTextBoxColumn actualaddress;
