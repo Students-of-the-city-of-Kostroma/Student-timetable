036a49b825e21dd9fed7e0ccbbaa7a063026ba5a
AndreiDevyaterikov <andreidevyaterikov@gmail.com>
2019-04-26 23:07:26 +0300

    Development of the button Add Forms Institutes

---
+++ TimetableOfClasses/AddInstitute.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace TimetableOfClasses
+{
+	partial class AddInstitute
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.ShortName = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.FullName = new System.Windows.Forms.TextBox();
+			this.Director = new System.Windows.Forms.TextBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.btCreateAndClose = new System.Windows.Forms.Button();
+			this.btCreateAndClean = new System.Windows.Forms.Button();
+			this.btCancel = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(13, 9);
+			this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(200, 17);
+			this.label1.TabIndex = 7;
+			this.label1.Text = "Краткое название института";
+			// 
+			// ShortName
+			// 
+			this.ShortName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.ShortName.Location = new System.Drawing.Point(13, 30);
+			this.ShortName.Margin = new System.Windows.Forms.Padding(4);
+			this.ShortName.Name = "ShortName";
+			this.ShortName.Size = new System.Drawing.Size(409, 22);
+			this.ShortName.TabIndex = 8;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(13, 56);
+			this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(195, 17);
+			this.label2.TabIndex = 9;
+			this.label2.Text = "Полное название института";
+			// 
+			// FullName
+			// 
+			this.FullName.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.FullName.Location = new System.Drawing.Point(13, 77);
+			this.FullName.Margin = new System.Windows.Forms.Padding(4);
+			this.FullName.Name = "FullName";
+			this.FullName.Size = new System.Drawing.Size(409, 22);
+			this.FullName.TabIndex = 10;
+			// 
+			// Director
+			// 
+			this.Director.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.Director.Location = new System.Drawing.Point(13, 124);
+			this.Director.Margin = new System.Windows.Forms.Padding(4);
+			this.Director.Name = "Director";
+			this.Director.Size = new System.Drawing.Size(409, 22);
+			this.Director.TabIndex = 11;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(13, 103);
+			this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(144, 17);
+			this.label3.TabIndex = 12;
+			this.label3.Text = "Директор института";
+			// 
+			// btCreateAndClose
+			// 
+			this.btCreateAndClose.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+			this.btCreateAndClose.Location = new System.Drawing.Point(533, 179);
+			this.btCreateAndClose.Margin = new System.Windows.Forms.Padding(4);
+			this.btCreateAndClose.Name = "btCreateAndClose";
+			this.btCreateAndClose.Size = new System.Drawing.Size(100, 46);
+			this.btCreateAndClose.TabIndex = 13;
+			this.btCreateAndClose.Text = "Создать и закрыть";
+			this.btCreateAndClose.UseVisualStyleBackColor = true;
+			this.btCreateAndClose.Click += new System.EventHandler(this.btCreateAndClose_Click);
+			// 
+			// btCreateAndClean
+			// 
+			this.btCreateAndClean.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+			this.btCreateAndClean.Location = new System.Drawing.Point(425, 179);
+			this.btCreateAndClean.Margin = new System.Windows.Forms.Padding(4);
+			this.btCreateAndClean.Name = "btCreateAndClean";
+			this.btCreateAndClean.Size = new System.Drawing.Size(100, 46);
+			this.btCreateAndClean.TabIndex = 14;
+			this.btCreateAndClean.Text = "Создать и очистить";
+			this.btCreateAndClean.UseVisualStyleBackColor = true;
+			this.btCreateAndClean.Click += new System.EventHandler(this.btCreateAndClean_Click);
+			// 
+			// btCancel
+			// 
+			this.btCancel.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+			this.btCancel.Location = new System.Drawing.Point(317, 179);
+			this.btCancel.Margin = new System.Windows.Forms.Padding(4);
+			this.btCancel.Name = "btCancel";
+			this.btCancel.Size = new System.Drawing.Size(100, 46);
+			this.btCancel.TabIndex = 15;
+			this.btCancel.Text = "Отмена";
+			this.btCancel.UseVisualStyleBackColor = true;
+			this.btCancel.Click += new System.EventHandler(this.btCancel_Click);
+			// 
+			// AddInstitute
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(646, 238);
+			this.Controls.Add(this.btCancel);
+			this.Controls.Add(this.btCreateAndClean);
+			this.Controls.Add(this.btCreateAndClose);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.Director);
+			this.Controls.Add(this.FullName);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.ShortName);
+			this.Controls.Add(this.label1);
+			this.Name = "AddInstitute";
+			this.Text = "AddInstitute";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox ShortName;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox FullName;
+		private System.Windows.Forms.TextBox Director;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Button btCreateAndClose;
+		private System.Windows.Forms.Button btCreateAndClean;
+		private System.Windows.Forms.Button btCancel;
+	}
+}
--- TimetableOfClasses/Institute.Designer.cs
+++ TimetableOfClasses/Institute.Designer.cs
@@ -28,68 +28,22 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.DG_Institute = new System.Windows.Forms.DataGridView();
-			this.ShortName = new System.Windows.Forms.DataGridViewTextBoxColumn();
-			this.FullName = new System.Windows.Forms.DataGridViewTextBoxColumn();
-			this.Director = new System.Windows.Forms.DataGridViewTextBoxColumn();
 			this.Add = new System.Windows.Forms.Button();
 			this.Change = new System.Windows.Forms.Button();
 			this.Delete = new System.Windows.Forms.Button();
+			this.DG_Institute = new System.Windows.Forms.DataGridView();
 			((System.ComponentModel.ISupportInitialize)(this.DG_Institute)).BeginInit();
 			this.SuspendLayout();
-			// 
-			// DG_Institute
-			// 
-			this.DG_Institute.AllowUserToAddRows = false;
-			this.DG_Institute.AllowUserToDeleteRows = false;
-			this.DG_Institute.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-			this.DG_Institute.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.ColumnHeader;
-			this.DG_Institute.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllHeaders;
-			this.DG_Institute.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-			this.DG_Institute.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.ShortName,
-            this.FullName,
-            this.Director});
-			this.DG_Institute.Location = new System.Drawing.Point(11, 11);
-			this.DG_Institute.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-			this.DG_Institute.Name = "DG_Institute";
-			this.DG_Institute.ReadOnly = true;
-			this.DG_Institute.RowTemplate.Height = 24;
-			this.DG_Institute.Size = new System.Drawing.Size(542, 219);
-			this.DG_Institute.TabIndex = 0;
-			// 
-			// ShortName
-			// 
-			this.ShortName.HeaderText = "Краткое название института";
-			this.ShortName.Name = "ShortName";
-			this.ShortName.ReadOnly = true;
-			this.ShortName.Width = 117;
-			// 
-			// FullName
-			// 
-			this.FullName.HeaderText = "Полное название интитута";
-			this.FullName.Name = "FullName";
-			this.FullName.ReadOnly = true;
-			this.FullName.Width = 114;
-			// 
-			// Director
-			// 
-			this.Director.HeaderText = "Директор института";
-			this.Director.Name = "Director";
-			this.Director.ReadOnly = true;
-			this.Director.Width = 123;
 			// 
 			// Add
 			// 
 			this.Add.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-			this.Add.Location = new System.Drawing.Point(467, 237);
-			this.Add.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.Add.Location = new System.Drawing.Point(623, 292);
+			this.Add.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
 			this.Add.Name = "Add";
-			this.Add.Size = new System.Drawing.Size(86, 32);
+			this.Add.Size = new System.Drawing.Size(115, 39);
 			this.Add.TabIndex = 1;
 			this.Add.Text = "Добавить";
 			this.Add.UseVisualStyleBackColor = true;
@@ -100,10 +54,10 @@
 			this.Change.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-			this.Change.Location = new System.Drawing.Point(377, 237);
-			this.Change.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.Change.Location = new System.Drawing.Point(503, 292);
+			this.Change.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
 			this.Change.Name = "Change";
-			this.Change.Size = new System.Drawing.Size(86, 32);
+			this.Change.Size = new System.Drawing.Size(115, 39);
 			this.Change.TabIndex = 2;
 			this.Change.Text = "Изменить";
 			this.Change.UseVisualStyleBackColor = true;
@@ -113,24 +67,40 @@
 			this.Delete.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-			this.Delete.Location = new System.Drawing.Point(287, 237);
-			this.Delete.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.Delete.Location = new System.Drawing.Point(383, 292);
+			this.Delete.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
 			this.Delete.Name = "Delete";
-			this.Delete.Size = new System.Drawing.Size(86, 32);
+			this.Delete.Size = new System.Drawing.Size(115, 39);
 			this.Delete.TabIndex = 3;
 			this.Delete.Text = "Удалить";
 			this.Delete.UseVisualStyleBackColor = true;
 			// 
+			// DG_Institute
+			// 
+			this.DG_Institute.AllowUserToAddRows = false;
+			this.DG_Institute.AllowUserToDeleteRows = false;
+			this.DG_Institute.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.DG_Institute.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+			this.DG_Institute.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.DG_Institute.Location = new System.Drawing.Point(13, 13);
+			this.DG_Institute.Margin = new System.Windows.Forms.Padding(4);
+			this.DG_Institute.Name = "DG_Institute";
+			this.DG_Institute.ReadOnly = true;
+			this.DG_Institute.Size = new System.Drawing.Size(726, 267);
+			this.DG_Institute.TabIndex = 4;
+			// 
 			// Institute
 			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(564, 280);
+			this.ClientSize = new System.Drawing.Size(752, 345);
+			this.Controls.Add(this.DG_Institute);
 			this.Controls.Add(this.Delete);
 			this.Controls.Add(this.Change);
 			this.Controls.Add(this.Add);
-			this.Controls.Add(this.DG_Institute);
-			this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
 			this.Name = "Institute";
 			this.Text = "Институт";
 			((System.ComponentModel.ISupportInitialize)(this.DG_Institute)).EndInit();
@@ -139,13 +109,9 @@
 		}
 
 		#endregion
-
-		private System.Windows.Forms.DataGridView DG_Institute;
 		private System.Windows.Forms.Button Add;
 		private System.Windows.Forms.Button Change;
 		private System.Windows.Forms.Button Delete;
-		private System.Windows.Forms.DataGridViewTextBoxColumn ShortName;
-		private System.Windows.Forms.DataGridViewTextBoxColumn FullName;
-		private System.Windows.Forms.DataGridViewTextBoxColumn Director;
+		private System.Windows.Forms.DataGridView DG_Institute;
 	}
 }
