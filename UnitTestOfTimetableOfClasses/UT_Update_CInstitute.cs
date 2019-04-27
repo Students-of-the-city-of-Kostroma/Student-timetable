08ddca5732de25066ff43d5f3c7fd30ccc12cdd9
nastyarabotko <nrabotko99@gmail.com>
2019-04-27 23:39:40 +0300

    task-513 add new UT_Update_CInstitute

---
+++ UnitTestOfTimetableOfClasses/UT_Update_CInstitute.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LibOfTimetableOfClasses;
+
+namespace UnitTestOfTimetableOfClasses
+{
+    [TestClass]
+    public class UT_Update_CInstitute
+    {
+        [TestMethod]
+        public void Task_513_1()
+        {
+            //arrange
+            MInstitute i = new MInstitute("Институт Автоматизированных систем и технологий", "ИАСТ", "Лустгартен Ю.Л.");
+            MInstitute i1 = new MInstitute("Факультет Автоматизированных систем и технологий", "ФАСТ", "Голубева Ю.А.");
+            CInstitute I = new CInstitute();
+            I.Insert(i);
+            I.Insert(i1);
+            bool ex = true;
+            //act
+            i1.fullname = "Институт Автоматизированных систем и технологий";
+            i1.shortname = "ИАСТ";
+            i1.director = "Лустгартен Ю.Л.";
+            bool act = I.Update(i1);
+            //assert
+            Assert.AreEqual(ex, act);
+
+        }
+    }
+}
