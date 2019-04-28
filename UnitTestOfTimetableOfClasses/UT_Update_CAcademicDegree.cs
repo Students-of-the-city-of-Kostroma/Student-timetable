8f3afca11fb6ecc4a2fbe8cba2d4e9d362ce4892
Mikleproxorov <mikle.proxorov.1985@gmail.com>
2019-04-28 22:49:29 +0300

    Added Autotest UT_Update_AcademicDegree

---
+++ UnitTestOfTimetableOfClasses/UT_Update_CAcademicDegree.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using LibOfTimetableOfClasses;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestOfTimetableOfClasses
+{
+	[TestClass]
+	public class UT_Update_CAcademicDegree
+	{
+		[TestMethod]
+		public void Task_484_1()//Замена корректными данными всех полей таблицы, не дублируя существующие
+		{
+			//arrange
+			MAcademicDegree MAcademic = new MAcademicDegree("Магистр", "Маг.");
+			MAcademicDegree MAcademic1 = new MAcademicDegree("Бакалавр", "");
+			CAcademicDegree CAcademic = new CAcademicDegree();
+			CAcademic.Insert(MAcademic);
+			CAcademic.Insert(MAcademic1);
+			bool ex = true;
+			//act
+			MAcademic1.FullName = "Аспирант";
+			bool act = CAcademic.Update(MAcademic1);
+			//assert
+			Assert.AreEqual(ex, act);
+		}
+
+	}
+}
