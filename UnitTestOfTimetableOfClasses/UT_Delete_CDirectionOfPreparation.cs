0783e936ebf2ad5025fd5a4b00111cda51f554f3
Zhezlova Daria <zh.daria4@gmail.com>
2019-05-01 02:08:32 +0300

    UT for MDoP and CDoP

---
+++ UnitTestOfTimetableOfClasses/UT_Delete_CDirectionOfPreparation.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LibOfTimetableOfClasses;
+
+namespace UnitTestOfTimetableOfClasses
+{
+	[TestClass]
+	public class UT_Delete_CDirectionOfPreparation
+	{
+		public void Pre_condition_Del()
+		{
+			Controllers.CDirectionOfPreparation.Clear();
+			bool ex = true;
+			bool act;
+			MDirectionOfPreparation T_DirectionOfPreparation = new MDirectionOfPreparation("01.02.03", "Филология",20);
+			int C1 = Controllers.CDirectionOfPreparation.Rows.Count;
+			act = Controllers.CDirectionOfPreparation.Insert(T_DirectionOfPreparation);
+			int C2 = Controllers.CDirectionOfPreparation.Rows.Count;
+			Assert.AreEqual(ex, act);
+			Assert.AreEqual(C1 + 1, C2);
+		}
+		[TestMethod]
+		public void Task_555_1()
+		{
+			Pre_condition_Del();
+			bool ex = true;
+			bool act;
+			MDirectionOfPreparation T_DirectionOfPreparation = new MDirectionOfPreparation("01.02.03", "Филология", 20);
+
+			int C1 = Controllers.CDirectionOfPreparation.Rows.Count;
+			act = Controllers.CDirectionOfPreparation.Delete(T_DirectionOfPreparation);
+			int C2 = Controllers.CDirectionOfPreparation.Rows.Count;
+			Assert.AreEqual(ex, act);
+			Assert.AreEqual(C1 - 1, C2);
+		}
+		[TestMethod]
+		public void Task_555_2()
+		{
+			bool ex = false;
+			bool act;
+			MDirectionOfPreparation T_DirectionOfPreparation = new MDirectionOfPreparation("01.02.03", "Филология", 20);
+			int C1 = Controllers.CDirectionOfPreparation.Rows.Count;
+			act = Controllers.CDirectionOfPreparation.Delete(T_DirectionOfPreparation);
+			int C2 = Controllers.CDirectionOfPreparation.Rows.Count;
+			Assert.AreEqual(ex, act);
+			Assert.AreEqual(C1, C2);
+		}
+	}
+}
