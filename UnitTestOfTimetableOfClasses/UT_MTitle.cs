b26e6572e8924ab91047c3fae0fc49ef3bd508c3
nastyarabotko <nrabotko99@gmail.com>
2019-05-13 01:03:25 +0300

    task 646 add UT_MTetle

---
+++ UnitTestOfTimetableOfClasses/UT_MTitle.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LibOfTimetableOfClasses;
+
+namespace UnitTestOfTimetableOfClasses
+{
+    [TestClass]
+    public class UT_MTitle
+    {
+        [TestMethod]
+        public void Issue_646_1()
+        {
+            try
+            {
+                MTitle T_Title = new MTitle("", "");
+                Assert.AreEqual(typeof(String), T_Title.FullName.GetType(), "Ожидался тип поля String");
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail(ex.Message);
+            }
+        }
+        [TestMethod]
+        public void Issue_646_2()
+        {
+            try
+            {
+                MTitle T_Title = new MTitle("", "");
+                Assert.AreEqual(typeof(String), T_Title.Reduction.GetType(), "Ожидался тип поля String");
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail(ex.Message);
+            }
+        }
+    }
+}
