01c42de253a0a424080e6399e720ea2bbadbacc6
Mihail <mischa.bobylev@gmail.com>
2019-05-13 22:46:27 +0300

    Переименовал поле Полное название профиля на Выбор профиля

--- TimetableOfClasses/AddProfile.Designer.cs
+++ TimetableOfClasses/AddProfile.Designer.cs
@@ -43,9 +43,9 @@
 			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
 			this.label1.Location = new System.Drawing.Point(13, 13);
 			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(186, 16);
+			this.label1.Size = new System.Drawing.Size(111, 16);
 			this.label1.TabIndex = 0;
-			this.label1.Text = "Полное название профиля";
+			this.label1.Text = "Выбор профиля";
 			// 
 			// label2
 			// 
