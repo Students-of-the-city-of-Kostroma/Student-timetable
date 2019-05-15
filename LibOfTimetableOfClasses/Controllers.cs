5a5604889ac9fae53e0bc492a3f507dba86668ca
denis <kapu4inko@gmail.com>
2019-05-15 19:17:02 +0300

    Добавление отношений в класс Controllers

--- LibOfTimetableOfClasses/Controllers.cs
+++ LibOfTimetableOfClasses/Controllers.cs
@@ -26,6 +26,9 @@ namespace LibOfTimetableOfClasses
 			dataSet.Tables.Add(CGroup);
 			dataSet.Tables.Add(CTrainingProfile);
 			dataSet.Relations.Add("Group-Training profile",CTrainingProfile.Columns["Shortname"], CGroup.Columns["Specialty"]);
+			dataSet.Tables.Add(СEnclosures);
+			dataSet.Tables.Add(CUniversity);
+			dataSet.Relations.Add("University-Enclosures", CUniversity.Columns["ShortName"], СEnclosures.Columns["University"]);
 		}
 	  }
 }
