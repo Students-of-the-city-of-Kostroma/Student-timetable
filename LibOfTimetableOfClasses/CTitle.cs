3ffaffa02aed42bd8c7643672ddeadcbac4980f5
AndreiDevyaterikov <andreidevyaterikov@gmail.com>
2019-05-16 10:54:57 +0300

    In the  book Academic titles there can not be several entries with the same field-Full entry record. titles

--- LibOfTimetableOfClasses/CTitle.cs
+++ LibOfTimetableOfClasses/CTitle.cs
@@ -27,11 +27,11 @@ namespace LibOfTimetableOfClasses
 
 
 			column = new DataColumn();
-            column.DataType = typeof(string);
-            column.ColumnName = "Полная запись уч. звания";
-            column.Unique = true;
-            table.Columns.Add(column);
-            keys[1] = column;
+			column.DataType = typeof(string);
+			column.ColumnName = "Полная запись уч. звания";
+			column.Unique = true;
+			table.Columns.Add(column);
+			keys[1] = column;
 		}
 
 		public override bool Delete(Model model)
