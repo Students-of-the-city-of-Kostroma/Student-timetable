415064728a4927604587beb8f1a30edc9701cf0b
AndreiDevyaterikov <andreidevyaterikov@gmail.com>
2019-05-18 01:21:05 +0300

    Fixed crash when deleting entries in AcademicTitle

--- LibOfTimetableOfClasses/MTitle.cs
+++ LibOfTimetableOfClasses/MTitle.cs
@@ -2,8 +2,8 @@
 	using System.Collections.Generic;
 	using System.Linq;
 	using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
+	using System.Text.RegularExpressions;
+	using System.Threading.Tasks;
 
 	namespace LibOfTimetableOfClasses
 	{
