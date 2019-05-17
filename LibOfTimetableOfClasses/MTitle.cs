543e6dbfbbf69ae98bc4d82671d0c86d6236e4b7
AndreiDevyaterikov <andreidevyaterikov@gmail.com>
2019-05-18 00:22:08 +0300

    Tab fixed

--- LibOfTimetableOfClasses/MTitle.cs
+++ LibOfTimetableOfClasses/MTitle.cs
@@ -4,59 +4,59 @@
 	using System.Text;
 	using System.Threading.Tasks;
 
-	namespace LibOfTimetableOfClasses
+namespace LibOfTimetableOfClasses
+{
+
+	/// <summary>
+	/// Уч. звание
+	/// </summary>
+	public class MTitle : Model
 	{
 
 		/// <summary>
-		/// Уч. звание
+		/// Полная запись ученого звания
 		/// </summary>
-		public class MTitle : Model
+		string _fullname;
+
+		/// <summary>
+		/// Сокращенная запись ученого звания
+		/// </summary>
+		string _reduction;
+
+
+		public string FullName
 		{
-
-			/// <summary>
-			/// Полная запись ученого звания
-			/// </summary>
-			string _fullname;
-
-			/// <summary>
-			/// Сокращенная запись ученого звания
-			/// </summary>
-			string _reduction;
-		
-
-			public string FullName
+			get
 			{
-				get
-				{
-					return _fullname;
-				}
-				set
-				{
+				return _fullname;
+			}
+			set
+			{
 				if (value[0] == '.') throw new Exception("Точка ставится после слова");
 				_fullname = value;
-				}
-			}
-
-			public string Reduction
-			{
-				get
-				{
-					return _reduction;
-				}
-				set
-				{
-				if (value[0] == '.') throw new Exception("Точка ставится после слова");
-				_reduction = value;
-				}
-			}
-
-			/// <summary>
-			/// Создание экземпляра уч. звания
-			/// </summary>
-			public MTitle(string fullname, string reduction) : base()
-			{
-				FullName = fullname;
-				Reduction = reduction;
 			}
 		}
+
+		public string Reduction
+		{
+			get
+			{
+				return _reduction;
+			}
+			set
+			{
+				if (value[0] == '.') throw new Exception("Точка ставится после слова");
+				_reduction = value;
+			}
+		}
+
+		/// <summary>
+		/// Создание экземпляра уч. звания
+		/// </summary>
+		public MTitle(string fullname, string reduction) : base()
+		{
+			FullName = fullname;
+			Reduction = reduction;
+		}
 	}
+}
