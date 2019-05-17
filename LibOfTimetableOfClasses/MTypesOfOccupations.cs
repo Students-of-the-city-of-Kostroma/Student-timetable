dea2a0226c4b55164a05cd900def342c2031e9e2
Mari Nazarova <nazarova.maria.uni@gmail.com>
2019-05-17 12:14:48 +0300

    Мнгоно всего

---
+++ LibOfTimetableOfClasses/MTypesOfOccupations.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibOfTimetableOfClasses
+{
+    /// <summary>
+    /// Вид занятий
+    /// </summary>
+    public class MTypesOfOccupations : Model
+    {
+        string _fullName;
+        string _shortName;
+
+        public string FullName
+        {
+            get
+            {
+                return _fullName;
+            }
+            set
+            {
+                _fullName = value;
+            }
+        }
+
+        public string ShortName
+        {
+            get
+            {
+                return _shortName;
+            }
+            set
+            {
+                _shortName = value;
+            }
+        }
+
+        public MTypesOfOccupations(string fullname, string shortname) : base()
+        {
+            FullName = fullname;
+            ShortName = shortname;
+        }
+    }
+}
--- TimetableOfClasses/TypesOfOccupations.cs
+++ TimetableOfClasses/TypesOfOccupations.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using LibOfTimetableOfClasses;
 
 namespace TimetableOfClasses
 {
@@ -15,6 +16,47 @@ namespace TimetableOfClasses
 		public TypesOfOccupations()
 		{
 			InitializeComponent();
-		}
-	}
+            DG_TypesOfOccupations.AutoGenerateColumns = false;
+            DG_TypesOfOccupations.DataSource = Controllers.CDiscipline;
+        }
+
+                private void DG_TypesOfOccupations_SelectionChanged(object sender, EventArgs e)
+        {
+            if (DG_TypesOfOccupations.SelectedCells.Count > 0)
+            {
+                btDelete.Enabled = true;
+				btChange.Enabled = true;
+			}
+            else
+            {
+                btDelete.Enabled = false;
+				btChange.Enabled = false;
+			}
+        }
+
+        private void btDelete_Click(object sender, EventArgs e)
+        {
+            string SelectedName = "";
+            foreach (DataGridViewRow row in DG_TypesOfOccupations.SelectedRows)
+            {
+                DataRow Row = ((DataRowView)row.DataBoundItem).Row;
+                SelectedName += (string)Row["FullName"] + ", ";
+            }
+            if (SelectedName.Length > 2)
+                SelectedName = SelectedName.Remove(SelectedName.Length - 2);
+            DialogResult dr = MessageBox.Show("Удалить дисциплину - " + SelectedName + "?", "Подтверждение", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+            if (DG_TypesOfOccupations.SelectedRows.Count > 0 && dr == DialogResult.OK)
+            {
+                int countSelected = DG_TypesOfOccupations.SelectedRows.Count;
+
+                MTypesOfOccupations mTypesOfOccupations;
+                foreach (DataGridViewRow row in DG_TypesOfOccupations.SelectedRows)
+                {
+                    DataRow Row = ((DataRowView)row.DataBoundItem).Row;
+                    mTypesOfOccupations = new MTypesOfOccupations((string)Row["FullName"], (string)Row["ShortName"]);
+                    Controllers.CTypesOfOccupations.Delete(mTypesOfOccupations); 
+                }
+            }
+        }
+    }
 }
