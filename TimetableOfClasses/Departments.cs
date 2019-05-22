440f10621eb9ff3ce1fb6a11d27cce6884a5c22d
sidelong44 <sid44@list.ru>
2019-05-22 23:45:24 +0300

    Добавлен раздел "Кафедры" в выпадающем списке "Справочник"

---
+++ TimetableOfClasses/Departments.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TimetableOfClasses {
+	public partial class Departments: Form {
+		public Departments() {
+			InitializeComponent();
+		}
+
+		private void FormLoad(object sender, EventArgs e) {
+			// Do
+		}
+
+		private void DeleteBtnClick(object sender, EventArgs e) {
+			// Do
+		}
+
+		private void UpdateBtnClick(object sender, EventArgs e) {
+			// Do
+		}
+
+		private void AddBtnClick(object sender, EventArgs e) {
+			// Do
+		}
+	}
+}
