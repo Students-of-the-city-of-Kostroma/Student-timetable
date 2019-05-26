ef0e78c88963089d11f471e86ea2fb12f92023c1
AndreiDevyaterikov <andreidevytaerikov@gmail.com>
2019-05-26 17:09:23 +0300

    Заблокировано масштабирование окна

--- TimetableOfClasses/AddAcademicTitle.Designer.cs
+++ TimetableOfClasses/AddAcademicTitle.Designer.cs
@@ -42,10 +42,13 @@
 			// 
 			// label1
 			// 
+			this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
 			this.label1.AutoSize = true;
-			this.label1.Location = new System.Drawing.Point(10, 7);
+			this.label1.Location = new System.Drawing.Point(13, 9);
+			this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
 			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(171, 13);
+			this.label1.Size = new System.Drawing.Size(221, 17);
 			this.label1.TabIndex = 6;
 			this.label1.Text = "Сокращенная запись уч. звания";
 			// 
@@ -53,40 +56,46 @@
 			// 
 			this.Reduction.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-			this.Reduction.Location = new System.Drawing.Point(10, 24);
+			this.Reduction.Location = new System.Drawing.Point(13, 30);
+			this.Reduction.Margin = new System.Windows.Forms.Padding(4);
 			this.Reduction.Name = "Reduction";
-			this.Reduction.Size = new System.Drawing.Size(302, 20);
+			this.Reduction.Size = new System.Drawing.Size(397, 22);
 			this.Reduction.TabIndex = 7;
 			this.Reduction.TextChanged += new System.EventHandler(this.Reduction_TextChanged);
 			this.Reduction.Validating += new System.ComponentModel.CancelEventHandler(this.Reduction_Validating);
 			// 
 			// label2
 			// 
+			this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
 			this.label2.AutoSize = true;
-			this.label2.Location = new System.Drawing.Point(10, 80);
+			this.label2.Location = new System.Drawing.Point(13, 56);
+			this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
 			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(139, 13);
+			this.label2.Size = new System.Drawing.Size(180, 17);
 			this.label2.TabIndex = 8;
 			this.label2.Text = "Полная запись уч. звания";
 			// 
 			// FullName
 			// 
-			this.FullName.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
+			this.FullName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-			this.FullName.Location = new System.Drawing.Point(10, 98);
+			this.FullName.Location = new System.Drawing.Point(13, 77);
+			this.FullName.Margin = new System.Windows.Forms.Padding(4);
 			this.FullName.Name = "FullName";
-			this.FullName.Size = new System.Drawing.Size(302, 20);
+			this.FullName.Size = new System.Drawing.Size(397, 22);
 			this.FullName.TabIndex = 9;
 			this.FullName.TextChanged += new System.EventHandler(this.FullName_TextChanged);
 			this.FullName.Validating += new System.ComponentModel.CancelEventHandler(this.FullName_Validating);
 			// 
 			// btCancel
 			// 
-			this.btCancel.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
-			this.btCancel.Location = new System.Drawing.Point(67, 142);
+			this.btCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.btCancel.Location = new System.Drawing.Point(246, 191);
+			this.btCancel.Margin = new System.Windows.Forms.Padding(4);
 			this.btCancel.Name = "btCancel";
-			this.btCancel.Size = new System.Drawing.Size(75, 37);
+			this.btCancel.Size = new System.Drawing.Size(100, 46);
 			this.btCancel.TabIndex = 10;
 			this.btCancel.Text = "Отмена";
 			this.btCancel.UseVisualStyleBackColor = true;
@@ -94,10 +103,11 @@
 			// 
 			// btCreateAndClean
 			// 
-			this.btCreateAndClean.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
-			this.btCreateAndClean.Location = new System.Drawing.Point(151, 142);
+			this.btCreateAndClean.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.btCreateAndClean.Location = new System.Drawing.Point(354, 191);
+			this.btCreateAndClean.Margin = new System.Windows.Forms.Padding(4);
 			this.btCreateAndClean.Name = "btCreateAndClean";
-			this.btCreateAndClean.Size = new System.Drawing.Size(75, 37);
+			this.btCreateAndClean.Size = new System.Drawing.Size(100, 46);
 			this.btCreateAndClean.TabIndex = 11;
 			this.btCreateAndClean.Text = "Создать и очистить";
 			this.btCreateAndClean.UseVisualStyleBackColor = true;
@@ -105,10 +115,11 @@
 			// 
 			// btCreateAndClose
 			// 
-			this.btCreateAndClose.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
-			this.btCreateAndClose.Location = new System.Drawing.Point(232, 142);
+			this.btCreateAndClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.btCreateAndClose.Location = new System.Drawing.Point(462, 191);
+			this.btCreateAndClose.Margin = new System.Windows.Forms.Padding(4);
 			this.btCreateAndClose.Name = "btCreateAndClose";
-			this.btCreateAndClose.Size = new System.Drawing.Size(75, 37);
+			this.btCreateAndClose.Size = new System.Drawing.Size(100, 46);
 			this.btCreateAndClose.TabIndex = 12;
 			this.btCreateAndClose.Text = "Создать и закрыть";
 			this.btCreateAndClose.UseVisualStyleBackColor = true;
@@ -120,9 +131,9 @@
 			// 
 			// AddAcademicTitle
 			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(434, 190);
+			this.ClientSize = new System.Drawing.Size(575, 250);
 			this.Controls.Add(this.btCreateAndClose);
 			this.Controls.Add(this.btCreateAndClean);
 			this.Controls.Add(this.btCancel);
@@ -130,12 +141,14 @@
 			this.Controls.Add(this.label2);
 			this.Controls.Add(this.Reduction);
 			this.Controls.Add(this.label1);
-			this.Margin = new System.Windows.Forms.Padding(2);
+			this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
 			this.Name = "AddAcademicTitle";
 			this.Text = "Добавление уч. звания";
 			((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
 			this.ResumeLayout(false);
 			this.PerformLayout();
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+
 
 		}
 
