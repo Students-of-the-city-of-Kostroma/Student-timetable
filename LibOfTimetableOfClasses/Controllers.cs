28d22f0531ca3e282e6ea3c6770328c57cf6bef5
DemiK0r <demikor97@gmail.com>
2019-06-01 21:19:53 +0300

    Вернул имя в первоначальный вид

--- LibOfTimetableOfClasses/Controllers.cs
+++ LibOfTimetableOfClasses/Controllers.cs
@@ -12,7 +12,7 @@ namespace LibOfTimetableOfClasses
 	/// <summary>
 	/// В этом классе храняться все экземпляры контроллеров.
 	/// </summary>
-	public class RefDataController
+	public class Controllers
 	{
 		public static CTeacher CTeacher = new CTeacher();
 		public static CStudyWeek CStudyWeek = new CStudyWeek();
@@ -27,7 +27,7 @@ namespace LibOfTimetableOfClasses
 		public static CAcademicDegree CAcademicDegree = new CAcademicDegree();
 		public static DataSet dataSet = new DataSet();
 
-		public RefDataController()
+		public Controllers()
 		{
 			dataSet.Tables.Add(CUniversity);
 			dataSet.Tables.Add(CInstitute);
