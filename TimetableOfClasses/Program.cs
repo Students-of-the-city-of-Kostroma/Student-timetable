0534aa299aa0d61c4cded6f997740e80f2d470f0
DemiK0r <demikor97@gmail.com>
2019-06-09 18:55:15 +0300

    fix bugs

--- TimetableOfClasses/Program.cs
+++ TimetableOfClasses/Program.cs
@@ -4,16 +4,16 @@ using System.Windows.Forms;
 namespace TimetableOfClasses
 {
 	static class Program
-    {
-        /// <summary>
-        /// Главная точка входа для приложения.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {		
-            Application.EnableVisualStyles();
+	{
+		/// <summary>
+		/// Главная точка входа для приложения.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Start());
-        }
-    }
+			Application.Run(new Start());
+		}
+	}
 }
