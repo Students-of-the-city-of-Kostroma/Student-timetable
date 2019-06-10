1e81e26118a2cf20b4b42d1bc6e0febb6f9a71a2
bormolei <Oxitxmq5q2>
2019-06-10 12:27:00 +0300

    Rework UT

--- UnitTestOfTimetableOfClasses/UT_Delete_CEnclosures.cs
+++ UnitTestOfTimetableOfClasses/UT_Delete_CEnclosures.cs
@@ -10,9 +10,15 @@ namespace UnitTestOfTimetableOfClasses
 		[TestMethod]
 		public void task_395_1()
 		{
-			// arrange
-			RefData.СEnclosures.Rows.Clear();
-			MEnclosures gr = new MEnclosures("А", "Костромской Государственный Университет", "Дзержинского", "111111", "1");
+
+            RefData.CUniversity.Rows.Clear();
+            MUniversity grUni = new MUniversity("4401006286", "КГУ", "Костромкой Государственный Университет", "156005, Костромская область, г. Кострома, ул. Дзержинского, 17", "156005, Костромская область, г. Кострома, ул. Дзержинского, 17", "Александр", "Наумов", "Рудольфович", "kgu@mail.ru", "84942317960");
+            bool actualUni = RefData.CUniversity.Insert(grUni);
+
+            Assert.AreEqual(true, actualUni);
+            // arrange
+            RefData.СEnclosures.Rows.Clear();
+			MEnclosures gr = new MEnclosures("А", grUni.FullName , "Дзержинского", "111111", "1");
 			bool expected = true;
 			//act 
 			RefData.СEnclosures.Insert(gr);
