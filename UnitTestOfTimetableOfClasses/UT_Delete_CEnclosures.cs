f228a7d4aaa57587d9a9ab4212002d846e50ce3e
Mari Nazarova <nazarova.maria.uni@gmail.com>
2019-06-14 22:53:36 +0300

    Исправлены сбои в автотестах расположенных в файле UT_Delete_CEnclosures

--- UnitTestOfTimetableOfClasses/UT_Delete_CEnclosures.cs
+++ UnitTestOfTimetableOfClasses/UT_Delete_CEnclosures.cs
@@ -10,14 +10,14 @@ namespace UnitTestOfTimetableOfClasses
 		[TestMethod]
 		public void task_395_1()
 		{
-            RefData.CUniversity.Rows.Clear();
-            MUniversity grUni = new MUniversity("4401006286", "КГУ", "Костромкой Государственный Университет", "156005, Костромская область, г. Кострома, ул. Дзержинского, 17", "156005, Костромская область, г. Кострома, ул. Дзержинского, 17", "Александр", "Наумов", "Рудольфович", "kgu@mail.ru", "84942317960");
-            bool actualUni = RefData.CUniversity.Insert(grUni);
-            Assert.AreEqual(true, actualUni);
-            // arrange 
-            RefData.СEnclosures.Rows.Clear();
-            MEnclosures gr = new MEnclosures("А", grUni.FullName, "Дзержинского", "111111", "1");
-            bool expected = true;
+			RefData.CUniversity.Rows.Clear();
+			MUniversity grUni = new MUniversity("4401006286", "КГУ", "Костромкой Государственный Университет", "156005, Костромская область, г. Кострома, ул. Дзержинского, 17", "156005, Костромская область, г. Кострома, ул. Дзержинского, 17", "Александр", "Наумов", "Рудольфович", "kgu@mail.ru", "84942317960");
+			bool actualUni = RefData.CUniversity.Insert(grUni);
+			Assert.AreEqual(true, actualUni);
+			// arrange 
+			RefData.СEnclosures.Rows.Clear();
+			MEnclosures gr = new MEnclosures("А", grUni.FullName, "Дзержинского", "111111", "1");
+			bool expected = true;
 			//act 
 			RefData.СEnclosures.Insert(gr);
 			bool actual = RefData.СEnclosures.Delete(gr);
