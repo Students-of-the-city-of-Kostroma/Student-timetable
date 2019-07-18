ac55d4f43833a10a55c5cbfc0d7f62a6717e807a
DariaMann <dasaman60@gmail.com>
2019-07-18 18:13:10 +0300

    first

--- TimetableOfClasses/AddStudyWeek.Designer.cs
+++ TimetableOfClasses/AddStudyWeek.Designer.cs
@@ -28,100 +28,100 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.createAndClose = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.txtNumber = new System.Windows.Forms.TextBox();
-            this.txtName = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.B_Сancel = new System.Windows.Forms.Button();
-            this.SuspendLayout();
-            // 
-            // createAndClose
-            // 
-            this.createAndClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.createAndClose.Location = new System.Drawing.Point(203, 145);
-            this.createAndClose.Name = "createAndClose";
-            this.createAndClose.Size = new System.Drawing.Size(75, 34);
-            this.createAndClose.TabIndex = 0;
-            this.createAndClose.Text = "Сохранить";
-            this.createAndClose.UseVisualStyleBackColor = true;
-            this.createAndClose.Click += new System.EventHandler(this.createAndClose_Click);
-            // 
-            // label1
-            // 
-            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+			this.createAndClose = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.txtNumber = new System.Windows.Forms.TextBox();
+			this.txtName = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.B_Сancel = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// createAndClose
+			// 
+			this.createAndClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.createAndClose.Location = new System.Drawing.Point(228, 118);
+			this.createAndClose.Name = "createAndClose";
+			this.createAndClose.Size = new System.Drawing.Size(75, 34);
+			this.createAndClose.TabIndex = 0;
+			this.createAndClose.Text = "Сохранить";
+			this.createAndClose.UseVisualStyleBackColor = true;
+			this.createAndClose.Click += new System.EventHandler(this.createAndClose_Click);
+			// 
+			// label1
+			// 
+			this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(12, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(51, 16);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Номер";
-            // 
-            // txtNumber
-            // 
-            this.txtNumber.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label1.Location = new System.Drawing.Point(12, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(51, 16);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Номер";
+			// 
+			// txtNumber
+			// 
+			this.txtNumber.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.txtNumber.Location = new System.Drawing.Point(12, 28);
-            this.txtNumber.MaxLength = 25;
-            this.txtNumber.Name = "txtNumber";
-            this.txtNumber.Size = new System.Drawing.Size(266, 22);
-            this.txtNumber.TabIndex = 2;
-            // 
-            // txtName
-            // 
-            this.txtName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+			this.txtNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.txtNumber.Location = new System.Drawing.Point(12, 28);
+			this.txtNumber.MaxLength = 25;
+			this.txtNumber.Name = "txtNumber";
+			this.txtNumber.Size = new System.Drawing.Size(291, 22);
+			this.txtNumber.TabIndex = 2;
+			// 
+			// txtName
+			// 
+			this.txtName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.txtName.Location = new System.Drawing.Point(12, 84);
-            this.txtName.MaxLength = 50;
-            this.txtName.Name = "txtName";
-            this.txtName.Size = new System.Drawing.Size(266, 22);
-            this.txtName.TabIndex = 4;
-            // 
-            // label2
-            // 
-            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+			this.txtName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.txtName.Location = new System.Drawing.Point(12, 84);
+			this.txtName.MaxLength = 50;
+			this.txtName.Name = "txtName";
+			this.txtName.Size = new System.Drawing.Size(291, 22);
+			this.txtName.TabIndex = 4;
+			// 
+			// label2
+			// 
+			this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.Location = new System.Drawing.Point(12, 65);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(107, 16);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "Наименование";
-            // 
-            // B_Сancel
-            // 
-            this.B_Сancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.B_Сancel.Location = new System.Drawing.Point(127, 145);
-            this.B_Сancel.Name = "B_Сancel";
-            this.B_Сancel.Size = new System.Drawing.Size(70, 34);
-            this.B_Сancel.TabIndex = 17;
-            this.B_Сancel.Text = "Отмена";
-            this.B_Сancel.UseVisualStyleBackColor = true;
-            this.B_Сancel.Click += new System.EventHandler(this.B_Сancel_Click);
-            // 
-            // AddStudyWeek
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.AutoScroll = true;
-            this.ClientSize = new System.Drawing.Size(290, 191);
-            this.Controls.Add(this.B_Сancel);
-            this.Controls.Add(this.txtName);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.txtNumber);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.createAndClose);
-            this.KeyPreview = true;
-            this.MinimumSize = new System.Drawing.Size(249, 230);
-            this.Name = "AddStudyWeek";
-            this.Text = "Добавить преподавателя";
-            this.ResumeLayout(false);
-            this.PerformLayout();
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label2.Location = new System.Drawing.Point(12, 65);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(107, 16);
+			this.label2.TabIndex = 3;
+			this.label2.Text = "Наименование";
+			// 
+			// B_Сancel
+			// 
+			this.B_Сancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.B_Сancel.Location = new System.Drawing.Point(152, 118);
+			this.B_Сancel.Name = "B_Сancel";
+			this.B_Сancel.Size = new System.Drawing.Size(70, 34);
+			this.B_Сancel.TabIndex = 17;
+			this.B_Сancel.Text = "Отмена";
+			this.B_Сancel.UseVisualStyleBackColor = true;
+			this.B_Сancel.Click += new System.EventHandler(this.B_Сancel_Click);
+			// 
+			// AddStudyWeek
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.AutoScroll = true;
+			this.ClientSize = new System.Drawing.Size(315, 164);
+			this.Controls.Add(this.B_Сancel);
+			this.Controls.Add(this.txtName);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.txtNumber);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.createAndClose);
+			this.KeyPreview = true;
+			this.MinimumSize = new System.Drawing.Size(331, 203);
+			this.Name = "AddStudyWeek";
+			this.Text = "Добавить преподавателя";
+			this.ResumeLayout(false);
+			this.PerformLayout();
 
         }
 
