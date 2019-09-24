64b6df3f50c064981db2f0572f86582b05f5244a
JennyShel18 <EvgeniaShelkunova00@gmail.com>
2019-09-24 23:41:55 +0400

    Changes3withtab

--- UnitTestOfTimetableOfClasses/UT_Deleted_CTeacher.cs
+++ UnitTestOfTimetableOfClasses/UT_Deleted_CTeacher.cs
@@ -4,18 +4,18 @@ using LibOfTimetableOfClasses;
 
 namespace UnitTestOfTimetableOfClasses
 {
-	[TestClass]
-	public class UT_Deleted_CTeacher
-	{
+    [TestClass]
+    public class UT_Deleted_CTeacher
+    {
         /// <summary>
         /// Удаление существующей строки
         /// </summary>
 		[TestMethod]
-		public void Task_248_1()
-		{
-			//arrange 
-			MTeacher tcher = new MTeacher("Садовская", "Ольга", "Борисовна", "КН", "Проф", "ФАСТ", "Пн, Вт", "Ср, Чт, Пт", "Воскресенье");
-			bool expected = true;
+        public void Task_248_1()
+        {
+            //arrange 
+            MTeacher tcher = new MTeacher("Садовская", "Ольга", "Борисовна", "КН", "Проф", "ФАСТ", "Пн, Вт", "Ср, Чт, Пт", "Воскресенье");
+            bool expected = true;
             bool expectedInsert = true;
             //act 
             bool actualInsert = RefData.CTeacher.Insert(tcher);
@@ -24,22 +24,22 @@ namespace UnitTestOfTimetableOfClasses
             //assert 
 
             Assert.AreEqual(expected, actual);
-		}
+        }
         /// <summary>
         /// Удаление не существующей строки 
         /// </summary>
 		[TestMethod]
-		public void Task_248_2()
-		{
-			//arrange 
-			MTeacher tcher = new MTeacher("Садовская", "Ольга", "Борисовна", "КН", "Проф", "ФАСТ", "Пн, Вт", "Ср, Чт, Пт", "Воскресенье");
-			bool expected = false;
-			//act 
-			bool actual = RefData.CTeacher.Delete(tcher);
+        public void Task_248_2()
+        {
+            //arrange 
+            MTeacher tcher = new MTeacher("Садовская", "Ольга", "Борисовна", "КН", "Проф", "ФАСТ", "Пн, Вт", "Ср, Чт, Пт", "Воскресенье");
+            bool expected = false;
+            //act 
+            bool actual = RefData.CTeacher.Delete(tcher);
             //assert 
-            
+
 
             Assert.AreEqual(expected, actual);
-		}
-	}
+        }
+    }
 }
