e130429d1e5f2f82244c9cb9ea7ab8d4cbc0f9d6
Proffet78 <valerimash78@gmail.com>
2019-09-25 12:57:19 +0300

    fixed ut

--- UnitTestOfTimetableOfClasses/UT_Update_CEnclosurse.cs
+++ UnitTestOfTimetableOfClasses/UT_Update_CEnclosurse.cs
@@ -7,12 +7,16 @@ namespace UnitTestOfTimetableOfClasses
     [TestClass]
     public class UT_Update_CEnclosurse
     {
+        /// <summary>
+        /// Ввод коректных данных, при условии, что они не дублируют данные других экземпляров (кроме основных полей)
+        /// </summary>
         [TestMethod]
         public void task_396_1()
         {
             //arrange
             MEnclosures gr = new MEnclosures("А", "Костромской Государственный Университет", "Дзержинского", "111111", "1");
-            RefData.CEnclosures.Insert(gr);
+            bool result = RefData.CEnclosures.Insert(gr);
+            Assert.IsTrue(result);
             bool expected = true;
             //act
             gr.Address = "Ивановская";
@@ -21,51 +25,84 @@ namespace UnitTestOfTimetableOfClasses
             bool actual = RefData.CEnclosures.Update(gr);
             //assert
             Assert.AreEqual(expected, actual);
+            //cleare data
+            result = RefData.CEnclosures.Delete(gr);
+            Assert.IsTrue(result);
         }
+        /// <summary>
+        /// Ввод корректных данных, при условии, что Адрес дублирует Адрес существующего экземпляра(не учитывая основные атрибуты)
+        /// </summary>
         [TestMethod]
         public void task_396_2()// повтор адреса
         {
             //arrange
             MEnclosures gr = new MEnclosures("А", "Костромской Государственный Университет", "Дзержинского", "111111", "1");
-            RefData.CEnclosures.Insert(gr);
+            bool result = RefData.CEnclosures.Insert(gr);
+            Assert.IsTrue(result);
             bool expected = false;
             //act
             MEnclosures gr1 = new MEnclosures("Б", "Костромской Государственный Университет", "Ивановская", "222222", "2");
-            RefData.CEnclosures.Insert(gr1);
+            result = RefData.CEnclosures.Insert(gr1);
+            Assert.IsTrue(result);
             gr.Address = "Ивановская";
             bool actual = RefData.CEnclosures.Update(gr);
             //assert
             Assert.AreEqual(expected, actual);
+            //cleare data
+            result = RefData.CEnclosures.Delete(gr);
+            Assert.IsTrue(result);
+            result = RefData.CEnclosures.Delete(gr1);
+            Assert.IsTrue(result);
         }
+        /// <summary>
+        /// Ввод корректных данных, при условии, что Телефон дублирует Телефон существующего экземпляра(не учитывая основные атрибуты)
+        /// </summary>
         [TestMethod]
         public void task_396_3()// повтор телефона
         {
             //arrange
             MEnclosures gr = new MEnclosures("А", "Костромской Государственный Университет", "Дзержинского", "111111", "1");
-            RefData.CEnclosures.Insert(gr);
+            bool result = RefData.CEnclosures.Insert(gr);
+            Assert.IsTrue(result);
             bool expected = false;
             //act
             MEnclosures gr1 = new MEnclosures("Б", "Костромской Государственный Университет", "Ивановская", "222222", "2");
-            RefData.CEnclosures.Insert(gr1);
+            result = RefData.CEnclosures.Insert(gr1);
+            Assert.IsTrue(result);
             gr.Phone = "222222";
             bool actual = RefData.CEnclosures.Update(gr);
             //assert
             Assert.AreEqual(expected, actual);
+            //cleare data
+            result = RefData.CEnclosures.Delete(gr);
+            Assert.IsTrue(result);
+            result = RefData.CEnclosures.Delete(gr1);
+            Assert.IsTrue(result);
         }
+        /// <summary>
+        /// Ввод корректных данных, при условии, что Примечание дублирует Примечание существующего экземпляра(не учитывая основные атрибуты)
+        /// </summary>
         [TestMethod]
         public void task_396_4()// повтор примечания
         {
             //arrange
             MEnclosures gr = new MEnclosures("А", "Костромской Государственный Университет", "Дзержинского", "111111", "1");
-            RefData.CEnclosures.Insert(gr);
+            bool result = RefData.CEnclosures.Insert(gr);
+            Assert.IsTrue(result);
             bool expected = true;
             //act
             MEnclosures gr1 = new MEnclosures("Б", "Костромской Государственный Университет", "Ивановская", "222222", "2");
-            RefData.CEnclosures.Insert(gr1);
+            result = RefData.CEnclosures.Insert(gr1);
+            Assert.IsTrue(result);
             gr.Comment = "2";
             bool actual = RefData.CEnclosures.Update(gr);
             //assert
             Assert.AreEqual(expected, actual);
+            //cleare data
+            result = RefData.CEnclosures.Delete(gr);
+            Assert.IsTrue(result);
+            result = RefData.CEnclosures.Delete(gr1);
+            Assert.IsTrue(result);
         }
     }
 }
