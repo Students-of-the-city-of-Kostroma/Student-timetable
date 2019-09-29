eeabfd5055e912b1d93a1bd69090739a13c3f584
NesgovorovVS <treft304@gmail.com>
2019-09-30 01:18:46 +0300

    Попытка 2

--- UnitTestOfTimetableOfClasses/UT_Delete_CGroup.cs
+++ UnitTestOfTimetableOfClasses/UT_Delete_CGroup.cs
@@ -4,49 +4,50 @@ using LibOfTimetableOfClasses;
 
 namespace UnitTestOfTimetableOfClasses
 {
-	[TestClass]
-	public class UT_Delete_CGroup
-	{
-		/// <summary>
-		/// Удаление существующей строки 
-		/// </summary>
-		[TestMethod]
+    [TestClass]
+    public class UT_Delete_CGroup
+    {
+        /// <summary>
+        /// Удаление существующей строки 
+        /// </summary>
+        [TestMethod]
         public void Task_248_1()
         {
             //arrange 
-            var dp = new MDirectionOfPreparation("11.11.11", "й", 1);
+            MDirectionOfPreparation dp = new MDirectionOfPreparation("11.11.11", "й", 1);
             MTrainingProfile tp = new MTrainingProfile("ИСиТa", "ИСиТ", "11.11.11");
-            bool expected = NewMethod();
-            //act 
-            bool actual = RefData.CDirectionOfPreparation.Insert(dp);
+            MGroup gr = new MGroup("17-ИСбо-2а", 1, "ИСиТ", 1, 1, 0, 0, "Воскресенье");
+            bool expected = true;
+            //act
+            bool actual;
+            actual = RefData.CDirectionOfPreparation.Insert(dp);
             Assert.AreEqual(expected, actual);
             actual = RefData.CTrainingProfile.Insert(tp);
             Assert.AreEqual(expected, actual);
-            MGroup gr = new MGroup("17-ИСбо-2а", 1, "ИСиТ", 1, 1, 0, 0, "Воскресенье");
+
+            expected = false;
             actual = RefData.CGroup.Insert(gr);
-            Assert.AreEqual(expected, actual=true);
+            Assert.AreEqual(expected, actual);
+
+            expected = true;
             actual = RefData.CGroup.Delete(gr);
             Assert.AreEqual(expected, actual);
-        }
 
-        private static bool NewMethod()
-        {
-            return true;
         }
-
         /// <summary>
         /// Удаление не существующей строки 
         /// </summary>
         [TestMethod]
-		 public void Task_248_2()
-		{
-			//arrange 
-			MGroup gr = new MGroup("17-ИСбо-2а", 1, "ИСиТ", 1, 1, 0, 0, "Воскресенье");
-			bool expected = true;
-			//act 
-			bool actual = RefData.CGroup.Delete(gr);
-			//assert 
-			Assert.AreEqual(expected, actual = true );
-		}
-	}
+        public void Task_248_2()
+        {
+            //arrange 
+            bool expected = false;
+            MGroup gr = new MGroup("17-ИСбо-2а", 1, "ИСиТ", 1, 1, 0, 0, "Воскресенье");
+           
+            //act 
+            bool actual = RefData.CGroup.Delete(gr);
+            //assert 
+            Assert.AreEqual(expected, actual);
+        }
+    }
 }
