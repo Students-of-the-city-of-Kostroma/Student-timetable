a82e529280e530087ad73ea1e92bc658050fcd7d
Proffet78 <valerimash78@gmail.com>
2019-10-08 00:16:20 +0300

    fixed ut 4.0

--- UnitTestOfTimetableOfClasses/UT_Update_CEnclosurse.cs
+++ UnitTestOfTimetableOfClasses/UT_Update_CEnclosurse.cs
@@ -16,6 +16,7 @@ namespace UnitTestOfTimetableOfClasses
             MUniversity university = new MUniversity("4401026216", "МГУ", "Московский Государственный Университет", "156005, Московская область, г. Москва, ул. Дзержинского, 17", "156005, Московская область, г. Москва, ул. Дзержинского, 17", "Александр", "Наумов", "Рудольфович", "mgu@mail.ru", "84942217960");
             bool actualUni = RefData.CUniversity.Insert(university);
             Assert.AreEqual(true, actualUni);
+
             //arrange
             MEnclosures gr = new MEnclosures("Ж", "Московский Государственный Университет", "Дзержинского", "111111", "1");
             bool result = RefData.CEnclosures.Insert(gr);
