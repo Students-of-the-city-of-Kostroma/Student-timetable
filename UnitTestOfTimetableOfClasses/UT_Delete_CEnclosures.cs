4e308d918d07677c7aee542bad8d8b85e0a95667
Виноградов Александр <ifuwmnn@gmail.com>
2019-11-28 18:20:42 +0300

    Update UT_Delete_CEnclosures.cs

--- UnitTestOfTimetableOfClasses/UT_Delete_CEnclosures.cs
+++ UnitTestOfTimetableOfClasses/UT_Delete_CEnclosures.cs
@@ -12,8 +12,11 @@ namespace UnitTestOfTimetableOfClasses
         [TestMethod]
         public void Task_395_1()
         {
+           MUniversity grUni = new MUniversity("4401006286", "КГУ", "Костромкой Государственный Университет", "156005, Костромская область, г. Кострома, ул. Дзержинского, 17", "156005, Костромская область, г. Кострома, ул. Дзержинского, 17", "Александр", "Наумов", "Рудольфович", "kgu@mail.ru", "84942317960");
+            bool actualUni = RefData.CUniversity.Insert(grUni);
+            Assert.AreEqual(true, actualUni);
             // arrange 
-            MEnclosures gr = new MEnclosures("ч", "Котромской Государственный Университет", "Дзержиского", "111112", "2");
+            MEnclosures gr = new MEnclosures("А", grUni.FullName, "Дзержинского", "111111", "1");
             bool expected = true;
             //act 
             RefData.CEnclosures.Insert(gr);
