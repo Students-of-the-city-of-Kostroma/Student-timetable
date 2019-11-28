1e02120d6140047e7e7a5b1bd7e841d7045ab855
Vladimir Nesgovorov <treft304@gmail.com>
2019-11-28 17:05:57 +0300

    Update UT_Delete_CGroup.cs

--- UnitTestOfTimetableOfClasses/UT_Delete_CGroup.cs
+++ UnitTestOfTimetableOfClasses/UT_Delete_CGroup.cs
@@ -10,17 +10,14 @@ namespace UnitTestOfTimetableOfClasses
         /// Удаление существующей строки 
         /// </summary>
         [TestMethod]
- task-1065
         public void Task_249_1()
         public void Task_248_1()
- Elite
         {
             //arrange 
             MDirectionOfPreparation dp = new MDirectionOfPreparation("11.11.11", "й", 1);
             MTrainingProfile tp = new MTrainingProfile("ИСиТa", "ИСиТ", "11.11.11");
             MGroup gr = new MGroup("17-ИСбо-2а", 1, "ИСиТ", 1, 1, 0, 0, "Воскресенье");
             bool expected = true;
- task-1065
             //act
             bool actual;
             actual = RefData.CDirectionOfPreparation.Insert(dp);
@@ -45,13 +42,11 @@ namespace UnitTestOfTimetableOfClasses
             Assert.AreEqual(expected, actual);
             actual = RefData.CGroup.Delete(gr);
             Assert.AreEqual(expected, actual);
- Elite
         }
         /// <summary>
         /// Удаление не существующей строки 
         /// </summary>
         [TestMethod]
- task-1065
         public void Task_249_2()
         {
             //arrange 
@@ -63,15 +58,12 @@ namespace UnitTestOfTimetableOfClasses
             //arrange 
             MGroup gr = new MGroup("17-ИСбо-2а", 1, "ИСиТ", 1, 1, 0, 0, "Воскресенье");
             bool expected = false;
- Elite
             //act 
             bool actual = RefData.CGroup.Delete(gr);
             //assert 
             Assert.AreEqual(expected, actual);
         }
     }
- task-1065
 }
 
 }
- Elite
