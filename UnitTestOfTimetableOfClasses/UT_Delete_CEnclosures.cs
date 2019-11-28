50a138d3473dcc10575e5fe67a3071bd26f6304e
Виноградов Александр <ifuwmnn@gmail.com>
2019-11-28 18:26:57 +0300

    Update UT_Delete_CEnclosures.cs

--- UnitTestOfTimetableOfClasses/UT_Delete_CEnclosures.cs
+++ UnitTestOfTimetableOfClasses/UT_Delete_CEnclosures.cs
@@ -13,16 +13,16 @@ namespace UnitTestOfTimetableOfClasses
         public void Task_395_1()
         {
            MUniversity grUni = new MUniversity("4401006286", "КГУ", "Костромкой Государственный Университет", "156005, Костромская область, г. Кострома, ул. Дзержинского, 17", "156005, Костромская область, г. Кострома, ул. Дзержинского, 17", "Александр", "Наумов", "Рудольфович", "kgu@mail.ru", "84942317960");
-            bool actualUni = RefData.CUniversity.Insert(grUni);
-            Assert.AreEqual(true, actualUni);
-            // arrange 
-            MEnclosures gr = new MEnclosures("А", grUni.FullName, "Дзержинского", "111111", "1");
-            bool expected = true;
-            //act 
-            RefData.CEnclosures.Insert(gr);
-            bool actual = RefData.CEnclosures.Delete(gr);
-            //assert 
-            Assert.AreEqual(expected, actual);
+			bool actualUni = RefData.CUniversity.Insert(grUni);
+			Assert.AreEqual(true, actualUni);
+			// arrange 
+			MEnclosures gr = new MEnclosures("А", grUni.FullName, "Дзержинского", "111111", "1");
+			bool expected = true;
+			//act 
+			RefData.CEnclosures.Insert(gr);
+			bool actual = RefData.CEnclosures.Delete(gr);
+			//assert 
+			Assert.AreEqual(expected, actual);
         }
         /// <summary>
         /// Удаление данных из пустой таблицы
@@ -32,12 +32,12 @@ namespace UnitTestOfTimetableOfClasses
         public void Task_395_2()
         {
            //arrange 
-            MEnclosures gr = new MEnclosures("р", "Косстромской Государственный Университет", "Дзержиннского", "111121", "2");
-            bool expected = false;
-            //act 
-            bool actual = RefData.CEnclosures.Delete(gr);
-            //assert 
-            Assert.AreEqual(expected, actual);
+			MEnclosures gr = new MEnclosures("А", "Костромской Государственный Университет", "Дзержинского", "111111", "1");
+			bool expected = false;
+			//act 
+			bool actual = RefData.CEnclosures.Delete(gr);
+			//assert 
+			Assert.AreEqual(expected, actual);
         }
     }
 }
