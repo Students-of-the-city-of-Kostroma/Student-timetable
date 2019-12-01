fd49b9baf9b8837a5382a9dcad48b70ff7e1eca8
Виноградов Александр <ifuwmnn@gmail.com>
2019-12-01 16:48:27 +0300

    Update UT_Delete_CEnclosures.cs

--- UnitTestOfTimetableOfClasses/UT_Delete_CEnclosures.cs
+++ UnitTestOfTimetableOfClasses/UT_Delete_CEnclosures.cs
@@ -10,16 +10,14 @@ namespace UnitTestOfTimetableOfClasses
         /// Удаление существующих данных выбранной строки из таблицы
         /// </summary>
         [TestMethod]
-        public void Task_395_1()
-        {
-// arrange 
+       public void Task_395_1()
+		{
             MEnclosures gr = new MEnclosures("Ж", "Костромской Государственный Университет", "Дзержинского", "111111", "1");
-            bool expected = true;
             //act 
             RefData.CEnclosures.Insert(gr);
-            bool actual = RefData.CEnclosures.Delete(gr);
+            bool result = RefData.CEnclosures.Insert(gr);
             //assert 
-            Assert.AreEqual(expected, actual);
+            Assert.AreEqual(true, RefData.CEnclosures.Delete(gr));
         }
         /// <summary>
         /// Удаление данных из пустой таблицы
