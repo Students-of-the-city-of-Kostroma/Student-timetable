0c9e2e8720fbeaf5fbe11eebe47f414a14747176
Nosolev <nosolev.alexandr@gmail.com>
2019-12-02 23:16:44 +0300

    Добавил файл с тестами изменения нагрузки

---
+++ UnitTestOfTimetableOfClasses/UT_Update_CAcademicLoad.cs
@@ -0,0 +1,27 @@
+﻿using LibOfTimetableOfClasses;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestOfTimetableOfClasses
+{
+    [TestClass]
+    public class UT_Update_CAcademicLoad
+    {
+        /// <summary>
+        /// Ввод коректных данных
+        /// </summary>
+        [TestMethod]
+        public void Task_1245_1()
+        {
+            //arrange
+            MAcademicLoad PreMa = new MAcademicLoad("17-ИСбо-2а", "100", "Правоведение", "Иванов Иван Иванович", "Лекция", "20");
+            bool actualPreMa = RefData.CAcademicLoad.Insert(PreMa);
+            Assert.AreEqual(true, actualPreMa);
+            //bool expected = true;
+            //act
+            //PreMa.Discipline = "Операционные системы";
+            //bool actual = RefData.CAcademicLoad.Update(PreMa);
+            //assert
+            //Assert.AreEqual(expected, actual);
+        }
+    }
+}
