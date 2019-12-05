fb63a264bbf75248b45f0f0b6ea4f20517a1507d
NesgovorovVS <treft304@gmail.com>
2019-12-05 23:31:57 +0300

    Автоматизация сценария

---
+++ UnitTestOfTimetableOfClasses/UT_Insert_CAdemicLoad.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Diagnostics;
+using LibOfTimetableOfClasses;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestOfTimetableOfClasses
+{
+    [TestClass]
+    public class UT_Insert_CAcademicLoad
+    {
+        /// <summary>
+        /// Ввод корректных данных в пустую таблицу
+        /// </summary>
+        [TestMethod]
+        public void Task_1240_1()
+        {
+            //arrange 
+            bool ex = true;
+            //act
+            MAcademicLoad ma = new MAcademicLoad("ВТ", "48", "Физкультура", "Иванов", "Леция", "20");
+            CAcademicLoad ca = new CAcademicLoad();
+            bool act = ca.Insert(ma);
+            //assert
+            Assert.AreEqual(ex, act);
+        }
+        /// <summary>
+        ///Ввод корректных данных в таблицу, при условии, что добавляемая запись не имеет аналогов в таблице
+        /// </summary>
+        [TestMethod]
+        public void Task_1240_2()
+        {
+            //arrange
+            Task_1240_1();
+            MAcademicLoad ma = new MAcademicLoad("ВТ", "36", "Физика", "Иванова", "Практика", "40");
+            bool expected = false;
+            //act
+            bool actual = RefData.CAcademicLoad.Insert(ma);
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+        /// <summary>
+        /// Ввод корректных данных в таблицу, при условии, что преподаватель с такой фамилией уже есть в таблице
+        /// </summary>
+        [TestMethod]
+        public void Task_1240_3()
+        {
+            //arrange 
+            MAcademicLoad ma = new MAcademicLoad("ВТ", "48", "Физкультура", "Иванов", "Леция", "20");
+            bool ex = true;
+            //act
+            bool act = RefData.CAcademicLoad.Insert(ma);
+            //assert
+            Assert.AreEqual(ex, act);
+        }
+        /// <summary>
+        /// Ввод корректных данных в таблицу, при условии, что дисциплина уже есть в таблице
+        /// </summary>
+        [TestMethod]
+        public void Task_1240_4()
+        {
+            //arrange 
+            MAcademicLoad ma = new MAcademicLoad("ВТ", "48", "Физкультура", "Иванов", "Леция", "20");
+            bool ex = true;
+            //act
+            bool act = RefData.CAcademicLoad.Insert(ma);
+            //assert
+            Assert.AreEqual(ex, act);
+        }
+        /// <summary>
+        /// Ввод корректных данных в таблицу, при условии, что группа уже есть в таблице
+        /// </summary>
+        [TestMethod]
+        public void Task_1240_5()
+        {
+            //arrange 
+            MAcademicLoad ma = new MAcademicLoad("ВТ", "48", "Физкультура", "Иванов", "Леция", "20");
+            bool ex = true;
+            //act
+            bool act = RefData.CAcademicLoad.Insert(ma);
+            //assert
+            Assert.AreEqual(ex, act);
+        }
+    }
+
+}
