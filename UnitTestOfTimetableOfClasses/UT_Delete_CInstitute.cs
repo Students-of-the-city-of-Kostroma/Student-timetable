7457852be9eb8c9c95c67da8ab10ab94ef17160f
badsster <zavyalova2019@gmail.com>
2020-03-03 14:48:28 +0300

    Исправлены сбои в автотестах

--- UnitTestOfTimetableOfClasses/UT_Delete_CInstitute.cs
+++ UnitTestOfTimetableOfClasses/UT_Delete_CInstitute.cs
@@ -8,67 +8,78 @@ namespace UnitTestOfTimetableOfClasses
     [TestClass]
     public class UT_Delete_CInstitute
     {
-        [TestMethod]
-        public void Task_617_1() //Удалить запись при совпадении всех атрибутов
+		/// <summary>
+		/// Удалить запись при совпадении всех атрибутов
+		/// </summary>
+		[TestMethod]
+        public void Task_617_1()
         {
-
-            MInstitute I_IInstitute = new MInstitute("Институт автоматизированных систем и технологий", "ИАСТ", "Лустгартен Ю.Л.", "Костромской Государственный Университет");
+			MInstitute I_IInstitute = new MInstitute("Институт автоматизированных систем и технологий", "ИАСТ", "Иванов Иван Иванович", "Костромской Государственный Университет");
             bool ex = true;
-            RefData.CInstitute.Insert(I_IInstitute);
             bool act = RefData.CInstitute.Delete(I_IInstitute);
 
             Assert.AreEqual(ex, act);
         }
-
-        [TestMethod]
-        public void Task_617_2() //Удалить запись при совпадении атрибута Краткое название
+		/// <summary>
+		/// Удалить запись при совпадении атрибута Краткое название
+		/// </summary>
+		[TestMethod]
+        public void Task_617_2() 
         {
             //act    
-            MInstitute I_IInstitute = new MInstitute("Любой институт", "ИАСТ", "Иванов Ю.Л.", "Политехнический колледж");
+            MInstitute I_IInstitute = new MInstitute("Любой институт", "ИАСТ", "Иванов Юрий Иванович", "Политехнический колледж");
             bool ex = false;
             bool act = RefData.CInstitute.Delete(I_IInstitute);
             //assert    
             Assert.AreEqual(ex, act);
         }
-
-        [TestMethod]
-        public void Task_617_3() //при совпадении атрибута Полное название
+		/// <summary>
+		/// При совпадении атрибута Полное название
+		/// </summary>
+		[TestMethod]
+        public void Task_617_3() 
         {
             //act    
-            MInstitute I_IInstitute = new MInstitute("Институт автоматизированных систем и технологий", "ФАСТ", "Иванов Ю.Л.", "Политехнический колледж");
+            MInstitute I_IInstitute = new MInstitute("Институт автоматизированных систем и технологий", "ФАСТ", "Иванов Юрий Иванович", "Политехнический колледж");
             bool ex = false;
             bool act = RefData.CInstitute.Delete(I_IInstitute);
             //assert    
             Assert.AreEqual(ex, act);
         }
-
-        [TestMethod]
-        public void Task_617_4() //при совпадении атрибута  Директор
+		/// <summary>
+		/// При совпадении атрибута  Директор
+		/// </summary>
+		[TestMethod]
+        public void Task_617_4() 
         {
             //act    
-            MInstitute I_IInstitute = new MInstitute("Любой институт", "ФАСТ", "Иванов Ю.Л.", "Политехнический колледж");
+            MInstitute I_IInstitute = new MInstitute("Любой институт", "ФАСТ", "Иванов Иван Иванович", "Политехнический колледж");
             bool ex = false;
             bool act = RefData.CInstitute.Delete(I_IInstitute);
             //assert    
             Assert.AreEqual(ex, act);
         }
-
-        [TestMethod]
-        public void Task_617_5() //при совпадении атрибута Наименование ВУЗа
+		/// <summary>
+		/// При совпадении атрибута Наименование ВУЗа
+		/// </summary>
+		[TestMethod]
+        public void Task_617_5() 
         {
             //act    
-            MInstitute I_IInstitute = new MInstitute("Любой институт", "ФАСТ", "Иванов Ю.Л.", "Костромской Государственный Университет");
+            MInstitute I_IInstitute = new MInstitute("Любой институт", "ФАСТ", "Иванов Юрий Иванович", "Костромской Государственный Университет");
             bool ex = false;
             bool act = RefData.CInstitute.Delete(I_IInstitute);
             //assert    
             Assert.AreEqual(ex, act);
         }
-
-        [TestMethod]
-        public void Task_617_6() //Удаление несуществующих данных из таблицы
+		/// <summary>
+		/// Удаление несуществующих данных из таблицы
+		/// </summary>
+		[TestMethod]
+        public void Task_617_6() 
         {
             //act    
-            MInstitute I_IInstitute = new MInstitute("Любой институт", "ФАСТ", "Иванов Ю.Л.", "Политехнический колледж");
+            MInstitute I_IInstitute = new MInstitute("Любой институт", "ФАСТ", "Иванов Юрий Иванович", "Политехнический колледж");
             bool ex = false;
             bool act = RefData.CInstitute.Delete(I_IInstitute);
             //assert    
