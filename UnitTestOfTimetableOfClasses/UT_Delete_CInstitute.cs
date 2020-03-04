bb85c20a542d7b5d98cfc0eb03b5701551605666
Юрий Силенок <yuri.silenok@gmail.com>
2020-03-04 16:26:50 +0300

    Update UT_Delete_CInstitute.cs

--- UnitTestOfTimetableOfClasses/UT_Delete_CInstitute.cs
+++ UnitTestOfTimetableOfClasses/UT_Delete_CInstitute.cs
@@ -14,7 +14,7 @@ namespace UnitTestOfTimetableOfClasses
 	[TestMethod]
         public void Task_617_1()
         {
-		MInstitute I_IInstitute = new MInstitute("Институт автоматизированных систем и технологий", "TEST", "Иванов Иван Иванович", "Костромской Государственный Университет");
+		MInstitute I_IInstitute = new MInstitute("Институт автоматизированных систем и технологий", "ТЕСТ", "Иванов Иван Иванович", "Костромской Государственный Университет");
 		Assert.IsTrue(RefData.CInstitute.Insert(I_IInstitute));
 		Assert.IsTrue(RefData.CInstitute.Delete(I_IInstitute));
         }
