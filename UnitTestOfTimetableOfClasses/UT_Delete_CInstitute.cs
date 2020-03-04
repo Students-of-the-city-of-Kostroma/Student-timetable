31ed684a838198f710cf78c2e196a22ff07e2e25
Юрий Силенок <yuri.silenok@gmail.com>
2020-03-04 16:17:52 +0300

    Update UT_Delete_CInstitute.cs

--- UnitTestOfTimetableOfClasses/UT_Delete_CInstitute.cs
+++ UnitTestOfTimetableOfClasses/UT_Delete_CInstitute.cs
@@ -8,35 +8,32 @@ namespace UnitTestOfTimetableOfClasses
     [TestClass]
     public class UT_Delete_CInstitute
     {
-		/// <summary>
-		/// Удалить запись при совпадении всех атрибутов
-		/// </summary>
-		[TestMethod]
+	/// <summary>
+	/// Удалить запись при совпадении всех атрибутов
+	/// </summary>
+	[TestMethod]
         public void Task_617_1()
         {
-			MInstitute I_IInstitute = new MInstitute("Институт автоматизированных систем и технологий", "ИАСТ", "Иванов Иван Иванович", "Костромской Государственный Университет");
-            bool ex = true;
-            bool act = RefData.CInstitute.Delete(I_IInstitute);
-
-            Assert.AreEqual(ex, act);
+		MInstitute I_IInstitute = new MInstitute("Любой институт", "ФАСТ", "Иванов Иван Иванович", "Политехнический колледж");
+		Assert.IsTrue(RefData.CInstitute.Insert(I_IInstitute));
+		Assert.IsTrue(RefData.CInstitute.Delete(I_IInstitute));
         }
-		/// <summary>
-		/// Удалить запись при совпадении атрибута Краткое название
-		/// </summary>
-		[TestMethod]
+	/// <summary>
+	/// Удалить запись при совпадении атрибута Краткое название
+	/// </summary>
+	[TestMethod]
         public void Task_617_2() 
         {
-            //act    
-            MInstitute I_IInstitute = new MInstitute("Любой институт", "ИАСТ", "Иванов Юрий Иванович", "Политехнический колледж");
-            bool ex = false;
-            bool act = RefData.CInstitute.Delete(I_IInstitute);
-            //assert    
-            Assert.AreEqual(ex, act);
+		MInstitute I_IInstitute = new MInstitute("Любой институт", "ФАСТ", "Иванов Иван Иванович", "Политехнический колледж");
+		Assert.IsTrue(RefData.CInstitute.Insert(I_IInstitute));
+		MInstitute I_IInstitute2 = new MInstitute("1", "ФАСТ", "1", "1");
+		Assert.IsFalse(RefData.CInstitute.Delete(I_IInstitute2));
+		Assert.IsTrue(RefData.CInstitute.Delete(I_IInstitute));
         }
-		/// <summary>
-		/// При совпадении атрибута Полное название
-		/// </summary>
-		[TestMethod]
+	/// <summary>
+	/// При совпадении атрибута Полное название
+	/// </summary>
+	[TestMethod]
         public void Task_617_3() 
         {
             //act    
