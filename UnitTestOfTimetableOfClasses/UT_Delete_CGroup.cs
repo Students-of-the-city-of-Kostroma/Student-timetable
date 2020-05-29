07652eb9c7412a72cb16a365c3a5804e8f7eefcf
NesgovorovVS <treft304@gmail.com>
2020-05-29 21:39:33 +0300

    t

--- UnitTestOfTimetableOfClasses/UT_Delete_CGroup.cs
+++ UnitTestOfTimetableOfClasses/UT_Delete_CGroup.cs
@@ -16,8 +16,8 @@ namespace UnitTestOfTimetableOfClasses
             int countRows = RefData.CGroup.Rows.Count;// countRows == 0
             MGroup mGroup = new MGroup("17-ИСбо-1в", 2, "ИС", 2, 13, 1, 4, "Воскресенье");//countRows == 0
             Assert.IsTrue(RefData.CGroup.Insert(mGroup)); //countRows == 0,RefData.CGroup.Rows.Count == 1 
-            Assert.AreEqual(countRows+1, RefData.CGroup.Rows.Count);//(n-1)   
-            Assert.AreEqual(RefData.CGroup.Rows[countRows]);//
+            Assert.AreEqual(countRows+1, RefData.CGroup.Rows.Count);// 
+            Assert.AreEqual(RefData.CGroup.Rows[countRows], RefData.CGroup.Rows[3]);//
             Assert.IsTrue(RefData.CGroup.Delete(mGroup));
         }
         /// <summary>
@@ -26,10 +26,9 @@ namespace UnitTestOfTimetableOfClasses
         [TestMethod]
         public void Task_249_2()
         {
+            RefData.InitRefData();
             //arrange 
-            MDirectionOfPreparation mDirectionOfPreparation = new MDirectionOfPreparation("09.03.02", "Информационные системы и технологии", 4);
-            MTrainingProfile mTrainingProfile = new MTrainingProfile("Информационные системы", "ИС", "09.03.02");
-            MGroup gr = new MGroup("17-ИСбо-2а", 1, "ИСиТ", 1, 1, 0, 0, "Воскресенье");
+            MGroup gr = new MGroup("17-ИСбо-1в", 1, "ИСиТ", 1, 1, 0, 0, "Воскресенье");
             bool expected = false;
             //act 
             bool actual = RefData.CGroup.Delete(gr);
