1ff3410028071abea4b93c99904565c9a02cd2cd
ssas18 <sasha.sas.kovalenko@gmail.com>
2020-07-02 15:47:18 +0300

    fix length of _fullname

--- LibOfTimetableOfClasses/MInstitute.cs
+++ LibOfTimetableOfClasses/MInstitute.cs
@@ -27,8 +27,8 @@ namespace LibOfTimetableOfClasses
                     throw new Exception("Поле Полное название института пустое");
                 if (!Regex.IsMatch(value, @"[А-Яа-я\-\' ']"))
                     throw new Exception("Поле Полное название института содержит недопустимые символы");
-                if (value.Length > 50)
-                    throw new Exception("Слишком длинное значение (не более 50)");
+                if (value.Length > 60)
+                    throw new Exception("Слишком длинное значение (не более 60)");
                 if (value.Length <= 1)
                     throw new Exception("Слишком короткое значение");
                 for (int i = 1; i < value.Length; i++)
