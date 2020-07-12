4dafd551b74807935f2af3e53c7805b36ae6d11e
Виноградов Александр <ifuwmnn@gmail.com>
2020-07-12 14:41:30 +0300

    Update UT_Delete_CEnclosures.cs

--- UnitTestOfTimetableOfClasses/UT_Delete_CEnclosures.cs
+++ UnitTestOfTimetableOfClasses/UT_Delete_CEnclosures.cs
@@ -11,31 +11,17 @@ namespace UnitTestOfTimetableOfClasses
         /// Удаление данных из пустой таблицы
         /// </summary>
         [TestMethod]
-        public void Task_395_2()
+        public void Task_395_1()
         {
             MUniversity grUni2 = new MUniversity("4401906286", "ВТБ", "Костьомкой Госуьарственный Унивеьситет", "159005, Костьомская оьласть, р. Коьтрома, пл. Дзержиьского, 27", "158005, Костьомская обьасть, р. Костьома, пл. Дзеьжинского, 47", "Алекьандр", "Наьмов", "Рудолььович", "kru@mail.ru", "84944417960");
             MEnclosures gr2 = new MEnclosures("Я", grUni2.FullName, "Дзббжинского", "112111", "8");
             Assert.AreEqual(refData.CEnclosures.Delete(gr2), false);
         }
         /// <summary>
-        /// удаление данных при совпадении всех ключевых полей
-        /// </summary>
-        [TestMethod]
-        public void Task_395_1()
-        {
-            MUniversity grUni = new MUniversity("4401906286", "ВТБ", "Костьомкой Госуьарственный Унивеьситет", "159005, Костьомская оьласть, р. Коьтрома, пл. Дзержиьского, 27", "158005, Костьомская обьасть, р. Костьома, пл. Дзеьжинского, 47", "Алекьандр", "Наьмов", "Рудолььович", "kru@mail.ru", "84944417960");
-            refData.CUniversity.Insert(grUni);
-            MEnclosures gr = new MEnclosures("Я", grUni.FullName, "Дзббжинского", "112111", "8");
-            refData.CEnclosures.Insert(gr);
-            MUniversity grUni2 = new MUniversity("4401906286", "ВТБ", "Костьомкой Госуьарственный Унивеьситет", "159005, Костьомская оьласть, р. Коьтрома, пл. Дзержиьского, 27", "158005, Костьомская обьасть, р. Костьома, пл. Дзеьжинского, 47", "Алекьандр", "Наьмов", "Рудолььович", "kru@mail.ru", "84944417960");
-            MEnclosures gr2 = new MEnclosures("Я", grUni2.FullName, "Дзббжинского", "112111", "8");
-            Assert.AreEqual(refData.CEnclosures.Delete(gr2), true);
-        }
-        /// <summary>
         /// Удаление данных при не совпадающих значениях поля Name
         /// </summary>
         [TestMethod]
-        public void Task_395_3()
+        public void Task_395_2()
         {
             MUniversity grUni = new MUniversity("4401906286", "ВТБ", "Костьомкой Госуьарственный Унивеьситет", "159005, Костьомская оьласть, р. Коьтрома, пл. Дзержиьского, 27", "158005, Костьомская обьасть, р. Костьома, пл. Дзеьжинского, 47", "Алекьандр", "Наьмов", "Рудолььович", "kru@mail.ru", "84944417960");
             refData.CUniversity.Insert(grUni);
@@ -49,7 +35,7 @@ namespace UnitTestOfTimetableOfClasses
         /// Удаление данных при не совпадающих значениях поля University
         /// </summary>
         [TestMethod]
-        public void Task_395_4()
+        public void Task_395_3()
         {
             MUniversity grUni = new MUniversity("4401906286", "ВТБ", "Костьомкой Госуьарственный Унивеьситет", "159005, Костьомская оьласть, р. Коьтрома, пл. Дзержиьского, 27", "158005, Костьомская обьасть, р. Костьома, пл. Дзеьжинского, 47", "Алекьандр", "Наьмов", "Рудолььович", "kru@mail.ru", "84944417960");
             refData.CUniversity.Insert(grUni);
@@ -58,7 +44,20 @@ namespace UnitTestOfTimetableOfClasses
             MUniversity grUni2 = new MUniversity("4401906286", "ВТБ", "Цостьомкой Госуьарственный Унивеьситет", "159005, Костьомская оьласть, р. Коьтрома, пл. Дзержиьского, 27", "158005, Костьомская обьасть, р. Костьома, пл. Дзеьжинского, 47", "Алекьандр", "Наьмов", "Рудолььович", "kru@mail.ru", "84944417960");
             MEnclosures gr2 = new MEnclosures("Я", grUni2.FullName, "Дзббжинского", "112111", "8");
             Assert.AreEqual(refData.CEnclosures.Delete(gr2), false);
-
+        }
+        /// <summary>
+        /// удаление данных при совпадении всех ключевых полей
+        /// </summary>
+        [TestMethod]
+        public void Task_395_4()
+        {
+            MUniversity grUni = new MUniversity("4401906286", "ВТБ", "Костьомкой Госуьарственный Унивеьситет", "159005, Костьомская оьласть, р. Коьтрома, пл. Дзержиьского, 27", "158005, Костьомская обьасть, р. Костьома, пл. Дзеьжинского, 47", "Алекьандр", "Наьмов", "Рудолььович", "kru@mail.ru", "84944417960");
+            refData.CUniversity.Insert(grUni);
+            MEnclosures gr = new MEnclosures("Я", grUni.FullName, "Дзббжинского", "112111", "8");
+            refData.CEnclosures.Insert(gr);
+            MUniversity grUni2 = new MUniversity("4401906286", "ВТБ", "Костьомкой Госуьарственный Унивеьситет", "159005, Костьомская оьласть, р. Коьтрома, пл. Дзержиьского, 27", "158005, Костьомская обьасть, р. Костьома, пл. Дзеьжинского, 47", "Алекьандр", "Наьмов", "Рудолььович", "kru@mail.ru", "84944417960");
+            MEnclosures gr2 = new MEnclosures("Я", grUni2.FullName, "Дзббжинского", "112111", "8");
+            Assert.AreEqual(refData.CEnclosures.Delete(gr2), true);
         }
     }
 }
