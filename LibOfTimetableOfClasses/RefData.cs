d344784a8c79a2ed2de47448a759bd1e30d1a9bf
Alexander Sokolov <sashabank82@gmail.com>
2020-08-24 18:08:21 +0300

    Отделение инициализации данных рефдаты

--- LibOfTimetableOfClasses/RefData.cs
+++ LibOfTimetableOfClasses/RefData.cs
@@ -1,4 +1,4 @@
-﻿using System.Data;
+﻿ using System.Data;
 
 namespace LibOfTimetableOfClasses
 {
@@ -50,7 +50,7 @@ namespace LibOfTimetableOfClasses
             DataSet.Relations.Add("AcademicLoad-Discipline", CAcademicLoad.Columns["Discipline"], CDiscipline.Columns["FullName"]);
             DataSet.Relations.Add("AcademicLoad-Teacher", CAcademicLoad.Columns["Professor"], CTeacher.Columns["FullName"]);
             DataSet.Relations.Add("AcademicLoad-TypesOfOccupations", CAcademicLoad.Columns["KindOfLesson"], CTypesOfOccupations.Columns["FullName"]);
-            InitData();
+           // InitData();
         }
     
         private void InitData() {
