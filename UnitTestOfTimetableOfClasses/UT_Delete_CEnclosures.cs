4900da18cc5974428ea6167378d5e356d416b4ef
Виноградов Александр <ifuwmnn@gmail.com>
2020-08-28 21:21:03 +0300

    Update UT_Delete_CEnclosures.cs

--- UnitTestOfTimetableOfClasses/UT_Delete_CEnclosures.cs
+++ UnitTestOfTimetableOfClasses/UT_Delete_CEnclosures.cs
@@ -25,13 +25,13 @@ namespace UnitTestOfTimetableOfClasses
         [TestMethod]
         public void Task_395_2()
         {
-            MUniversity grUni = new MUniversity("4401906286", "ВТБ", "Костьомкой Госуьарственный Унивеьситет", "159005, Костьомская оьласть, р. Коьтрома, пл. Дзержиьского, 27", "158005, Костьомская обьасть, р. Костьома, пл. Дзеьжинского, 47", "Алекьандр", "Наьмов", "Рудолььович", "kru@mail.ru", "84944417960");
-            Assert.IsTrue(refData.CUniversity.Insert(grUni), "Не была добавлена запись");
-            MEnclosures gr = new MEnclosures("Я", grUni.FullName, "Дзббжинского", "112111", "8");
-            Assert.IsTrue(refData.CUniversity.Insert(gr), "Не была добавлена запись");
             MUniversity grUni2 = new MUniversity("4401906286", "ВТБ", "Костьомкой Госуьарственный Унивеьситет", "159005, Костьомская оьласть, р. Коьтрома, пл. Дзержиьского, 27", "158005, Костьомская обьасть, р. Костьома, пл. Дзеьжинского, 47", "Алекьандр", "Наьмов", "Рудолььович", "kru@mail.ru", "84944417960");
-            MEnclosures gr2 = new MEnclosures("М", grUni2.FullName, "Дзббжинского", "112111", "8");
-            Assert.IsFalse(refData.CEnclosures.Delete(gr2), "Запись была удалена");
+            Assert.IsTrue(refData.CUniversity.Insert(grUni2), "Не была добавлена запись");
+            MEnclosures gr2 = new MEnclosures("Я", grUni2.FullName, "Дзббжинского", "112111", "8");
+            Assert.IsTrue(refData.CUniversity.Insert(gr2), "Не была добавлена запись");
+            MUniversity grUni22 = new MUniversity("4401906286", "ВТБ", "Костьомкой Госуьарственный Унивеьситет", "159005, Костьомская оьласть, р. Коьтрома, пл. Дзержиьского, 27", "158005, Костьомская обьасть, р. Костьома, пл. Дзеьжинского, 47", "Алекьандр", "Наьмов", "Рудолььович", "kru@mail.ru", "84944417960");
+            MEnclosures gr22 = new MEnclosures("М", grUni22.FullName, "Дзббжинского", "112111", "8");
+            Assert.IsFalse(refData.CEnclosures.Delete(gr22), "Запись была удалена");
         }
         /// <summary>
         /// Удаление данных при не совпадающих значениях поля University
@@ -39,13 +39,13 @@ namespace UnitTestOfTimetableOfClasses
         [TestMethod]
         public void Task_395_3()
         {
-            MUniversity grUni = new MUniversity("4401906286", "ВТБ", "Костьомкой Госуьарственный Унивеьситет", "159005, Костьомская оьласть, р. Коьтрома, пл. Дзержиьского, 27", "158005, Костьомская обьасть, р. Костьома, пл. Дзеьжинского, 47", "Алекьандр", "Наьмов", "Рудолььович", "kru@mail.ru", "84944417960");
-            Assert.IsTrue(refData.CUniversity.Insert(grUni), "Не была добавлена запись");
-            MEnclosures gr = new MEnclosures("Я", grUni.FullName, "Дзббжинского", "112111", "8");
-            Assert.IsTrue(refData.CUniversity.Insert(gr), "Не была добавлена запись");
-            MUniversity grUni2 = new MUniversity("4401906286", "ВТБ", "Цостьомкой Госуьарственный Унивеьситет", "159005, Костьомская оьласть, р. Коьтрома, пл. Дзержиьского, 27", "158005, Костьомская обьасть, р. Костьома, пл. Дзеьжинского, 47", "Алекьандр", "Наьмов", "Рудолььович", "kru@mail.ru", "84944417960");
-            MEnclosures gr2 = new MEnclosures("Я", grUni2.FullName, "Дзббжинского", "112111", "8");
-            Assert.IsFalse(refData.CEnclosures.Delete(gr2), "Запись была удалена");
+            MUniversity grUni3 = new MUniversity("4401906286", "ВТБ", "Костьомкой Госуьарственный Унивеьситет", "159005, Костьомская оьласть, р. Коьтрома, пл. Дзержиьского, 27", "158005, Костьомская обьасть, р. Костьома, пл. Дзеьжинского, 47", "Алекьандр", "Наьмов", "Рудолььович", "kru@mail.ru", "84944417960");
+            Assert.IsTrue(refData.CUniversity.Insert(grUni3), "Не была добавлена запись");
+            MEnclosures gr3 = new MEnclosures("Я", grUni3.FullName, "Дзббжинского", "112111", "8");
+            Assert.IsTrue(refData.CUniversity.Insert(gr3), "Не была добавлена запись");
+            MUniversity grUni33 = new MUniversity("4401906286", "ВТБ", "Цостьомкой Госуьарственный Унивеьситет", "159005, Костьомская оьласть, р. Коьтрома, пл. Дзержиьского, 27", "158005, Костьомская обьасть, р. Костьома, пл. Дзеьжинского, 47", "Алекьандр", "Наьмов", "Рудолььович", "kru@mail.ru", "84944417960");
+            MEnclosures gr33 = new MEnclosures("Я", grUni33.FullName, "Дзббжинского", "112111", "8");
+            Assert.IsFalse(refData.CEnclosures.Delete(gr33), "Запись была удалена");
         }
         /// <summary>
         /// удаление данных при совпадении всех ключевых полей
@@ -53,13 +53,13 @@ namespace UnitTestOfTimetableOfClasses
         [TestMethod]
         public void Task_395_4()
         {
-            MUniversity grUni = new MUniversity("4401906286", "ВТБ", "Костьомкой Госуьарственный Унивеьситет", "159005, Костьомская оьласть, р. Коьтрома, пл. Дзержиьского, 27", "158005, Костьомская обьасть, р. Костьома, пл. Дзеьжинского, 47", "Алекьандр", "Наьмов", "Рудолььович", "kru@mail.ru", "84944417960");
-            Assert.IsTrue(refData.CUniversity.Insert(grUni), "Не была добавлена запись");
-            MEnclosures gr = new MEnclosures("Я", grUni.FullName, "Дзббжинского", "112111", "8");
-            Assert.IsTrue(refData.CUniversity.Insert(gr), "Не была добавлена запись");
-            MUniversity grUni2 = new MUniversity("4401906286", "ВТБ", "Костьомкой Госуьарственный Унивеьситет", "159005, Костьомская оьласть, р. Коьтрома, пл. Дзержиьского, 27", "158005, Костьомская обьасть, р. Костьома, пл. Дзеьжинского, 47", "Алекьандр", "Наьмов", "Рудолььович", "kru@mail.ru", "84944417960");
-            MEnclosures gr2 = new MEnclosures("Я", grUni2.FullName, "Дзббжинского", "112111", "8");
-            Assert.IsTrue(refData.CEnclosures.Delete(gr2), "Запись не была удалена");
+            MUniversity grUni4 = new MUniversity("4401906286", "ВТБ", "Костьомкой Госуьарственный Унивеьситет", "159005, Костьомская оьласть, р. Коьтрома, пл. Дзержиьского, 27", "158005, Костьомская обьасть, р. Костьома, пл. Дзеьжинского, 47", "Алекьандр", "Наьмов", "Рудолььович", "kru@mail.ru", "84944417960");
+            Assert.IsTrue(refData.CUniversity.Insert(grUni4), "Не была добавлена запись");
+            MEnclosures gr4 = new MEnclosures("Я", grUni4.FullName, "Дзббжинского", "112111", "8");
+            Assert.IsTrue(refData.CUniversity.Insert(gr4), "Не была добавлена запись");
+            MUniversity grUni44 = new MUniversity("4401906286", "ВТБ", "Костьомкой Госуьарственный Унивеьситет", "159005, Костьомская оьласть, р. Коьтрома, пл. Дзержиьского, 27", "158005, Костьомская обьасть, р. Костьома, пл. Дзеьжинского, 47", "Алекьандр", "Наьмов", "Рудолььович", "kru@mail.ru", "84944417960");
+            MEnclosures gr44 = new MEnclosures("Я", grUni44.FullName, "Дзббжинского", "112111", "8");
+            Assert.IsTrue(refData.CEnclosures.Delete(gr44), "Запись не была удалена");
         }
     }
 }
