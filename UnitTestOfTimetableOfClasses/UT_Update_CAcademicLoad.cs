8f0f4185c859a83282a116ad106cc80437d42aea
Александр Коваленко <sasha.sas.kovalenko@gmail.com>
2020-09-09 20:35:56 +0300

    fixed some errors

--- UnitTestOfTimetableOfClasses/UT_Update_CAcademicLoad.cs
+++ UnitTestOfTimetableOfClasses/UT_Update_CAcademicLoad.cs
@@ -11,25 +11,34 @@ namespace UnitTestOfTimetableOfClasses
         {
             MDirectionOfPreparation mDirectionOfPreparation = new MDirectionOfPreparation("09.03.02", "Информационные системы и технологии", 4);
             bool actualmDirectionOfPreparation = refData.CDirectionOfPreparation.Insert(mDirectionOfPreparation);
+                Assert.IsTrue(actualmDirectionOfPreparation, "Не удалось вставить направление подготовки");
             MTrainingProfile mTrainingProfile = new MTrainingProfile("Информационные системы", "ИС", "09.03.02");
             bool actualmTrainingProfile = refData.CTrainingProfile.Insert(mTrainingProfile);
+                Assert.IsTrue(actualmTrainingProfile, "Не удалось вставить профиль подготовки");
             MGroup mGroup = new MGroup("17-ИСбо-2а", 4, "ИС", 1, 20, 2, 5, "Суббота");
             bool actualmGroup = refData.CGroup.Insert(mGroup);
+                Assert.IsTrue(actualmGroup, "Не удалось вставить группу 1");
             MGroup mGroup2 = new MGroup("17-ИСбо-2б", 3, "ИС", 1, 10, 1, 6, "Суббота");
             bool actualmGroup2 = refData.CGroup.Insert(mGroup2);
+                Assert.IsTrue(actualmGroup2, "Не удалось вставить группу 2");
 
             MDiscipline mDiscipline1 = new MDiscipline("Правоведение", "Право", "Сентябрь-Декабрь");
             bool actuamDiscipline1 = refData.CDiscipline.Insert(mDiscipline1);
+                Assert.IsTrue(actuamDiscipline1, "Не удалось вставить дисциплину");
 
             MAcademicDegree mAcademicDegree = new MAcademicDegree("Кандидат наук", "КН");
             bool actuamAcademicDegree = refData.CAcademicDegree.Insert(mAcademicDegree);
+                Assert.IsTrue(actuamAcademicDegree, "Не удалось вставить ученую степень");
             MTitle mTitle = new MTitle("Доцент", "Доц");
             bool actualmTitle = refData.CTitle.Insert(mTitle);
+                Assert.IsTrue(actualmTitle, "Не удалось вставить ученое звание"); 
             MTeacher mTeacher = new MTeacher("Иван", "Иванов", "Иванович", "КН", "Доц", "ИАСТ", "Пн, Чт", "ПТ", "СБ, ВС");
             bool actualamTeacher = refData.CTeacher.Insert(mTeacher);
+                Assert.IsTrue(actualamTeacher, "Не удалось вставить преподавателя");
 
             MTypesOfOccupations mTypesOfOccupations = new MTypesOfOccupations("Лекция", "Лек");
             bool actualmTypesOfOccupations = refData.CTypesOfOccupations.Insert(mTypesOfOccupations);
+                Assert.IsTrue(actualmTypesOfOccupations, "Не удалось вставить тип занятия");
 
             MAcademicLoad PreMa = new MAcademicLoad("17-ИСбо-2а", "100", "Правоведение", "Иванов Иван Иванович", "Лекция", "20");
             bool actualPreMa = refData.CAcademicLoad.Insert(PreMa);
