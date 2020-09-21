5d2060f2844123db14af2262c439ceafb4f67e5c
Kruuliter <solenit@mail.ru>
2020-09-21 23:17:12 +0300

    modification due to branch changes

--- UnitTestOfTimetableOfClasses/UT_Delete_CTeacher.cs
+++ UnitTestOfTimetableOfClasses/UT_Delete_CTeacher.cs
@@ -8,27 +8,63 @@ namespace UnitTestOfTimetableOfClasses
     {
         readonly RefData refData = new RefData();
         [TestMethod]
-        public void Task_248_1() //Удаление существующей строки 
+        public void Task_1187_1()//Удаление из пустой таблицы
         {
-            //arrange 
+            //arrage
             MTeacher tcher = new MTeacher("Садовская", "Ольга", "Борисовна", "Кандидат наук", "Профессор", "ФАСТ", "Пн, Вт", "Ср, Чт, Пт", "Воскресенье");
-            bool expected = true;
-            //act 
-            refData.CTeacher.Insert(tcher);
+            bool expected = false;
+            //act
             bool actual = refData.CTeacher.Delete(tcher);
-            //assert 
+            //assert
             Assert.AreEqual(expected, actual);
         }
 
         [TestMethod]
-        public void Task_248_2() //Удаление не существующей строки 
+        public void Task_1187_2()//Удаление из заполненной таблицы
         {
-            //arrange 
+            //arrage
             MTeacher tcher = new MTeacher("Садовская", "Ольга", "Борисовна", "Кандидат наук", "Профессор", "ФАСТ", "Пн, Вт", "Ср, Чт, Пт", "Воскресенье");
+            MTeacher tcher1 = new MTeacher("Садовская", "Елена", "Григорьевна", "К. псих. н.", "Доцент", "РАСТ", "Пн, Вт", "Ср, Чт, Пт", "Воскресенье");
+            MTeacher tcher2 = new MTeacher("Садовская", "Ирина", "Борисовна", "К. технич. н.", "Нет", "ИАСТ", "Пн, Вт", "Ср, Чт, Пт", "Воскресенье");
             bool expected = false;
-            //act 
+            //act
+            refData.CTeacher.Insert(tcher1);
+            refData.CTeacher.Insert(tcher2);
             bool actual = refData.CTeacher.Delete(tcher);
-            //assert 
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Task_1187_3()//Удаление не существующей строки из таблицы
+        {
+            //arrage
+            MTeacher tcher = new MTeacher("Садовская", "Ольга", "Борисовна", "Кандидат наук", "Профессор", "ФАСТ", "Пн, Вт", "Ср, Чт, Пт", "Воскресенье");
+            MTeacher tcher1 = new MTeacher("Садовская", "Елена", "Григорьевна", "К. псих. н.", "Доцент", "РАСТ", "Пн, Вт", "Ср, Чт, Пт", "Воскресенье");
+            MTeacher tcher2 = new MTeacher("Садовская", "Ирина", "Борисовна", "К. технич. н.", "Нет", "ИАСТ", "Пн, Вт", "Ср, Чт, Пт", "Воскресенье");
+            bool expected = false;
+            //act
+            refData.CTeacher.Insert(tcher1);
+            refData.CTeacher.Insert(tcher2);
+            bool actual = refData.CTeacher.Delete(tcher);
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Task_1187_4()//Удаление существующей строки из таблицы
+        {
+            //arrage
+            MTeacher tcher = new MTeacher("Садовская", "Ольга", "Борисовна", "Кандидат наук", "Профессор", "ФАСТ", "Пн, Вт", "Ср, Чт, Пт", "Воскресенье");
+            MTeacher tcher1 = new MTeacher("Садовская", "Елена", "Григорьевна", "Кандидат наук", "Профессор", "ФАСТ", "Пн, Вт", "Ср, Чт, Пт", "Воскресенье");
+            MTeacher tcher2 = new MTeacher("Садовская", "Ирина", "Борисовна", "Кандидат наук", "Профессор", "ФАСТ", "Пн, Вт", "Ср, Чт, Пт", "Воскресенье");
+            bool expected = true;
+            //act
+            refData.CTeacher.Insert(tcher);
+            refData.CTeacher.Insert(tcher1);
+            refData.CTeacher.Insert(tcher2);
+            bool actual = refData.CTeacher.Delete(tcher);
+            //assert
             Assert.AreEqual(expected, actual);
         }
     }
