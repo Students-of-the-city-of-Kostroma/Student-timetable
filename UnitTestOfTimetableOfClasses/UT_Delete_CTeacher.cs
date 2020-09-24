10933e289c578311157553a7abfefc6fba7b32c6
Kruuliter <solenit@mail.ru>
2020-09-24 20:30:34 +0300

    Update test UT_Delete_CTeacher.cs

--- UnitTestOfTimetableOfClasses/UT_Delete_CTeacher.cs
+++ UnitTestOfTimetableOfClasses/UT_Delete_CTeacher.cs
@@ -6,11 +6,11 @@ namespace UnitTestOfTimetableOfClasses
     [TestClass]
     public class UT_Delete_CTeacher
     {
-        readonly RefData refData = new RefData();
         [TestMethod]
         public void Task_1187_1()//Удаление из пустой таблицы
         {
             //arrage
+            RefData refData = new RefData();
             MTeacher tcher = new MTeacher("Садовская", "Ольга", "Борисовна", "Кандидат наук", "Профессор", "ФАСТ", "Пн, Вт", "Ср, Чт, Пт", "Воскресенье");
             bool expected = false;
             //act
@@ -23,6 +23,7 @@ namespace UnitTestOfTimetableOfClasses
         public void Task_1187_2()//Удаление из заполненной таблицы
         {
             //arrage
+            RefData refData = new RefData();
             MTeacher tcher = new MTeacher("Садовская", "Ольга", "Борисовна", "Кандидат наук", "Профессор", "ФАСТ", "Пн, Вт", "Ср, Чт, Пт", "Воскресенье");
             MTeacher tcher1 = new MTeacher("Садовская", "Елена", "Григорьевна", "К. псих. н.", "Доцент", "РАСТ", "Пн, Вт", "Ср, Чт, Пт", "Воскресенье");
             MTeacher tcher2 = new MTeacher("Садовская", "Ирина", "Борисовна", "К. технич. н.", "Нет", "ИАСТ", "Пн, Вт", "Ср, Чт, Пт", "Воскресенье");
@@ -39,6 +40,7 @@ namespace UnitTestOfTimetableOfClasses
         public void Task_1187_3()//Удаление не существующей строки из таблицы
         {
             //arrage
+            RefData refData = new RefData();
             MTeacher tcher = new MTeacher("Садовская", "Ольга", "Борисовна", "Кандидат наук", "Профессор", "ФАСТ", "Пн, Вт", "Ср, Чт, Пт", "Воскресенье");
             MTeacher tcher1 = new MTeacher("Садовская", "Елена", "Григорьевна", "К. псих. н.", "Доцент", "РАСТ", "Пн, Вт", "Ср, Чт, Пт", "Воскресенье");
             MTeacher tcher2 = new MTeacher("Садовская", "Ирина", "Борисовна", "К. технич. н.", "Нет", "ИАСТ", "Пн, Вт", "Ср, Чт, Пт", "Воскресенье");
@@ -55,6 +57,7 @@ namespace UnitTestOfTimetableOfClasses
         public void Task_1187_4()//Удаление существующей строки из таблицы
         {//тут ошибка
             //arrage
+            RefData refData = new RefData();
             MTeacher tcher = new MTeacher("Садовская", "Ольга", "Борисовна", "Кандидат наук", "Профессор", "ФАСТ", "Пн, Вт", "Ср, Чт, Пт", "Воскресенье");
             MTeacher tcher1 = new MTeacher("Садовская", "Елена", "Григорьевна", "Кандидат наук", "Профессор", "ФАСТ", "Пн, Вт", "Ср, Чт, Пт", "Воскресенье");
             MTeacher tcher2 = new MTeacher("Садовская", "Ирина", "Борисовна", "Кандидат наук", "Профессор", "ФАСТ", "Пн, Вт", "Ср, Чт, Пт", "Воскресенье");
