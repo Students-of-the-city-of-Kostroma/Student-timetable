828df06fbca7f737760a150f6d8d911d583ee63e
MyachevM <mishamen125@gmail.com>
2020-09-28 23:15:01 +0300

    Fixed Unit Test

--- UnitTestOfTimetableOfClasses/UT_Delete_CTeacher.cs
+++ UnitTestOfTimetableOfClasses/UT_Delete_CTeacher.cs
@@ -8,28 +8,52 @@ namespace UnitTestOfTimetableOfClasses
     {
         readonly RefData refData = new RefData();
         [TestMethod]
-        public void Task_248_1() //Удаление существующей строки 
+        public void issue_1281_1() //Удаление сведений в пустой таблице преподавателей 
         {
-            //arrange 
+            Assert.AreEqual(refData.CTeacher.Rows.Count == 0, true, "При проверке отсутствия записей в таблице преподавателей, она оказалась не пустой"); 
             MTeacher tcher = new MTeacher("Садовская", "Ольга", "Борисовна", "Кандидат наук", "Профессор", "ФАСТ", "Пн, Вт", "Ср, Чт, Пт", "Воскресенье");
-            bool expected = true;
-            //act 
-            refData.CTeacher.Insert(tcher);
-            bool actual = refData.CTeacher.Delete(tcher);
-            //assert 
-            Assert.AreEqual(expected, actual);
+            Assert.AreEqual(refData.CTeacher.Delete(tcher), false, "Преподаватель был удалён, не смотря на то, что таблица преподавателей пуста");
         }
 
         [TestMethod]
-        public void Task_248_2() //Удаление не существующей строки 
+        public void issue_1281_2() //Попытка удалить преподавателя при несовпадающих значениях фамилии 
         {
-            //arrange 
-            MTeacher tcher = new MTeacher("Садовская", "Ольга", "Борисовна", "Кандидат наук", "Профессор", "ФАСТ", "Пн, Вт", "Ср, Чт, Пт", "Воскресенье");
-            bool expected = false;
-            //act 
-            bool actual = refData.CTeacher.Delete(tcher);
-            //assert 
-            Assert.AreEqual(expected, actual);
+
+            MAcademicDegree mad = new MAcademicDegree ("Кандидат наук","КН");
+            Assert.AreEqual(refData.CAcademicDegree.Insert(mad), true, "При попытке добавить учёную степень в таблицу с учёными степенями она не была добавлена");
+            MTitle mt = new MTitle("Профессор", "ПР");
+            Assert.AreEqual(refData.CTitle.Insert(mt), true, "При попытке добавить должность в таблицу должностей она не была добавлена");
+            MTeacher tcher = new MTeacher("Садовская", "Ольга", "Борисовна", "КН", "ПР", "ФАСТ", "Пн, Вт", "Ср, Чт, Пт", "Воскресенье");
+            Assert.AreEqual(refData.CTeacher.Insert(tcher), true, "При попытке добавить преподавателя в таблицу с преподавателями он не был добавлен");
+            MTeacher tcher2 = new MTeacher("Прядкина", "Ольга", "Борисовна", "КН", "ПР", "ФАСТ", "Пн, Вт", "Ср, Чт, Пт", "Воскресенье");
+            Assert.AreEqual(refData.CTeacher.Delete(tcher2), false, "Преподаватель был удалён, не смотря на то, что фамилии не совпадают");
+        }
+
+        [TestMethod]
+        public void issue_1281_3() //Попытка удалить преподавателя при несовпадающих значениях института 
+        {
+            MAcademicDegree mad = new MAcademicDegree("Кандидат наук", "КН");
+            Assert.AreEqual(refData.CAcademicDegree.Insert(mad), true, "При попытке добавить учёную степень в таблицу с учёными степенями она не была добавлена");
+            MTitle mt = new MTitle("Профессор", "ПР");
+            Assert.AreEqual(refData.CTitle.Insert(mt), true, "При попытке добавить должность в таблицу должностей она не была добавлена");
+            MTeacher tcher = new MTeacher("Садовская", "Ольга", "Борисовна", "КН", "ПР", "ФАСТ", "Пн, Вт", "Ср, Чт, Пт", "Воскресенье");
+            Assert.AreEqual(refData.CTeacher.Insert(tcher), true, "При попытке добавить преподавателя в таблицу с преподавателями он не был добавлен");
+            MTeacher tcher2 = new MTeacher("Садовская", "Ольга", "Борисовна", "КН", "ПР", "ИГНИСТ", "Пн, Вт", "Ср, Чт, Пт", "Воскресенье");
+            Assert.AreEqual(refData.CTeacher.Delete(tcher2), false, "Преподаватель был удалён, не смотря на то, что название институтов не совпадают");
+        }
+
+        [TestMethod]
+        public void issue_1281_4() //Попытка удалить данные при всех совпадающих значениях
+        {
+
+            MAcademicDegree mad = new MAcademicDegree("Кандидат наук", "КН");
+            Assert.AreEqual(refData.CAcademicDegree.Insert(mad), true, "При попытке добавить учёную степень в таблицу с учёными степенями она не была добавлена");
+            MTitle mt = new MTitle("Профессор", "ПР");
+            Assert.AreEqual(refData.CTitle.Insert(mt), true, "При попытке добавить должность в таблицу должностей она не была добавлена");
+            MTeacher tcher = new MTeacher("Садовская", "Ольга", "Борисовна", "КН", "ПР", "ФАСТ", "Пн, Вт", "Ср, Чт, Пт", "Воскресенье");
+            Assert.AreEqual(refData.CTeacher.Insert(tcher), true, "При попытке добавить преподавателя в таблицу с преподавателями он не был добавлен");
+            MTeacher tcher2 = new MTeacher("Садовская", "Ольга", "Борисовна", "КН", "ПР", "ФАСТ", "Пн, Вт", "Ср, Чт, Пт", "Воскресенье");
+            Assert.AreEqual(refData.CTeacher.Delete(tcher2), true, "Преподаватель не был удалён, не смотря на то, что все поля совпадают");
         }
     }
 }
