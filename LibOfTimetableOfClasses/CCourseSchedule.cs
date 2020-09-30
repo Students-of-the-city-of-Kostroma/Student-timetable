07aea91f30142a09cbe71812df47d3227b600ced
ZhizhikinIvan <drivezh@gmail.com>
2020-09-30 21:00:48 +0300

    Добавлены модель и таблица для расписания занятий

---
+++ LibOfTimetableOfClasses/CCourseSchedule.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibOfTimetableOfClasses
+{
+    /// <summary>
+    /// Таблица, хранящая расписание занятий
+    /// </summary>
+    public class CCourseSchedule : DataTable, IController
+    {
+        public CCourseSchedule() : base("Расписание")
+        {
+            DataColumn column = new DataColumn
+            {
+                DataType = typeof(string),
+                ColumnName = "Discipline",
+            };
+            Columns.Add(column);
+
+            column = new DataColumn
+            {
+                DataType = typeof(string),
+                ColumnName = "Classroom"
+            };
+            Columns.Add(column);
+
+            column = new DataColumn
+            {
+                DataType = typeof(ushort),
+                ColumnName = "DayOfWeek"
+            };
+            Columns.Add(column);
+
+            column = new DataColumn
+            {
+                DataType = typeof(string),
+                ColumnName = "StartTime"
+            };
+            Columns.Add(column);
+
+            column = new DataColumn
+            {
+                DataType = typeof(string),
+                ColumnName = "EndTime"
+            };
+            Columns.Add(column);
+        }
+
+        public bool Delete(Model model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Insert(Model model)
+        {
+            MCourseSchedule mSchedule = (MCourseSchedule)model;
+
+            try
+            {
+                DataRow newRow = NewRow();
+                newRow["Discipline"] = mSchedule.Discipline;
+                newRow["Classroom"] = mSchedule.Classroom;
+                newRow["DayOfWeek"] = mSchedule.DayOfWeek;
+                newRow["StartTime"] = mSchedule.StartTime;
+                newRow["EndTime"] = mSchedule.EndTime;
+
+                Rows.Add(newRow);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                return false;
+            }
+        }
+
+        public bool Update(Model model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ LibOfTimetableOfClasses/MCourseSchedule.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibOfTimetableOfClasses
+{
+    /// <summary>
+    /// Класс определяющий учебное расписание
+    /// </summary>
+    public class MCourseSchedule : Model
+    {
+        private string _discipline;
+        private string _classroom;
+        private string _dayOfWeek;
+        private TimeSpan _startTime;
+        private TimeSpan _endTime;
+
+        public MCourseSchedule(string discipline, string classroom, string dayOfWeek, TimeSpan startTime, TimeSpan endTime)
+        {
+            Discipline = discipline;
+            Classroom = classroom;
+            DayOfWeek = dayOfWeek;
+            StartTime = startTime;
+            EndTime = endTime;
+        }
+
+
+        /// <summary>
+        /// Предмет
+        /// </summary>
+        public string Discipline
+        {
+            get
+            {
+                return _discipline;
+            }
+            set
+            {
+                _discipline = value;
+            }
+        }
+
+        /// <summary>
+        /// Аудитория
+        /// </summary>
+        public string Classroom
+        {
+            get
+            {
+                return _classroom;
+            }
+            set
+            {
+                _classroom = value;
+            }
+        }
+
+        /// <summary>
+        /// День недели
+        /// </summary>
+        public string DayOfWeek
+        {
+            get
+            {
+                return _dayOfWeek;
+            }
+            set
+            {
+                _dayOfWeek = value;
+            }
+        }
+
+        /// <summary>
+        /// Начало занятий
+        /// </summary>
+        public TimeSpan StartTime
+        {
+            get
+            {
+                return _startTime;
+            }
+            set
+            {
+                _startTime = value;
+            }
+        }
+
+        /// <summary>
+        /// Конец занятий
+        /// </summary>
+        public TimeSpan EndTime
+        {
+            get
+            {
+                return _endTime;
+            }
+            set
+            {
+                _endTime = value;
+            }
+        }
+    }
+}
