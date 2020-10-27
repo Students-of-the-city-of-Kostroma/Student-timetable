8edec0ef78b567b8feb5f3076ee473ff6aa89f53
ZhizhikinIvan <drivezh@gmail.com>
2020-10-27 23:23:37 +0300

    Убрал DTO

--- TimetableOfClasses/Timetable.cs
+++ TimetableOfClasses/Timetable.cs
@@ -5,18 +5,12 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Windows.Forms;
-using TimetableOfClasses.DTO;
 
 namespace TimetableOfClasses
 {
     public partial class Timetable : Form
     {
         private LibOfTimetableOfClasses.RefData refData;
-
-        private List<InstituteDto> institutes = new List<InstituteDto>();
-        private List<GroupDto> allGroups = new List<GroupDto>();
-        private List<CourseDto> allCourses = new List<CourseDto>();
-        private List<TrainingProfileDto> allProfiles = new List<TrainingProfileDto>();
 
         private string[] headers = new string[8] { "Время", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье" };
 
@@ -45,55 +39,25 @@ namespace TimetableOfClasses
         {
             this.refData = data;
 
-            institutes = data.CInstitute.AsEnumerable()
-                .Select(row => new InstituteDto
-                {
-                    ID = row.Field<string>("ShortName"),
-                    FullName = row.Field<string>("FullName")
-                }).ToList();
-            cboInstitute.DataSource = institutes;
+            cboInstitute.DataSource = data.CInstitute;
             cboInstitute.DisplayMember = "FullName";
-            cboInstitute.ValueMember = "ID";
+            cboInstitute.ValueMember = "ShortName";
 
             cboCourse.Enabled = false;
             cboGroup.Enabled = false;
-
-            allCourses = data.CDirectionOfPreparation.AsEnumerable()
-                .Select(row => new CourseDto
-                {
-                    ID = row.Field<string>("CodeOfDP"),
-                    NameOfDP = row.Field<string>("NameOfDP"), 
-                    InstituteShortName = row.Field<string>("InstituteShortName")
-                }).ToList();
-
-            allProfiles = data.CTrainingProfile.AsEnumerable()
-                .Select(row => new TrainingProfileDto
-                {
-                    Code = row.Field<string>("Shiphr"),
-                    ShortName = row.Field<string>("ShortName")
-                }).ToList();
-
-            allGroups = data.CGroup.AsEnumerable()
-                .Select(row => new GroupDto
-                {
-                    ID = row.Field<string>("Group"),
-                    Group = row.Field<string>("Group"),
-                    Speciality = row.Field<string>("Specialty")
-                }).ToList();
-
         }
 
         private void CboInstitute_SelectionChangeCommitted(object sender, System.EventArgs e)
         {
             ComboBox cbo = (ComboBox)sender;
-            InstituteDto selectedInstitute = (InstituteDto)cbo.SelectedItem;
 
-            var courses = allCourses.Where<CourseDto>(gr => gr.InstituteShortName == selectedInstitute.ID).ToList();
+            DataView courses = refData.CDirectionOfPreparation.DefaultView;
+            courses.RowFilter = "InstituteShortName = '" + cbo.SelectedValue + "'";
             if (courses.Count > 0)
             {
                 cboCourse.Enabled = true;
                 cboCourse.DisplayMember = "NameOfDP";
-                cboCourse.ValueMember = "ID";
+                cboCourse.ValueMember = "CodeOfDP";
                 cboCourse.DataSource = courses;
             }
             else
@@ -110,22 +74,16 @@ namespace TimetableOfClasses
         private void CboCourse_SelectionChangeCommitted(object sender, System.EventArgs e)
         {
             ComboBox cbo = (ComboBox)sender;
-            CourseDto selectedCourse = (CourseDto)cbo.SelectedItem;
 
-            var groups = allProfiles.Join(allGroups, p => p.ShortName, gr => gr.Speciality, 
-                (p, gr) => new GroupDto
-                {
-                    ID = gr.ID,
-                    Group = gr.Group,
-                    Speciality = gr.Speciality,
-                    Code = p.Code
-                }).ToList().Where<GroupDto>(gr => gr.Code == selectedCourse.ID).ToList();
+            DataRow[] profile = refData.CTrainingProfile.Select("Shiphr = '" + cbo.SelectedValue + "'");
+            DataView groups = refData.CGroup.DefaultView;
+            groups.RowFilter = "Specialty = '" + profile[0].Field<string>("ShortName") + "'";
 
-            if (groups.Count() > 0)
+            if (groups.Count > 0)
             {
                 cboGroup.Enabled = true;
                 cboGroup.DisplayMember = "Group";
-                cboGroup.ValueMember = "ID";
+                cboGroup.ValueMember = "Group";
                 cboGroup.DataSource = groups;
             }
             else
@@ -140,14 +98,13 @@ namespace TimetableOfClasses
         private void CboGroup_SelectionChangeCommitted(object sender, System.EventArgs e)
         {
             ComboBox cbo = (ComboBox)sender;
-            GroupDto selectedGroup = (GroupDto)cbo.SelectedItem;
 
             TlpSchedule_Clear();
             TlpSchedule_Init();
 
             var groupSchedule = (from al in refData.CAcademicLoad.AsEnumerable()
                                 join cs in refData.CCourseSchedule.AsEnumerable() on (int?)al["ID"] equals (int)cs["AcademicId"]
-                                where (string)al["Group"] == selectedGroup.Group
+                                where (string)al["Group"] == cbo.SelectedValue.ToString()
                                 orderby (string)cs["DayOfWeek"]
                                 select new
                                 {
