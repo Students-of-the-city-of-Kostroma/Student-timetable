1254b2c8e480d0e7f06cddb9609508c1ed2e3cde
Vladimir Nesgovorov <treft304@gmail.com>
2020-11-01 12:07:55 +0300

    add

--- UnitTestOfTimetableOfClasses/CAcademicLoad/Insert/UnitTest.cs
+++ UnitTestOfTimetableOfClasses/CAcademicLoad/Insert/UnitTest.cs
@@ -8,17 +8,17 @@ using System.Diagnostics;
 using LibOfTimetableOfClasses;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace UnitTestOfTimetableOfClasses
+namespace UnitTestOfTimetableOfClasses.Insert.AcademicLoad
 {
     [TestClass]
-    public class UT_Insert_CAcademicLoad
+    public class UT_ICAcademicLoad
     {
         RefData refData = new RefData();
         /// <summary>
         /// Ввод корректных данных в пустую таблицу
         /// </summary>
         [TestMethod]
-        public void Task_1240_1()
+        public void ICAcademicLoad_1()
         {
             bool expected = true;
             MAcademicLoad mal = new MAcademicLoad("ВТ", "48", "Физкультура", "Иванов", "Лекция", "20");
@@ -31,10 +31,9 @@ namespace UnitTestOfTimetableOfClasses
         /// учёная степень с такой сокращённой записью уже есть в таблице
         /// </summary>
         [TestMethod]
-        public void Task_1240_2()
+        public void ICAcademicLoad_2()
         {
             //arrange
-            Task_1240_1();
             MAcademicLoad MAcademicL = new MAcademicLoad("ВТ", "48", "Физкультура", "Иванов", "Лекция", "20");
             bool expected = false;
             //act
