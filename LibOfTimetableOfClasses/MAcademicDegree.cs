d3aedb4a261d4e7014ee2b963c9d0250049bf5d2
HappyRogue36 <83300022+HappyRogue36@users.noreply.github.com>
2021-05-20 17:47:09 +0300

    Update MAcademicDegree.cs

--- LibOfTimetableOfClasses/MAcademicDegree.cs
+++ LibOfTimetableOfClasses/MAcademicDegree.cs
@@ -8,50 +8,50 @@ namespace LibOfTimetableOfClasses
     public class MAcademicDegree : Model
     {
 
-        string _fullname;
-        string _reduction;
-        string _academicDegree;
+        string _fullname; //Полное имя
+        string _reduction; //Сокращённая учёная степень
+        string _academicDegree; //Учёная степень
 
-        public string AcademicDegree
+        public string AcademicDegree //Свойство для учёной степени
         {
             get
             {
-                return _academicDegree;
+                return _academicDegree; //Получение степени
             }
             set
             {
-                if (value == null || value == "") throw new Exception("Строка не может быть пустой");
-                _academicDegree = value;
+                if (value == null || value == "") throw new Exception("Строка не может быть пустой"); //Проверка на корректность полученной строки
+                _academicDegree = value; //Добавление в учёную степень полученных данных
             }
         }
 
         /// <summary>
         /// Полная запись учёной степени
         /// </summary>
-        public string FullName
+        public string FullName //Свойство для имени
         {
             get
             {
-                return _fullname;
+                return _fullname; //получение имени
             }
             set
             {
-                _fullname = value;
+                _fullname = value; //добавление имени
             }
         }
 
         /// <summary>
         /// Сокращенная запись учёной степени
         /// </summary>
-        public string Reduction
+        public string Reduction //Свойство для сокращённой учёной степени
         {
             get
             {
-                return _reduction;
+                return _reduction; //получение
             }
             set
             {
-                _reduction = value;
+                _reduction = value; //добавление
             }
         }
 
