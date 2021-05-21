c53e40c25b530ce848a689e33256b3fcdaf2397b
AMAT0RY <81570058+AMAT0RY@users.noreply.github.com>
2021-05-21 13:18:25 +0300

    Revert "Ссылка на источник и донат"
    This reverts commit eda0fa5602ca56a29bcce8a905a3991ad9397c6d.

--- TimetableOfClasses/Characteristic.cs
+++ TimetableOfClasses/Characteristic.cs
@@ -1,44 +1,9 @@
 ﻿using System.Windows.Forms;
-using System.Diagnostics;
-using System.Drawing;
+
 namespace TimetableOfClasses
 {
     public partial class Characteristic : Form
     {
         public Characteristic() => InitializeComponent();
-
-        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
-        {
-            Process.Start("https://github.com/Students-of-the-city-of-Kostroma/Student-timetable");
-        }
-
-        private void label12_Click(object sender, System.EventArgs e)
-        {
-
-        }
-
-        private void timer1_Tick(object sender, System.EventArgs e)
-        {
-            sml.Visible = !sml.Visible;
-            int i = 0;
-            if (sml.ForeColor == Color.Black)
-            {
-                i = 1;
-            }
-            switch (i)
-            {
-
-                case 0:
-                    if (sml.Visible == true)
-                        sml.ForeColor = Color.Black;
-                    break;
-
-                case 1:
-                    if (sml.Visible == true)
-                        sml.ForeColor = Color.Red;
-                    break;
-
-            }
-        }
     }
 }
