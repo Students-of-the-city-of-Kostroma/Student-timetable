7c6c4896a169cf364a08c20da88c14ec576ee0f9
HappyRogue36 <83300022+HappyRogue36@users.noreply.github.com>
2021-05-21 10:38:53 +0300

    Update CTypesOfOccupations.cs

--- LibOfTimetableOfClasses/CTypesOfOccupations.cs
+++ LibOfTimetableOfClasses/CTypesOfOccupations.cs
@@ -1,107 +1,118 @@
-﻿using System;
+using System;
 using System.Data;
 using System.Diagnostics;
 
 namespace LibOfTimetableOfClasses
 {
-    /// <summary>
-    /// Контроллер для объекта Вид занятия
-    /// </summary>
-    public class CTypesOfOccupations : DataTable, IController
-    {
-        public CTypesOfOccupations() : base("Вид занятия")
-        {
-            DataColumn column = new DataColumn(); //Создание таблицы
-            column.DataType = typeof(string); // Стоблцы имеют формат строки
-            column.ColumnName = "FullName"; // Имя стоблца Ф.И.О
-            column.Unique = true; //Уникальный пользователь
-            Columns.Add(column); //Дабавление в таблицу
+	/// <summary>
+	/// Контроллер для объекта Вид занятия
+	/// </summary>
+	public class CTypesOfOccupations : DataTable, IController
+	{
+		public CTypesOfOccupations() : base("Вид занятия")
+		{
+			DataColumn column = new DataColumn(); 
+			column.DataType = typeof(string); 
+			column.ColumnName = "FullName"; 
+			column.Unique = true; 
+			Columns.Add(column); 
 
-            column = new DataColumn(); //Создание нового обекта типа столбца
-            column.DataType = typeof(string); //Стоблцы имеют формат строки
-            column.ColumnName = "ShortName";// Имя стоблца Ф.И
-            Columns.Add(column); //Дабавление в таблицу
-        }
+			column = new DataColumn(); 
+			column.DataType = typeof(string); 
+			column.ColumnName = "ShortName";
+			Columns.Add(column); 
+		}
 
 
+		/// <summary>
+		/// Свойство, удаляющие объект
+		/// </summary>
+		/// <param name="model">Обьект для удаления</param>
+		/// <returns>Возвращаем удалён ли объект</returns>
+		public bool Delete(Model model) 
+		{
+			MTypesOfOccupations mTypesofOccupations = (MTypesOfOccupations)model; 
 
-        public bool Delete(Model model) //Булевское удаление 
-        {
-            MTypesOfOccupations mTypesofOccupations = (MTypesOfOccupations)model; //отделение инициализации данных класса
+			for (int i = 0; i < Rows.Count; i++) 
+			{
+				if ((string)Rows[i]["FullName"] == mTypesofOccupations.FullName
+				&& (string)Rows[i]["ShortName"] == mTypesofOccupations.ShortName) 
+				{
+					Rows[i].Delete(); 
+					return true;
+				}
+			}
+			return false;
+		}
+		/// <summary>
+		/// Свойство, добавляющие объект
+		/// </summary>
+		/// <param name="model">Обьект для добавления</param>
+		/// <returns>Возвращаем добавлен ли объект</returns>
+		public bool Insert(Model model)
+		{
+			MTypesOfOccupations mTypesOfOccupations = (MTypesOfOccupations)model;
 
-            for (int i = 0; i < Rows.Count; i++) //проверяем строки
-            {
-                if ((string)Rows[i]["FullName"] == mTypesofOccupations.FullName 
-                && (string)Rows[i]["ShortName"] == mTypesofOccupations.ShortName) //Проверка на корректность строки
-                {
-                    Rows[i].Delete(); //Удаление строки при истине
-                    return true;
-                }
-            }
-            return false;
-        }
+			if (mTypesOfOccupations.FullName == null || mTypesOfOccupations.ShortName == null) 
+				return false;
 
-        public bool Insert(Model model)//добавление 
-        {
-            MTypesOfOccupations mTypesOfOccupations = (MTypesOfOccupations)model;//отделение инициализации данных класса
+			for (int i = 0; i < Rows.Count; i++)
+			{
+				if ((string)Rows[i]["FullName"] == mTypesOfOccupations.FullName
+				&& (string)Rows[i]["ShortName"] == mTypesOfOccupations.ShortName)
+					return false;
+			}
 
-            if (mTypesOfOccupations.FullName == null || mTypesOfOccupations.ShortName == null) //Проверка на существование объекта
-                return false;
+			try
+			{
+				DataRow newRow = NewRow();
+				newRow["FullName"] = mTypesOfOccupations.FullName;
+				newRow["ShortName"] = mTypesOfOccupations.ShortName;
+				Rows.Add(newRow);
+				return true;
+			}
+			catch (Exception ex)
+			{
+				Debug.WriteLine(ex.Message);
+				return false;
+			}
+		}
+		/// <summary>
+		/// Метод обновления 
+		/// </summary>
+		/// <param name="model">Обьект для обновления</param>
+		/// <returns>Возвращает обнавлен ли объект</returns>
+		public bool Update(Model model)
+		{
+			MTypesOfOccupations mTypesOfOccupations = (MTypesOfOccupations)model;
 
-            for (int i = 0; i < Rows.Count; i++)//проверяем строки
-            {
-                if ((string)Rows[i]["FullName"] == mTypesOfOccupations.FullName
-                && (string)Rows[i]["ShortName"] == mTypesOfOccupations.ShortName) //если строки присутвуют не нужно удаление 
-                    return false;
-            }
+			if ((mTypesOfOccupations.FullName == null && mTypesOfOccupations.ShortName == null))
+				return false;
 
-            try
-            {
-                DataRow newRow = NewRow(); //Новая строка
-                //newRow["ID"] = Guid.NewGuid();
-                newRow["FullName"] = mTypesOfOccupations.FullName;// новая строка с Ф.И.О
-                newRow["ShortName"] = mTypesOfOccupations.ShortName;// новая строка с Ф.И
-                Rows.Add(newRow);//дабавление
-                return true;
-            }
-            catch (Exception ex)
-            {
-                Debug.WriteLine(ex.Message);//если не корректные данные выводим ошибку
-                return false;
-            }
-        }
-
-        public bool Update(Model model)//обновление
-        {
-            MTypesOfOccupations mTypesOfOccupations = (MTypesOfOccupations)model;//отделение инициализации данных класса
-
-            if ((mTypesOfOccupations.FullName == null && mTypesOfOccupations.ShortName == null)) //Проверка на существование объекта
-                return false;
-
-            for (int i = 0; i < Rows.Count; i++) //проверяем строки
-            {
-                if ((string)Rows[i]["FullName"] == mTypesOfOccupations.FullName) //Проверка на полное имя
-                {
-                    try
-                    {
-                        Rows[i].BeginEdit(); //Старт изменения
-                        Rows[i]["ShortName"] = mTypesOfOccupations.ShortName; //проверка на существование короткого имени
-                        Rows[i].EndEdit();// конец изменений
-                        Rows[i].AcceptChanges(); //Принятие изменений
-                        return true;
-                    }
-                    catch (Exception ex)
-                    {
-                        Debug.WriteLine(ex.Message); //если не корректные данные выводим ошибку
-                        return false;
-                    }
-                }
-            }
-            return false;
-        }
-        public bool Update(DataRow row, Model model) //корректность Update
-        {
-            throw new NotImplementedException(); //создание новой ошибки
-        }
-    }
+			for (int i = 0; i < Rows.Count; i++)
+			{
+				if ((string)Rows[i]["FullName"] == mTypesOfOccupations.FullName) 
+				{
+					try
+					{
+						Rows[i].BeginEdit();
+						Rows[i]["ShortName"] = mTypesOfOccupations.ShortName; 
+						Rows[i].EndEdit();
+						Rows[i].AcceptChanges(); 
+						return true;
+					}
+					catch (Exception ex)
+					{
+						Debug.WriteLine(ex.Message);
+						return false;
+					}
+				}
+			}
+			return false;
+		}
+		public bool Update(DataRow row, Model model)
+		{
+			throw new NotImplementedException();
+		}
+	}
 }
