44a694d9027d38bc2d631b3582f3cde86f5fda51
Anna583840 <81610246+Anna583840@users.noreply.github.com>
2021-05-21 09:10:17 +0300

    Ввод номера пар в зависимости от выбранной нумерации
    1. Сделан корректный выбор между нумерацией с 0 по 7 пару и  с 1 по 8
    2. В зависимости от выбранной нумерации сделан корректный ввод значений в поле элемента Most

--- TimetableOfClasses/Characteristic.cs
+++ TimetableOfClasses/Characteristic.cs
@@ -5,5 +5,36 @@ namespace TimetableOfClasses
     public partial class Characteristic : Form
     {
         public Characteristic() => InitializeComponent();
+
+        private void radioButton1_Click(object sender, System.EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                radioButton2.Checked = false;
+            }
+            else radioButton2.Checked = true;
+
+        }
+
+        private void Most_SelectedItemChanged(object sender, System.EventArgs e)
+        {
+            
+                if (int.TryParse(Most.Text, out int a))
+                {
+                    if (radioButton1.Checked)
+                    {
+                        if (a <= 7 && a >= 0)
+                            Most.Text = a.ToString();
+                        else Most.Text = "";
+                    }
+                    if (radioButton1.Checked == false)
+                    {
+                        if (a <= 8 && a >= 1)
+                            Most.Text = a.ToString();
+                        else Most.Text = "";
+                    }
+                }
+                else Most.Text = "";
+        }
     }
 }
