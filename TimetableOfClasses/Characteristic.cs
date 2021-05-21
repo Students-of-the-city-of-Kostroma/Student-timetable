96fc3f84b62744232ef5432d0440f5aa536daeec
DariyaKoroleva <81414458+DariyaKoroleva@users.noreply.github.com>
2021-05-21 12:43:32 +0300

    Update Characteristic.cs

--- TimetableOfClasses/Characteristic.cs
+++ TimetableOfClasses/Characteristic.cs
@@ -5,5 +5,12 @@ namespace TimetableOfClasses
     public partial class Characteristic : Form
     {
         public Characteristic() => InitializeComponent();
+        private void numericUpDown1_ValueChanged(object sender, System.EventArgs e)
+		{
+			numericUpDown1.Maximum = 12;
+			numericUpDown1.Minimum = 1;
+			numericUpDown1.ReadOnly = true;
+			numericUpDown1.Increment = 1;
+		}
     }
 }
