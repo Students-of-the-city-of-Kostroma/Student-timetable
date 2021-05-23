3a61396897de0f786051459bfc60559fa322f690
AMAT0RY <81570058+AMAT0RY@users.noreply.github.com>
2021-05-23 17:03:25 +0300

    Revert "Изменения во вкладке "Прочее" ( Ссылка на источник и донат)"
    This reverts commit 30a4463923affe69b3178c1def899bbb83f92446.

--- TimetableOfClasses/Characteristic.Designer.cs
+++ TimetableOfClasses/Characteristic.Designer.cs
@@ -25,7 +25,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Characteristic));
             this.Ok = new System.Windows.Forms.Button();
             this.Cancel = new System.Windows.Forms.Button();
@@ -58,9 +57,9 @@
             this.label7 = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             this.label9 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
-            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             this.Bells = new System.Windows.Forms.TabPage();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -68,12 +67,6 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.Cycles = new System.Windows.Forms.TabPage();
             this.Etc = new System.Windows.Forms.TabPage();
-            this.label11 = new System.Windows.Forms.Label();
-            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
-            this.label12 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.sml = new System.Windows.Forms.Label();
-            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.Numeration.SuspendLayout();
             this.tabControl1.SuspendLayout();
             this.Day.SuspendLayout();
@@ -83,7 +76,6 @@
             this.groupBox1.SuspendLayout();
             this.Bells.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
-            this.Etc.SuspendLayout();
             this.SuspendLayout();
             // 
             // Ok
@@ -146,7 +138,7 @@
             this.radioButton1.AutoSize = true;
             this.radioButton1.Location = new System.Drawing.Point(7, 20);
             this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(65, 17);
+            this.radioButton1.Size = new System.Drawing.Size(67, 17);
             this.radioButton1.TabIndex = 0;
             this.radioButton1.TabStop = true;
             this.radioButton1.Text = "С 0 по 7";
@@ -178,8 +170,8 @@
             this.Day.Controls.Add(this.Numeration);
             this.Day.Location = new System.Drawing.Point(4, 22);
             this.Day.Name = "Day";
-            this.Day.Padding = new System.Windows.Forms.Padding(3);
-            this.Day.Size = new System.Drawing.Size(354, 363);
+            this.Day.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.Day.Size = new System.Drawing.Size(349, 363);
             this.Day.TabIndex = 0;
             this.Day.Text = "День";
             this.Day.UseVisualStyleBackColor = true;
@@ -194,7 +186,7 @@
             this.label6.Location = new System.Drawing.Point(13, 125);
             this.label6.MaximumSize = new System.Drawing.Size(325, 30);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(309, 28);
+            this.label6.Size = new System.Drawing.Size(324, 28);
             this.label6.TabIndex = 9;
             this.label6.Text = "Настройки, приведённые ниже, могут быть изменены при задании характеристик групп " +
     "и препдавателей:";
@@ -234,7 +226,7 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.domainUpDown1.Location = new System.Drawing.Point(278, 303);
             this.domainUpDown1.Name = "domainUpDown1";
-            this.domainUpDown1.Size = new System.Drawing.Size(65, 20);
+            this.domainUpDown1.Size = new System.Drawing.Size(65, 22);
             this.domainUpDown1.TabIndex = 6;
             // 
             // label5
@@ -321,7 +313,7 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.Third.Location = new System.Drawing.Point(256, 97);
             this.Third.Name = "Third";
-            this.Third.Size = new System.Drawing.Size(65, 20);
+            this.Third.Size = new System.Drawing.Size(65, 22);
             this.Third.TabIndex = 8;
             // 
             // Second
@@ -330,7 +322,7 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.Second.Location = new System.Drawing.Point(256, 71);
             this.Second.Name = "Second";
-            this.Second.Size = new System.Drawing.Size(65, 20);
+            this.Second.Size = new System.Drawing.Size(65, 22);
             this.Second.TabIndex = 7;
             // 
             // First
@@ -339,7 +331,7 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.First.Location = new System.Drawing.Point(256, 45);
             this.First.Name = "First";
-            this.First.Size = new System.Drawing.Size(65, 20);
+            this.First.Size = new System.Drawing.Size(65, 22);
             this.First.TabIndex = 6;
             // 
             // Most
@@ -348,7 +340,7 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.Most.Location = new System.Drawing.Point(256, 19);
             this.Most.Name = "Most";
-            this.Most.Size = new System.Drawing.Size(65, 20);
+            this.Most.Size = new System.Drawing.Size(65, 22);
             this.Most.TabIndex = 5;
             // 
             // Date
@@ -362,7 +354,7 @@
             this.Date.Controls.Add(this.groupBox1);
             this.Date.Location = new System.Drawing.Point(4, 22);
             this.Date.Name = "Date";
-            this.Date.Padding = new System.Windows.Forms.Padding(3);
+            this.Date.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
             this.Date.Size = new System.Drawing.Size(354, 363);
             this.Date.TabIndex = 1;
             this.Date.Text = "Даты";
@@ -374,9 +366,9 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.numericUpDown1.Location = new System.Drawing.Point(265, 22);
-            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(2);
+            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(60, 20);
+            this.numericUpDown1.Size = new System.Drawing.Size(60, 22);
             this.numericUpDown1.TabIndex = 9;
             // 
             // button1
@@ -408,7 +400,7 @@
             this.label10.Location = new System.Drawing.Point(10, 156);
             this.label10.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(143, 13);
+            this.label10.Size = new System.Drawing.Size(153, 13);
             this.label10.TabIndex = 4;
             this.label10.Text = "Список праздничных дней:";
             // 
@@ -418,7 +410,7 @@
             this.checkBox2.Location = new System.Drawing.Point(4, 44);
             this.checkBox2.Margin = new System.Windows.Forms.Padding(2);
             this.checkBox2.Name = "checkBox2";
-            this.checkBox2.Size = new System.Drawing.Size(171, 17);
+            this.checkBox2.Size = new System.Drawing.Size(173, 17);
             this.checkBox2.TabIndex = 3;
             this.checkBox2.Text = "Учитывать нулевой семестр";
             this.checkBox2.UseVisualStyleBackColor = true;
@@ -429,7 +421,7 @@
             this.label7.Location = new System.Drawing.Point(55, 24);
             this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(176, 13);
+            this.label7.Size = new System.Drawing.Size(181, 13);
             this.label7.TabIndex = 1;
             this.label7.Text = "Максимальный номер семестра:";
             // 
@@ -459,28 +451,8 @@
             this.dateTimePicker2.Location = new System.Drawing.Point(193, 40);
             this.dateTimePicker2.Margin = new System.Windows.Forms.Padding(2);
             this.dateTimePicker2.Name = "dateTimePicker2";
-            this.dateTimePicker2.Size = new System.Drawing.Size(138, 20);
+            this.dateTimePicker2.Size = new System.Drawing.Size(138, 22);
             this.dateTimePicker2.TabIndex = 3;
-            // 
-            // label9
-            // 
-            this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(6, 40);
-            this.label9.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(92, 13);
-            this.label9.TabIndex = 1;
-            this.label9.Text = "Дата окончания:";
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(6, 17);
-            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(74, 13);
-            this.label8.TabIndex = 0;
-            this.label8.Text = "Дата начала:";
             // 
             // dateTimePicker1
             // 
@@ -490,8 +462,28 @@
             this.dateTimePicker1.Location = new System.Drawing.Point(193, 14);
             this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(2);
             this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(138, 20);
+            this.dateTimePicker1.Size = new System.Drawing.Size(138, 22);
             this.dateTimePicker1.TabIndex = 2;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(6, 40);
+            this.label9.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(97, 13);
+            this.label9.TabIndex = 1;
+            this.label9.Text = "Дата окончания:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(6, 17);
+            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(75, 13);
+            this.label8.TabIndex = 0;
+            this.label8.Text = "Дата начала:";
             // 
             // Bells
             // 
@@ -499,8 +491,8 @@
             this.Bells.Controls.Add(this.textBox1);
             this.Bells.Location = new System.Drawing.Point(4, 22);
             this.Bells.Name = "Bells";
-            this.Bells.Padding = new System.Windows.Forms.Padding(3);
-            this.Bells.Size = new System.Drawing.Size(354, 363);
+            this.Bells.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.Bells.Size = new System.Drawing.Size(349, 363);
             this.Bells.TabIndex = 2;
             this.Bells.Text = "Звонки";
             this.Bells.UseVisualStyleBackColor = true;
@@ -517,7 +509,7 @@
             this.Column2});
             this.dataGridView1.EnableHeadersVisualStyles = false;
             this.dataGridView1.Location = new System.Drawing.Point(181, 6);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersVisible = false;
             this.dataGridView1.RowHeadersWidth = 51;
@@ -547,7 +539,7 @@
             // textBox1
             // 
             this.textBox1.Location = new System.Drawing.Point(3, 6);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
@@ -559,83 +551,21 @@
             // 
             this.Cycles.Location = new System.Drawing.Point(4, 22);
             this.Cycles.Name = "Cycles";
-            this.Cycles.Padding = new System.Windows.Forms.Padding(3);
-            this.Cycles.Size = new System.Drawing.Size(354, 363);
+            this.Cycles.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.Cycles.Size = new System.Drawing.Size(349, 363);
             this.Cycles.TabIndex = 3;
             this.Cycles.Text = "Циклы";
             this.Cycles.UseVisualStyleBackColor = true;
             // 
             // Etc
             // 
-            this.Etc.Controls.Add(this.sml);
-            this.Etc.Controls.Add(this.textBox2);
-            this.Etc.Controls.Add(this.label12);
-            this.Etc.Controls.Add(this.linkLabel1);
-            this.Etc.Controls.Add(this.label11);
             this.Etc.Location = new System.Drawing.Point(4, 22);
             this.Etc.Name = "Etc";
-            this.Etc.Padding = new System.Windows.Forms.Padding(3);
-            this.Etc.Size = new System.Drawing.Size(354, 363);
+            this.Etc.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.Etc.Size = new System.Drawing.Size(349, 363);
             this.Etc.TabIndex = 4;
             this.Etc.Text = "Прочее";
             this.Etc.UseVisualStyleBackColor = true;
-            // 
-            // label11
-            // 
-            this.label11.AutoSize = true;
-            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label11.Location = new System.Drawing.Point(62, 14);
-            this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(233, 25);
-            this.label11.TabIndex = 0;
-            this.label11.Text = "Ссылка на источник";
-            // 
-            // linkLabel1
-            // 
-            this.linkLabel1.AutoSize = true;
-            this.linkLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.linkLabel1.Location = new System.Drawing.Point(105, 51);
-            this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(160, 20);
-            this.linkLabel1.TabIndex = 1;
-            this.linkLabel1.TabStop = true;
-            this.linkLabel1.Text = "Репозиторий github.";
-            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
-            // 
-            // label12
-            // 
-            this.label12.AutoSize = true;
-            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label12.Location = new System.Drawing.Point(3, 178);
-            this.label12.Name = "label12";
-            this.label12.Size = new System.Drawing.Size(234, 24);
-            this.label12.TabIndex = 2;
-            this.label12.Text = "Пожертвования - сбер";
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(25, 208);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 3;
-            this.textBox2.Text = "0752228344174";
-            // 
-            // sml
-            // 
-            this.sml.AutoSize = true;
-            this.sml.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.sml.ForeColor = System.Drawing.Color.Red;
-            this.sml.Location = new System.Drawing.Point(238, 134);
-            this.sml.Name = "sml";
-            this.sml.Size = new System.Drawing.Size(110, 108);
-            this.sml.TabIndex = 4;
-            this.sml.Text = "♥";
-            // 
-            // timer1
-            // 
-            this.timer1.Enabled = true;
-            this.timer1.Interval = 1000;
-            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // Characteristic
             // 
@@ -663,8 +593,6 @@
             this.Bells.ResumeLayout(false);
             this.Bells.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-            this.Etc.ResumeLayout(false);
-            this.Etc.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -710,11 +638,5 @@
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
-        private System.Windows.Forms.Label sml;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Label label12;
-        private System.Windows.Forms.LinkLabel linkLabel1;
-        private System.Windows.Forms.Label label11;
-        private System.Windows.Forms.Timer timer1;
     }
 }
