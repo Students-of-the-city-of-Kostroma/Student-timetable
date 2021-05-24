e92eb8676daf8c13855d8909ac452b67a7696bc5
Дмитрий <73197707+xShende@users.noreply.github.com>
2021-05-24 18:35:38 +0300

    Update Teachers.cs

--- TimetableOfClasses/Teachers.cs
+++ TimetableOfClasses/Teachers.cs
@@ -69,9 +69,9 @@ namespace TimetableOfClasses
                 Logs.GetError(ex);
             }
         }
-        /// <summary>
-	/// Метод обновления данных у объекта "учитель"
-	/// </summary>
+	///<summary>
+	///Метод обновления данных объекта "учитель"
+	///</summary>
         private void Update(object sender, EventArgs e)
         {
             try
