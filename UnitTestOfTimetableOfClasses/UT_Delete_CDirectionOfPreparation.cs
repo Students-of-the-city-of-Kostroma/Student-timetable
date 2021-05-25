e4541694b54ce30f6521b970959f17e3bc9c7c2f
luc1oz <vic.stepano2017@yandex.ru>
2021-05-25 09:19:34 +0300

    Revert "Починка автотестов"
    This reverts commit 2544dc65d916f4af27740fc0711c62788d84516f.

--- UnitTestOfTimetableOfClasses/UT_Delete_CDirectionOfPreparation.cs
+++ UnitTestOfTimetableOfClasses/UT_Delete_CDirectionOfPreparation.cs
@@ -9,20 +9,20 @@ namespace UnitTestOfTimetableOfClasses
         readonly RefData refData = new RefData();
         public void Pre_condition_Del()
         {
-            bool ex = false;
+            bool ex = true;
             bool act;
             MDirectionOfPreparation T_DirectionOfPreparation = new MDirectionOfPreparation("01.02.03", "Филология", 20, "КГУ");
             int C1 = refData.CDirectionOfPreparation.Rows.Count;
             act = refData.CDirectionOfPreparation.Insert(T_DirectionOfPreparation);
             int C2 = refData.CDirectionOfPreparation.Rows.Count;
             Assert.AreEqual(ex, act);
-            Assert.AreEqual(C1, C2);
+            Assert.AreEqual(C1 + 1, C2);
         }
         [TestMethod]
         public void Task_555_1()
         {
             Pre_condition_Del();
-            bool ex = false;
+            bool ex = true;
             bool act;
             MDirectionOfPreparation T_DirectionOfPreparation = new MDirectionOfPreparation("01.02.03", "Филология", 20, "КГУ");
 
@@ -30,7 +30,7 @@ namespace UnitTestOfTimetableOfClasses
             act = refData.CDirectionOfPreparation.Delete(T_DirectionOfPreparation);
             int C2 = refData.CDirectionOfPreparation.Rows.Count;
             Assert.AreEqual(ex, act);
-            Assert.AreEqual(C1, C2);
+            Assert.AreEqual(C1 - 1, C2);
         }
         [TestMethod]
         public void Task_555_2()
--- UnitTestOfTimetableOfClasses/UT_Delete_CInstitute.cs
+++ UnitTestOfTimetableOfClasses/UT_Delete_CInstitute.cs
@@ -14,7 +14,7 @@ namespace UnitTestOfTimetableOfClasses
         {
 
             MInstitute I_IInstitute = new MInstitute("Институт автоматизированных систем и технологий", "ИАСТ", "Лустгартен Ю.Л.", "Костромской Государственный Университет");
-            bool ex = false;
+            bool ex = true;
             refData.CInstitute.Insert(I_IInstitute);
             bool act = refData.CInstitute.Delete(I_IInstitute);
 
