f413eca0d0216511b3a15ec9e5c3d029a393b7b6
rv.zelenskiy <zelnskiroman@gmail.com>
2021-05-28 00:44:36 +0300

    CDiscipline Insert, Update, Delete unit tests.

---
+++ UnitTestOfTimetableOfClasses/CDiscipline/UT_CDiscipline_Delete/UT_CDiscipline_Delete.cs
@@ -0,0 +1,48 @@
+﻿using LibOfTimetableOfClasses;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+
+namespace UnitTestOfTimetableOfClasses.CDiscipline.UT_CDiscipline_Delete
+{
+    [TestClass]
+    public class UT_CDiscipline_Delete
+    {
+        RefData refData = new RefData();
+
+        /// <summary>
+        /// Удаление существующей записи.
+        /// </summary>
+        [TestMethod]
+        public void CDiscipline_Delete_Existing()
+        {
+            // arrange
+            MDiscipline gr = new MDiscipline("Клеопатра", "КПК", "1");
+            bool expected = true;
+
+            // act
+            refData.CDiscipline.Insert(gr);
+            bool actual = refData.CDiscipline.Delete(gr);
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        /// Удаление существующей записи.
+        /// </summary>
+        [TestMethod]
+        public void CDiscipline_Delete_NonExisting()
+        {
+            // arrange
+            MDiscipline gr = new MDiscipline("Клеопатра", "КПК", "1");
+            bool expected = false;
+
+            // act
+            bool actual = refData.CDiscipline.Delete(gr);
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ UnitTestOfTimetableOfClasses/CDiscipline/UT_CDiscipline_Insert/UT_CDiscipline_Insert.cs
@@ -0,0 +1,47 @@
+﻿using LibOfTimetableOfClasses;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestOfTimetableOfClasses.UT_CDiscipline
+{
+    [TestClass]
+    public class UT_CDiscipline_Insert
+    {
+        RefData refData = new RefData();
+
+        /// <summary>
+        /// Добавление записи в таблицу.
+        /// </summary>
+        [TestMethod]
+        public void CDiscipline_Insert_Emtpy()
+        {
+            // arrange
+            MDiscipline gr = new MDiscipline("Клеопатра", "КП", "1");
+            bool expected = true;
+
+            // act
+            bool actual = refData.CDiscipline.Insert(gr);
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        /// Добавление записи в таблицу, в которой уже есть дисциплина.
+        /// </summary>
+        [TestMethod]
+        public void CDiscipline_Insert_Existing()
+        {
+            // arrange
+            MDiscipline gr = new MDiscipline("Клеопатра", "КП", "1");
+            bool expected = false;
+
+            // act
+            refData.CDiscipline.Insert(gr);
+            bool actual = refData.CDiscipline.Insert(gr);
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
