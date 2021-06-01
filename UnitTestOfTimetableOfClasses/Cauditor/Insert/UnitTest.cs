897948c3e6de4dbc5d0d4dec4fe88a1004cde804
Дмитрий <demaster59ru85@gmail.com>
2021-06-01 22:08:09 +0300

    ForFix#1489
    ForFix

--- UnitTestOfTimetableOfClasses/Cauditor/Insert/UnitTest.cs
+++ UnitTestOfTimetableOfClasses/Cauditor/Insert/UnitTest.cs
@@ -2,7 +2,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 
-namespace UnitTestOfTimetableOfClasses.UT_CAuditor.UT_Insert
+namespace UnitTestOfTimetableOfClasses.UT_CAuditor.ForFix
 {
     [TestClass]
     public class UT_ICAuditor
