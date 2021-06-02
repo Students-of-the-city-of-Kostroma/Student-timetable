8c4e0804f4a5cda0dab3ea0c8efe0a2c11730143
DreAMer2308 <82033589+DreAMer2308@users.noreply.github.com>
2021-06-02 22:47:12 +0300

    Update MUniversity.cs

--- LibOfTimetableOfClasses/MUniversity.cs
+++ LibOfTimetableOfClasses/MUniversity.cs
@@ -288,7 +288,29 @@ namespace LibOfTimetableOfClasses
             MiddleNameRector = middleNameRector;
             Email = email;
             Phone = phone;
-
+        }
+        
+        private bool isLetterСaseNormal(string input)
+        {
+            input = Regex.Replace(input, @"\s+", " ");
+            input = Regex.Replace(input, @"-+", "-");
+            input = Regex.Replace(input, @" - ", "-");
+            input = Regex.Replace(input, @"- -", " ");
+            bool isSpacePressed = true;
+            foreach (var ch in input)
+            {
+                if (isSpacePressed)
+                {
+                    if (Char.IsLower(ch))
+                        return false;
+                    isSpacePressed = false;
+                }
+                else if (Char.IsUpper(ch))
+                    return false;
+                if (ch == ' ' || ch == '-')
+                    isSpacePressed = true;
+            }
+            return true;
         }
     }
 }
