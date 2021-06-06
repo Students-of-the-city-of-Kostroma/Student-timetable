a276541a69bdca1ad588d87f3385defed333c493
batygin307 <79691517+batygin307@users.noreply.github.com>
2021-06-06 12:13:18 +0300

    Update MEnclosures.cs
    #1500 - Fix 4 Style, 1 Duplication issues in multiple files. Метод isLetterСaseNormal перенесен в класс LetterCaseNormal.

--- LibOfTimetableOfClasses/MEnclosures.cs
+++ LibOfTimetableOfClasses/MEnclosures.cs
@@ -174,30 +174,7 @@ namespace LibOfTimetableOfClasses
             Address = address;
             Phone = phone;
             Comment = comment;
-        }
-
-        private bool IsLetterСaseNormal(string input)
-        {
-            input = Regex.Replace(input, @"\s+", " ");
-            input = Regex.Replace(input, @"-+", "-");
-            input = Regex.Replace(input, @" - ", "-");
-            input = Regex.Replace(input, @"- -", " ");
-            bool isSpacePressed = true;
-            foreach (var ch in input)
-            {
-                if (isSpacePressed)
-                {
-                    if (Char.IsLower(ch))
-                        return false;
-                    isSpacePressed = false;
-                }
-                else if (Char.IsUpper(ch))
-                    return false;
-                if (ch == ' ' || ch == '-')
-                    isSpacePressed = true;
-            }
-            return true;
-        }
+        }       
     }
 }
 
