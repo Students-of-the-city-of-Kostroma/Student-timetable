6b6494ce8dbf5995570d32bda61a50eb0c76d5c4
DreAMer2308 <82033589+DreAMer2308@users.noreply.github.com>
2021-06-15 15:51:27 +0300

    Update MInstitute.cs

--- LibOfTimetableOfClasses/MInstitute.cs
+++ LibOfTimetableOfClasses/MInstitute.cs
@@ -23,11 +23,16 @@ namespace LibOfTimetableOfClasses
             }
             set
             {
-                if (value == null || !Regex.IsMatch(value, @"[А-Яа-я\-\' ']") || value.Length > 60 || value.Length <= 1)
-                    throw new Exception("Некорректная запись поля");
+                if (value == null)
+                    throw new Exception("Поле Полное название института пустое");
+                if (!Regex.IsMatch(value, @"[А-Яа-я\-\' ']"))
+                    throw new Exception("Поле Полное название института содержит недопустимые символы");
+                if (value.Length > 60 && value.Length <= 1)
+                    throw new Exception("Некорректная длина поля");
                 for (int i = 1; i < value.Length; i++)
                 {
-                    if (((value[i] < 'А' || value[i] > 'я') && value[i] != ' ' && value[i] != '.') || ((value[i] >= 'А' && value[i] <= 'Я') && value[i - 1] != ' ')) throw new Exception("Заглавные буквы разрешены только в начале слова !");
+                    if (((value[i] < 'А' || value[i] > 'я') && value[i] != ' ' && value[i] != '.') || ((value[i] >= 'А' && value[i] <= 'Я') && value[i - 1] != ' ')) 
+                        throw new Exception("Заглавные буквы разрешены только в начале слова !");
                 }
                 for (int i = 1; i < value.Length-1; i++)
                     if ((value[i-1] >= 'А' && value[i-1] <= 'я') && (value[i]=='.' && value[i+1] !=' ')) 
